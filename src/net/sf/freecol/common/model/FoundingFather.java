a39cb29ff42c8b7ac17c6fd85868c521bd63ed7d
Stian Grenborgen <stiangre@users.sourceforge.net>
2004-07-15 11:31:54 +0000

    Moved strings to 'FreeColMessages.properties'.

--- src/net/sf/freecol/common/model/FoundingFather.java
+++ src/net/sf/freecol/common/model/FoundingFather.java
@@ -54,33 +54,39 @@ public class FoundingFather {
 
                             FATHER_COUNT = 25;
 
+    
     public static String getDescription(int foundingFather) {
+        return getPrefix + ".description";
+    }
+
+
+    private static String getPrefix() {
         switch (foundingFather) {
-            case ADAM_SMITH: return "Factories produce 1.5 manufactured goods per 1 raw material";
-            case JACOB_FUGGER: return "All boycotts currently in effect are dropped";
-            case PETER_MINUIT: return "Indians no longer demand payment for their land";
-            case PETER_STUYVESANT: return "The construction of custom houses becomes possible";
-            case JAN_DE_WITT: return "Trade with foreign colonies becomes possible";
-            case FERDINAND_MAGELLAN: return "Naval vessels movement is increased by 1 and the time to sail to/from Europe/America is shortened";
-            case FRANSICO_DE_CORONADO: return "All existing colonies become visible on the map";
-            case HERNANDO_DE_SOTO: return "Exploration of Lost City Rumors always yields a positive result and all units have an extended sight radius";
-            case HENRY_HUDSON: return "Increases output of all Fur trappers by 100%";
-            case LA_SALLE: return "Gives all existing and future colonies a stockade when they population reaches 3";
-            case HERNAN_CORTES: return "Conquered native settlements always yeild treasure (and in greater abundance) and the King\'s galleons transport it free of charge";
-            case GEORGE_WASHINGTION: return "Any soldier or dragoon who wins a combat is automatically upgraded to the next possible level";
-            case PAUL_REVERE: return "When a colony with no standing soldiers is attacked, a colonist automatically takes up any stockpiled muskets and defends";
-            case FRANCIS_DRAKE: return "Increases the combat strength of all Privateers by 50%";
-            case JOHN_PAUL_JONES: return "A Frigate is added to your colonial navy (free)";
-            case THOMAS_JEFFERSON: return "Increases Liberty Bell production in colonies by 50%";
-            case POCAHONTAS: return "All tension levels between you and natives are removed and Indian alarm is generated half as fast";
-            case THOMAS_PAINE: return "Increases Liberty Bell production in colonies by the value of the current tax rate";
-            case SIMON_BOLIVAR: return "Sons of Liberty membership in all existing colonies is increased by 20%";
-            case BENJAMIN_FRANKLIN: return "The King\'s foreign wars no longer have effect on relationships in the New World and Europeans in the New World always offer peace";
-            case WILLIAM_BREWSTER: return "No more criminals or servants appear on the docks and you can select which immigrant in the recruitment pool to move to the docks";
-            case WILLIAM_PENN: return "Cross production in all colonies in increased by 50%";
-            case FATHER_JEAN_DE_BREBEUF: return "All missionaries function as experts";
-            case JUAN_DE_SEPULVEDA: return "Increases chance that a subjugated Indian settlement will \"convert\" and join a colony";
-            case BARTOLOME_DE_LAS_CASAS: return "All existing Indian converts are converted to free colonists";
+            case ADAM_SMITH: return "foundingFather.adamSmith";
+            case JACOB_FUGGER: return "foundingFather.jacobFugger";
+            case PETER_MINUIT: return "foundingFather.peterMinuit";
+            case PETER_STUYVESANT: return "foundingFather.peterStuyvesant";
+            case JAN_DE_WITT: return "foundingFather.janDeWitt";
+            case FERDINAND_MAGELLAN: return "foundingFather.ferdinandMagellan";
+            case FRANSICO_DE_CORONADO: return "foundingFather.fransicoDeCoronado";
+            case HERNANDO_DE_SOTO: return "foundingFather.hernandoDeSoto";
+            case HENRY_HUDSON: return "foundingFather.henryHudson";
+            case LA_SALLE: return "foundingFather.laSalle";
+            case HERNAN_CORTES: return "foundingFather.hernanCortes";
+            case GEORGE_WASHINGTION: return "foundingFather.georgeWashingtion";
+            case PAUL_REVERE: return "foundingFather.paulRevere";
+            case FRANCIS_DRAKE: return "foundingFather.francisDrake";
+            case JOHN_PAUL_JONES: return "foundingFather.johnPaulJones";
+            case THOMAS_JEFFERSON: return "foundingFather.thomasJefferson";
+            case POCAHONTAS: return "foundingFather.pocahontas";
+            case THOMAS_PAINE: return "foundingFather.thomasPaine";
+            case SIMON_BOLIVAR: return "foundingFather.simonBolivar";
+            case BENJAMIN_FRANKLIN: return "foundingFather.benjaminFranklin";
+            case WILLIAM_BREWSTER: return "foundingFather.williamBrewster";
+            case WILLIAM_PENN: return "foundingFather.williamPenn";
+            case FATHER_JEAN_DE_BREBEUF: return "foundingFather.fatherJeanDeBrebeuf";
+            case JUAN_DE_SEPULVEDA: return "foundingFather.juanDeSepulveda";
+            case BARTOLOME_DE_LAS_CASAS: return "foundingFather.bartolmeDeLasCasas";
             default:
                 throw new IllegalArgumentException("FoundingFather has invalid type.");
         }
