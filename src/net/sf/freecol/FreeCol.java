93817a9847b21ba72e33a68cc471c16351098b89
Stian Grenborgen <stiangre@users.sourceforge.net>
2006-11-26 11:21:23 +0000

    Updating the build script (dist).

--- src/net/sf/freecol/FreeCol.java
+++ src/net/sf/freecol/FreeCol.java
@@ -40,7 +40,7 @@ public final class FreeCol {
 
     public static final  Specification  specification = new Specification();
 
-    private  static final String FREECOL_VERSION = "0.5.1_cvs";
+    private  static final String FREECOL_VERSION = "0.5.2";
 
     public static final String  META_SERVER_ADDRESS = "meta.freecol.org";
     public static final int     META_SERVER_PORT = 3540;
