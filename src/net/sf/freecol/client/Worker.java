b053ed09db85707addc3e9851fd44dbf3e16b9e5
kloune <kloune@users.sourceforge.net>
2007-02-13 11:09:44 +0000

    - Added documentation to the Worker

--- src/net/sf/freecol/client/Worker.java
+++ src/net/sf/freecol/client/Worker.java
@@ -2,43 +2,60 @@ package net.sf.freecol.client;
 
 import java.util.concurrent.LinkedBlockingQueue;
 
+/**
+ * The Worker Thread executes jobs one after another. The thread manages a queue
+ * where new jobs can be enqueued. The jobs are processed synchronously by the
+ * worker.
+ */
 public final class Worker extends Thread {
-	public static final String COPYRIGHT = "Copyright (C) 2003-2007 The FreeCol Team";
-	public static final String LICENSE = "http://www.gnu.org/licenses/gpl.html";
-	public static final String REVISION = "$Revision$";
+    public static final String COPYRIGHT = "Copyright (C) 2003-2007 The FreeCol Team";
 
-	private final LinkedBlockingQueue<Runnable> jobList;
-	private volatile boolean stopRunning;
+    public static final String LICENSE = "http://www.gnu.org/licenses/gpl.html";
 
-	public Worker() {
-		super("Worker");
-		jobList = new LinkedBlockingQueue<Runnable>();
-		stopRunning = false;
-	}
+    public static final String REVISION = "$Revision$";
 
-	public void run() {
-		while (!stopRunning) {
-			try {
-				// run the next waiting job
-				Runnable job = jobList.take();
-				try {
-					job.run();
-				} catch (Exception e) {
-					System.err.println("a job produced an error:");
-					e.printStackTrace();
-				}
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    private final LinkedBlockingQueue<Runnable> jobList;
 
-	public void schedule(Runnable job) {
-		jobList.add(job);
-	}
+    private volatile boolean stopRunning;
 
-	public void askToStop() {
-		stopRunning = true;
-		this.interrupt();
-	}
+
+    public Worker() {
+        super("Worker");
+        jobList = new LinkedBlockingQueue<Runnable>();
+        stopRunning = false;
+    }
+
+    public void run() {
+        while (!stopRunning) {
+            try {
+                // run the next waiting job
+                Runnable job = jobList.take();
+                try {
+                    job.run();
+                } catch (Exception e) {
+                    System.err.println("a job produced an error:");
+                    e.printStackTrace();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Adds a new job to the queue
+     * 
+     * @param job the job to add to the queue.
+     */
+    public void schedule(Runnable job) {
+        jobList.add(job);
+    }
+
+    /**
+     * Makes the worker thread stop running.
+     */
+    public void askToStop() {
+        stopRunning = true;
+        this.interrupt();
+    }
 }
