6a87611bcffd4ae7200f6bd288a77616c3b152a4
erik_bergersjo <erik_bergersjo@users.sourceforge.net>
2007-03-18 17:42:48 +0000

    Added logging and error dialog for uncaught exceptions in key handler.

--- src/net/sf/freecol/client/gui/CanvasKeyListener.java
+++ src/net/sf/freecol/client/gui/CanvasKeyListener.java
@@ -1,48 +1,48 @@
-
 package net.sf.freecol.client.gui;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import net.sf.freecol.FreeCol;
 import net.sf.freecol.client.control.InGameController;
 import net.sf.freecol.common.model.Map;
 
-
 /**
-* Listens to keys being pressed at the level of the Canvas.
-*/
+ * Listens to keys being pressed at the level of the Canvas.
+ */
 public final class CanvasKeyListener implements KeyListener {
     private static final Logger logger = Logger.getLogger(CanvasKeyListener.class.getName());
 
-    public static final String  COPYRIGHT = "Copyright (C) 2003-2005 The FreeCol Team";
-    public static final String  LICENSE = "http://www.gnu.org/licenses/gpl.html";
-    public static final String  REVISION = "$Revision$";
+    public static final String COPYRIGHT = "Copyright (C) 2003-2005 The FreeCol Team";
 
-    private final Canvas            parent;
+    public static final String LICENSE = "http://www.gnu.org/licenses/gpl.html";
+
+    public static final String REVISION = "$Revision$";
+
+    private final Canvas parent;
+
     private final InGameController inGameController;
 
-    //private KeyEvent keyEvent;
 
-    
     /**
-    * The constructor to use.
-    * 
-    * @param parent The container of all child panels.
-    * @param inGameController The controller to be used for
-    *       handling the events.
-    */
+     * The constructor to use.
+     * 
+     * @param parent The container of all child panels.
+     * @param inGameController The controller to be used for handling the
+     *            events.
+     */
     public CanvasKeyListener(Canvas parent, InGameController inGameController) {
         this.parent = parent;
         this.inGameController = inGameController;
     }
 
-
     /**
-    * Invoked when a key has been pressed.
-    * @param e The event that holds key-information.
-    */
+     * Invoked when a key has been pressed.
+     * 
+     * @param e The event that holds key-information.
+     */
     public void keyPressed(KeyEvent e) {
         if (parent.getGUI().isDragStarted()) {
             parent.getGUI().stopDrag();
@@ -55,127 +55,141 @@ public final class CanvasKeyListener implements KeyListener {
 
         if (e.getModifiers() != 0) {
             return;
-        }        
+        }
 
-        if(parent.getGUI().getViewMode().getView() == ViewMode.MOVE_UNITS_MODE){
-            switch(e.getKeyCode()) {
-                case KeyEvent.VK_ESCAPE:
-                    //main menu
-                    break;
-                case KeyEvent.VK_NUMPAD1:
-                case KeyEvent.VK_END:
-                    inGameController.moveActiveUnit(Map.SW);
-                    break;
-                case KeyEvent.VK_NUMPAD2:
-                case KeyEvent.VK_KP_DOWN:
-                case KeyEvent.VK_DOWN:
-                    inGameController.moveActiveUnit(Map.S);
-                    break;
-                case KeyEvent.VK_NUMPAD3:
-                case KeyEvent.VK_PAGE_DOWN:
-                    inGameController.moveActiveUnit(Map.SE);
-                    break;
-                case KeyEvent.VK_NUMPAD4:
-                case KeyEvent.VK_KP_LEFT:
-                case KeyEvent.VK_LEFT:
-                    inGameController.moveActiveUnit(Map.W);
-                    break;
-                case KeyEvent.VK_NUMPAD5:
-                case KeyEvent.VK_C:
-                    inGameController.centerActiveUnit();
-                    break;
-                case KeyEvent.VK_NUMPAD6:
-                case KeyEvent.VK_KP_RIGHT:
-                case KeyEvent.VK_RIGHT:
-                    inGameController.moveActiveUnit(Map.E);
-                    break;
-                case KeyEvent.VK_NUMPAD7:
-                case KeyEvent.VK_HOME:
-                    inGameController.moveActiveUnit(Map.NW);
-                    break;
-                case KeyEvent.VK_NUMPAD8:
-                case KeyEvent.VK_KP_UP:
-                case KeyEvent.VK_UP:
-                    inGameController.moveActiveUnit(Map.N);
-                    break;
-                case KeyEvent.VK_NUMPAD9:
-                case KeyEvent.VK_PAGE_UP:
-                    inGameController.moveActiveUnit(Map.NE);
-                    break;
-                default:
-                    logger.info("The typed key (" + e.getKeyCode() + ") doesn't have a function yet.");
+        try {
+            int viewMode = parent.getGUI().getViewMode().getView();
+            if (viewMode == ViewMode.MOVE_UNITS_MODE) {
+                moveUnit(e);
+            } else if (viewMode == ViewMode.VIEW_TERRAIN_MODE) {
+                moveCursor(e);
             }
-        } else if(parent.getGUI().getViewMode().getView() == ViewMode.VIEW_TERRAIN_MODE){
-            switch(e.getKeyCode()) {
-                case KeyEvent.VK_ESCAPE:
-                    //main menu
-                    break;
-                case KeyEvent.VK_NUMPAD1:
-                case KeyEvent.VK_END:
-                    parent.getGUI().moveTileCursor(Map.SW);
-                    break;
-                case KeyEvent.VK_NUMPAD2:
-                case KeyEvent.VK_KP_DOWN:
-                case KeyEvent.VK_DOWN:
-                    parent.getGUI().moveTileCursor(Map.S);
-                    break;
-                case KeyEvent.VK_NUMPAD3:
-                case KeyEvent.VK_PAGE_DOWN:
-                    parent.getGUI().moveTileCursor(Map.SE);
-                    break;
-                case KeyEvent.VK_NUMPAD4:
-                case KeyEvent.VK_KP_LEFT:
-                case KeyEvent.VK_LEFT:
-                    parent.getGUI().moveTileCursor(Map.W);
-                    break;
-                case KeyEvent.VK_NUMPAD5:
-                case KeyEvent.VK_C:
-                    inGameController.centerActiveUnit();
-                    break;
-                case KeyEvent.VK_NUMPAD6:
-                case KeyEvent.VK_KP_RIGHT:
-                case KeyEvent.VK_RIGHT:
-                    parent.getGUI().moveTileCursor(Map.E);
-                    break;
-                case KeyEvent.VK_NUMPAD7:
-                case KeyEvent.VK_HOME:
-                    parent.getGUI().moveTileCursor(Map.NW);
-                    break;
-                case KeyEvent.VK_NUMPAD8:
-                case KeyEvent.VK_KP_UP:
-                case KeyEvent.VK_UP:
-                    parent.getGUI().moveTileCursor(Map.N);
-                    break;
-                case KeyEvent.VK_NUMPAD9:
-                case KeyEvent.VK_PAGE_UP:
-                    parent.getGUI().moveTileCursor(Map.NE);
-                    break;
-                case KeyEvent.VK_SPACE:
-                    TerrainCursor cursor = parent.getGUI().getCursor();
-                    parent.showTilePopup(parent.getGUI().getSelectedTile(),cursor.getCanvasX(),cursor.getCanvasY());
-                    break;
-                default:
-                    logger.info("The typed key (" + e.getKeyCode() + ") doesn't have a function yet.");
-            }           
+        } catch (Exception ex) {
+            logger.log(Level.SEVERE, "CanvasKeyListener.keyPressed - exception!", ex);
+            parent.errorMessage(null, ex.toString());
         }
     }
-    
 
-    
+    private void moveCursor(KeyEvent e) {
+        switch (e.getKeyCode()) {
+        case KeyEvent.VK_ESCAPE:
+            // main menu
+            break;
+        case KeyEvent.VK_NUMPAD1:
+        case KeyEvent.VK_END:
+            parent.getGUI().moveTileCursor(Map.SW);
+            break;
+        case KeyEvent.VK_NUMPAD2:
+        case KeyEvent.VK_KP_DOWN:
+        case KeyEvent.VK_DOWN:
+            parent.getGUI().moveTileCursor(Map.S);
+            break;
+        case KeyEvent.VK_NUMPAD3:
+        case KeyEvent.VK_PAGE_DOWN:
+            parent.getGUI().moveTileCursor(Map.SE);
+            break;
+        case KeyEvent.VK_NUMPAD4:
+        case KeyEvent.VK_KP_LEFT:
+        case KeyEvent.VK_LEFT:
+            parent.getGUI().moveTileCursor(Map.W);
+            break;
+        case KeyEvent.VK_NUMPAD5:
+        case KeyEvent.VK_C:
+            inGameController.centerActiveUnit();
+            break;
+        case KeyEvent.VK_NUMPAD6:
+        case KeyEvent.VK_KP_RIGHT:
+        case KeyEvent.VK_RIGHT:
+            parent.getGUI().moveTileCursor(Map.E);
+            break;
+        case KeyEvent.VK_NUMPAD7:
+        case KeyEvent.VK_HOME:
+            parent.getGUI().moveTileCursor(Map.NW);
+            break;
+        case KeyEvent.VK_NUMPAD8:
+        case KeyEvent.VK_KP_UP:
+        case KeyEvent.VK_UP:
+            parent.getGUI().moveTileCursor(Map.N);
+            break;
+        case KeyEvent.VK_NUMPAD9:
+        case KeyEvent.VK_PAGE_UP:
+            parent.getGUI().moveTileCursor(Map.NE);
+            break;
+        case KeyEvent.VK_SPACE:
+            TerrainCursor cursor = parent.getGUI().getCursor();
+            parent.showTilePopup(parent.getGUI().getSelectedTile(), cursor.getCanvasX(), cursor.getCanvasY());
+            break;
+        default:
+            logger.info("The typed key (" + e.getKeyCode() + ") doesn't have a function yet.");
+        }
+    }
+
+    private void moveUnit(KeyEvent e) {
+        switch (e.getKeyCode()) {
+        case KeyEvent.VK_ESCAPE:
+            // main menu
+            break;
+        case KeyEvent.VK_NUMPAD1:
+        case KeyEvent.VK_END:
+            inGameController.moveActiveUnit(Map.SW);
+            break;
+        case KeyEvent.VK_NUMPAD2:
+        case KeyEvent.VK_KP_DOWN:
+        case KeyEvent.VK_DOWN:
+            inGameController.moveActiveUnit(Map.S);
+            break;
+        case KeyEvent.VK_NUMPAD3:
+        case KeyEvent.VK_PAGE_DOWN:
+            inGameController.moveActiveUnit(Map.SE);
+            break;
+        case KeyEvent.VK_NUMPAD4:
+        case KeyEvent.VK_KP_LEFT:
+        case KeyEvent.VK_LEFT:
+            inGameController.moveActiveUnit(Map.W);
+            break;
+        case KeyEvent.VK_NUMPAD5:
+        case KeyEvent.VK_C:
+            inGameController.centerActiveUnit();
+            break;
+        case KeyEvent.VK_NUMPAD6:
+        case KeyEvent.VK_KP_RIGHT:
+        case KeyEvent.VK_RIGHT:
+            inGameController.moveActiveUnit(Map.E);
+            break;
+        case KeyEvent.VK_NUMPAD7:
+        case KeyEvent.VK_HOME:
+            inGameController.moveActiveUnit(Map.NW);
+            break;
+        case KeyEvent.VK_NUMPAD8:
+        case KeyEvent.VK_KP_UP:
+        case KeyEvent.VK_UP:
+            inGameController.moveActiveUnit(Map.N);
+            break;
+        case KeyEvent.VK_NUMPAD9:
+        case KeyEvent.VK_PAGE_UP:
+            inGameController.moveActiveUnit(Map.NE);
+            break;
+        default:
+            logger.info("The typed key (" + e.getKeyCode() + ") doesn't have a function yet.");
+        }
+    }
+
     /**
-    * Invoked when a key has been released.
-    * @param e The event that holds key-information.
-    */
+     * Invoked when a key has been released.
+     * 
+     * @param e The event that holds key-information.
+     */
     public void keyReleased(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
             parent.getGUI().setDragPath(null);
         }
     }
-    
+
     /**
-    * Invoked when a key has been typed.
-    * @param e The event that holds key-information.
-    */
+     * Invoked when a key has been typed.
+     * 
+     * @param e The event that holds key-information.
+     */
     public void keyTyped(KeyEvent e) {
         // Ignore for now
     }
