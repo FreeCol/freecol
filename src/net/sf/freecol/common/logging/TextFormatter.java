2c4e0da2df693c357403816c111477a7e9fff45e
erik_bergersjo <erik_bergersjo@users.sourceforge.net>
2007-03-29 13:25:45 +0000

    Added full stack trace to log.

--- src/net/sf/freecol/common/logging/TextFormatter.java
+++ src/net/sf/freecol/common/logging/TextFormatter.java
@@ -1,59 +1,52 @@
-
 package net.sf.freecol.common.logging;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Date;
 import java.util.logging.Formatter;
-import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
-
-
-
 /**
-* Formats a log record's data into human-readable text.
-*/
+ * Formats a log record's data into human-readable text.
+ */
 final class TextFormatter extends Formatter {
 
-    public static final String  COPYRIGHT = "Copyright (C) 2003-2005 The FreeCol Team";
-    public static final String  LICENSE = "http://www.gnu.org/licenses/gpl.html";
-    public static final String  REVISION = "$Revision$";
+    public static final String COPYRIGHT = "Copyright (C) 2003-2005 The FreeCol Team";
+
+    public static final String LICENSE = "http://www.gnu.org/licenses/gpl.html";
+
+    public static final String REVISION = "$Revision$";
 
 
     /**
-    * The constructor to use.
-    */
+     * The constructor to use.
+     */
     public TextFormatter() {
     }
 
-
-
-
-
     /**
-    * Formats the given log record's data into human-readable text.
-    *
-    * @param record The log record whose data needs to be formatted.
-    * @return The log record's data as a string.
-    */
+     * Formats the given log record's data into human-readable text.
+     * 
+     * @param record The log record whose data needs to be formatted.
+     * @return The log record's data as a string.
+     */
     public String format(LogRecord record) {
-        String level;
-        if (record.getLevel() == Level.INFO) {
-            level = "INFO";
-        } else if (record.getLevel() == Level.ALL) {
-            level = "ALL";
-        } else if (record.getLevel() == Level.SEVERE) {
-            level = "SEVERE";
-        } else if (record.getLevel() == Level.WARNING) {
-            level = "WARNING";
-        } else {
-            level = "UNKNOWN";
+        StringBuilder result = new StringBuilder();
+        result.append(record.getSourceClassName()).append(' ').append(record.getSourceMethodName());
+        result.append("\n\t").append(record.getLevel().getName()).append(": ").append(
+                record.getMessage().replaceAll("\n", "\n\t"));
+        result.append("\n\t").append(new Date(record.getMillis()).toString());
+        result.append("\n\tThread ID: ").append(record.getThreadID()).append('\n');
+        if (record.getThrown() != null) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            pw.println("\tStack trace:");
+            record.getThrown().printStackTrace(pw);
+            pw.println("----------------------------");
+            pw.flush();
+            result.append(sw.toString());
         }
 
-        String result = record.getSourceClassName() + ' ' + record.getSourceMethodName();
-        result += "\n\t" + level + ": " + record.getMessage().replaceAll("\n", "\n\t");
-        result += "\n\t" + new Date(record.getMillis()).toString();
-        result += "\n\tThread ID: " + record.getThreadID() + '\n';
-
-        return result;
+        return result.toString();
     }
 }
