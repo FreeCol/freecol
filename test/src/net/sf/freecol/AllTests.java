229ab7569ba1db8cacee18909dbb8c93be46730b
coezbek <coezbek@users.sourceforge.net>
2007-04-12 00:49:02 +0000

    Add minor documentation for testcase.

--- test/src/net/sf/freecol/AllTests.java
+++ test/src/net/sf/freecol/AllTests.java
@@ -3,16 +3,20 @@ package net.sf.freecol;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+/**
+ * The main test class for Freecol. All tests in the subfolders will be run.
+ * 
+ */
 public class AllTests {
 
-	public static Test suite() {
-		TestSuite suite = new TestSuite("Test for net.sf.freecol");
-		//$JUnit-BEGIN$
+    public static Test suite() {
+        TestSuite suite = new TestSuite("Test for net.sf.freecol");
+        // $JUnit-BEGIN$
 
-		//$JUnit-END$
-		suite.addTest(net.sf.freecol.common.AllTests.suite());
-		suite.addTest(net.sf.freecol.server.generator.AllTests.suite());
-		return suite;
-	}
+        // $JUnit-END$
+        suite.addTest(net.sf.freecol.common.AllTests.suite());
+        suite.addTest(net.sf.freecol.server.generator.AllTests.suite());
+        return suite;
+    }
 
 }
