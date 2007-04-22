72ea455d679c8b171827b6fc3250162052932c5d
coezbek <coezbek@users.sourceforge.net>
2007-04-22 17:32:57 +0000

    Documentation patch.

--- src/net/sf/freecol/common/model/Nameable.java
+++ src/net/sf/freecol/common/model/Nameable.java
@@ -1,17 +1,14 @@
 
 package net.sf.freecol.common.model;
 
-
-
 /**
-* An object that can change its name.
-*/
+ * An object that has a name, which also can be changed.
+ */
 public interface Nameable {
 
     public static final String  COPYRIGHT = "Copyright (C) 2003-2007 The FreeCol Team";
     public static final String  LICENSE = "http://www.gnu.org/licenses/gpl.html";
     public static final String  REVISION = "$Revision$";
-
 
     /**
     * Gets the name of this <code>Nameable</code>.
