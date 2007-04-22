b2603bcd90b25b36609a5585b2939f0cff06fd01
coezbek <coezbek@users.sourceforge.net>
2007-04-22 17:43:51 +0000

    Fixed test to conform to changes in newTurn.

--- test/src/net/sf/freecol/common/model/GameTest.java
+++ test/src/net/sf/freecol/common/model/GameTest.java
@@ -3,15 +3,18 @@ package net.sf.freecol.common.model;
 import java.util.Vector;
 
 import junit.framework.TestCase;
+import net.sf.freecol.common.FreeColException;
 import net.sf.freecol.common.model.Game;
 import net.sf.freecol.common.model.Player;
 import net.sf.freecol.util.test.MockModelController;
 
 public class GameTest extends TestCase {
 
-	public void testGame() {
+	public void testGame() throws FreeColException {
 
 		Game game = new Game(new MockModelController());
+        
+        game.setMap(new Map(game, Map.SMALL));
 
 		game.addPlayer(new Player(game, "TestPlayer", false, Player.DUTCH));
 
@@ -25,7 +28,7 @@ public class GameTest extends TestCase {
 		Vector<Player> players = new Vector<Player>();
 
 		for (int i = Player.NUMBER_OF_NATIONS - 1; i >= 0; i--) {
-			Player p = new Player(game, String.valueOf(i), false, i != Player.DUTCH && i != Player.FRENCH && i != Player.SPANISH && i != Player.ENGLISH, i);
+			Player p = new Player(game, String.valueOf(i), false, !Player.isEuropeanNoREF(i), i);
 			game.addPlayer(p);
 			players.add(p);
 		}
