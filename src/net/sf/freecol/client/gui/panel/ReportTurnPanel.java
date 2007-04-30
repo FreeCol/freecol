2f6713168baabbb478e6727c3a28fcb672e7599e
Michael Vehrs <burschik@users.sourceforge.net>
2007-04-30 13:27:14 +0000

    Now uses StyledDocument.

--- src/net/sf/freecol/client/gui/panel/ReportTurnPanel.java
+++ src/net/sf/freecol/client/gui/panel/ReportTurnPanel.java
@@ -2,15 +2,23 @@ package net.sf.freecol.client.gui.panel;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Insets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.MissingResourceException;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
+import javax.swing.text.StyledDocument;
 
 import net.sf.freecol.client.ClientOptions;
 import net.sf.freecol.client.FreeColClient;
@@ -38,6 +46,7 @@ public final class ReportTurnPanel extends ReportPanel implements ActionListener
 
     private final FreeColClient freeColClient;
 
+    private HashMap<String, Colony> colonies;
 
     /**
      * The constructor that will add the items to this panel.
@@ -54,6 +63,14 @@ public final class ReportTurnPanel extends ReportPanel implements ActionListener
      * Prepares this panel to be displayed.
      */
     public void initialize(ArrayList<ModelMessage> messages) {
+
+        colonies = new HashMap<String, Colony>();
+        Player player = getCanvas().getClient().getMyPlayer();
+
+        for (Colony colony : player.getColonies()) {
+            colonies.put(colony.getName(), colony);
+        }
+
         Comparator<ModelMessage> comparator = getCanvas().getClient().getClientOptions().getModelMessageComparator();
         if (comparator != null) {
             Collections.sort(messages, comparator);
@@ -80,7 +97,7 @@ public final class ReportTurnPanel extends ReportPanel implements ActionListener
         // Display Panel
         reportPanel.removeAll();
 
-        int[] widths = new int[] { 0, margin, 0, margin, 0, margin, 0 };
+        int[] widths = new int[] { 0, margin, 500, margin, 0, margin, 0 };
         int[] heights = new int[2 * (messages.size() + headlines) - 1];
         int imageColumn = 1;
         int textColumn = 3;
@@ -115,25 +132,21 @@ public final class ReportTurnPanel extends ReportPanel implements ActionListener
 
             final JLabel label = new JLabel();
             if (message.getDisplay() != null) {
-                label.setIcon(getCanvas().getImageIcon(message.getDisplay()));
+                if (message.getDisplay() instanceof Colony) {
+                    final JButton button = new JButton();
+                    button.setIcon(getCanvas().getImageIcon(message.getDisplay()));
+                    button.setActionCommand(((Colony) message.getDisplay()).getName());
+                    button.addActionListener(this);
+                    button.setBorder(BorderFactory.createEmptyBorder());
+                    reportPanel.add(button, higConst.rc(row, imageColumn, ""));
+                } else {
+                    label.setIcon(getCanvas().getImageIcon(message.getDisplay()));
+                    reportPanel.add(label, higConst.rc(row, imageColumn, ""));
+                }
             }
-            reportPanel.add(label, higConst.rc(row, imageColumn, ""));
 
-            final JTextArea textArea = new JTextArea();
-            try {
-                String text = Messages.message(message.getMessageID(), message.getData());
-                textArea.setText(text);
-                textArea.setColumns(42);
-                textArea.setOpaque(false);
-                textArea.setLineWrap(true);
-                textArea.setWrapStyleWord(true);
-                textArea.setFocusable(false);
-                textArea.setFont(defaultFont);
-                reportPanel.add(textArea, higConst.rc(row, textColumn, "l"));
-
-            } catch (MissingResourceException e) {
-                logger.warning("could not find message with id: " + message.getMessageID() + ".");
-            }
+            final JTextPane textPane = getTextPane(message);
+            reportPanel.add(textPane, higConst.rc(row, textColumn));
             if (message.getType() == ModelMessage.WAREHOUSE_CAPACITY) {
                 JButton ignoreButton = new JButton("x");
                 ignoreButton.setToolTipText(Messages.message("model.message.ignore", message.getData()));
@@ -141,7 +154,7 @@ public final class ReportTurnPanel extends ReportPanel implements ActionListener
                     public void actionPerformed(ActionEvent event) {
                         boolean flag = label.isEnabled();
                         freeColClient.getInGameController().ignoreMessage(message, flag);
-                        textArea.setEnabled(!flag);
+                        textPane.setEnabled(!flag);
                         label.setEnabled(!flag);
                     }
                 });
@@ -155,7 +168,7 @@ public final class ReportTurnPanel extends ReportPanel implements ActionListener
                 public void actionPerformed(ActionEvent event) {
                     boolean flag = filterOption.getValue();
                     filterOption.setValue(!flag);
-                    textArea.setEnabled(!flag);
+                    textPane.setEnabled(!flag);
                     label.setEnabled(!flag);
                 }
             });
@@ -211,7 +224,100 @@ public final class ReportTurnPanel extends ReportPanel implements ActionListener
         }
 
         headline.setFont(smallHeaderFont);
+        headline.setOpaque(false);
+        headline.setForeground(LINK_COLOR);
+        headline.setBorder(BorderFactory.createEmptyBorder());
         return headline;
+    }
+
+    //Create a text pane.
+    private JTextPane getTextPane(ModelMessage message) {
+        JTextPane textPane = new JTextPane();
+        textPane.setOpaque(false);
+	textPane.setEditable(false);
+
+        StyledDocument doc = textPane.getStyledDocument();
+        //Initialize some styles.
+        Style def = StyleContext.getDefaultStyleContext().getStyle(StyleContext.DEFAULT_STYLE);
+	
+        Style regular = doc.addStyle("regular", def);
+        StyleConstants.setFontFamily(def, "Dialog");
+        StyleConstants.setBold(def, true);
+	StyleConstants.setFontSize(def, 12);
+
+        Style buttonStyle = doc.addStyle("button", regular);
+        StyleConstants.setForeground(buttonStyle, LINK_COLOR);
+
+        String text = Messages.message(message.getMessageID());
+        String colonyName = null;
+        String[][] data = message.getData();
+        if (data != null) {
+            for (int i = 0; i < data.length; i++) {
+                if (data[i] == null || data[i].length != 2) {
+                    logger.warning("Data has a wrong format for message: " + message);
+                } else if (data[i][0] == null || data[i][1] == null) {
+                    logger.warning("Data in model message is 'null': " + message + ", " + data[i][0] + ", "
+                            + data[i][1]);
+                } else if (data[i][0].equals("%colony%")) {
+                    colonyName = data[i][1];
+                    continue;
+                } else {
+                    text = text.replaceAll(data[i][0], data[i][1]);
+                }
+            }
+        }
+
+        int index = text.indexOf("%colony%");
+        try {
+            if (index == -1) {
+                doc.insertString(doc.getLength(),
+                                 Messages.message(message.getMessageID(), message.getData()),
+                                 doc.getStyle("regular"));
+            } else {
+                doc.insertString(doc.getLength(), 
+                                 text.substring(0, index),
+                                 doc.getStyle("regular"));
+                StyleConstants.setComponent(doc.getStyle("button"), createColonyButton(colonyName));
+                doc.insertString(doc.getLength(), " ", doc.getStyle("button"));
+                doc.insertString(doc.getLength(), 
+                                 text.substring(index + "%colony%".length()),
+                                 doc.getStyle("regular"));
+            }
+        } catch(Exception e) {
+            logger.warning(e.toString());
+        }
+
+        return textPane;
+    }
+
+
+    private JButton createColonyButton(String colonyName) {
+
+        JButton button = new JButton(colonyName);
+        button.setMargin(new Insets(0,0,0,0));
+        button.setOpaque(false);
+        button.setForeground(LINK_COLOR);
+        button.setAlignmentY(0.8f);
+        button.setBorder(BorderFactory.createEmptyBorder());
+        button.setActionCommand(colonyName);
+        button.addActionListener(this);
+        return button;
+    }
+
+    /**
+     * This function analyses an event and calls the right methods to take care
+     * of the user's requests.
+     * 
+     * @param event The incoming ActionEvent.
+     */
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        String command = event.getActionCommand();
+        if (command.equals("-1")) {
+            super.actionPerformed(event);
+        } else {
+            getCanvas().showColonyPanel(colonies.get(command));
+        }
     }
 
 }
