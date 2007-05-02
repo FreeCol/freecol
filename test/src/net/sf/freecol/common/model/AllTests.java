454a473e944ef56c40782a4dfd0970f9d52bb648
coezbek <coezbek@users.sourceforge.net>
2007-05-02 15:41:10 +0000

    Added missing tests to suite.

--- test/src/net/sf/freecol/common/model/AllTests.java
+++ test/src/net/sf/freecol/common/model/AllTests.java
@@ -10,16 +10,19 @@ public class AllTests {
     public static final String LICENSE = "http://www.gnu.org/licenses/gpl.html";
 
     public static final String REVISION = "$Revision$";
-    
+
+
     public static Test suite() {
         TestSuite suite = new TestSuite("Test for net.sf.freecol.common");
         //$JUnit-BEGIN$
         suite.addTestSuite(TileTest.class);
+        suite.addTestSuite(PlayerTest.class);
+        suite.addTestSuite(BuildingTest.class);
         suite.addTestSuite(MapTest.class);
         suite.addTestSuite(UnitTest.class);
+        suite.addTestSuite(GoodsTest.class);
         suite.addTestSuite(ColonyProductionTest.class);
         suite.addTestSuite(GameTest.class);
-        suite.addTestSuite(BuildingTest.class);
         //$JUnit-END$
         return suite;
     }
