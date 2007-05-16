ddc90e3d3fd5c34d78a5ae41596b689e6e0f2e6d
kloune <kloune@users.sourceforge.net>
2007-05-16 22:19:36 +0000

    - Removes an exception that is thrown when a path is updated from the current tile with same destination for the unit.

--- src/net/sf/freecol/client/gui/GUI.java
+++ src/net/sf/freecol/client/gui/GUI.java
@@ -2130,6 +2130,8 @@ public final class GUI {
         } else {
             if (activeUnit.getDestination() instanceof Europe) {
                 currentPath = freeColClient.getGame().getMap().findPathToEurope(activeUnit, activeUnit.getTile());
+            } else if (activeUnit.getDestination().getTile() == activeUnit.getTile()) {
+                // No need to do anything as the unit has arrived, there is no path to be shown.
             } else {
                 currentPath = activeUnit.findPath(activeUnit.getDestination().getTile());
             }
