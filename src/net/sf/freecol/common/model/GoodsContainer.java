c28f0b40652fe8e09e5a8ec36e7ec091479f530c
sergiocambra <sergiocambra@users.sourceforge.net>
2007-06-15 19:38:49 +0000

    Import java.util.List

--- src/net/sf/freecol/common/model/GoodsContainer.java
+++ src/net/sf/freecol/common/model/GoodsContainer.java
@@ -4,6 +4,7 @@ package net.sf.freecol.common.model;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.xml.stream.XMLStreamException;
