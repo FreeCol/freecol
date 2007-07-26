425f6a6d4d1ee664669ec25782cb3f12eee1c10d
Stian Grenborgen <stiangre@users.sourceforge.net>
2007-07-26 09:44:57 +0000

    This fixes a bug causing a food image to be used instead of a fish image on ocean tiles.

--- src/net/sf/freecol/client/gui/ImageLibrary.java
+++ src/net/sf/freecol/client/gui/ImageLibrary.java
@@ -798,7 +798,7 @@ public final class ImageLibrary extends ImageProvider {
                 case Tile.ARCTIC:
                     return -1;
                 case Tile.OCEAN:
-                    return BONUS_FOOD;
+                    return BONUS_FISH;
                 case Tile.HIGH_SEAS:
                     return -1;
                 default:
