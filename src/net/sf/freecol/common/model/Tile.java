e918760b8744d6fe2a2b3c7feb48f1e996df067d
sergiocambra <sergiocambra@users.sourceforge.net>
2007-08-21 11:58:51 +0000

    Fix saving of missions

--- src/net/sf/freecol/common/model/Tile.java
+++ src/net/sf/freecol/common/model/Tile.java
@@ -2498,7 +2498,7 @@ break;
             }
             if (missionary != null) {
                 out.writeStartElement("missionary");
-                missionary.toXML(out, player, false, false);
+                missionary.toXML(out, player, showAll, toSavedGame;
                 out.writeEndElement();
             }
             if (hasResource()) {
