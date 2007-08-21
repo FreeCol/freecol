a0d01d47191c22caa7c2fae806a0299ad8181fc7
sergiocambra <sergiocambra@users.sourceforge.net>
2007-08-21 12:18:32 +0000

    revert last change

--- src/net/sf/freecol/common/model/FoundingFather.java
+++ src/net/sf/freecol/common/model/FoundingFather.java
@@ -1,24 +1,47 @@
 
 package net.sf.freecol.common.model;
 
+import java.util.Hashtable;
+import java.util.Map;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import net.sf.freecol.client.gui.i18n.Messages;
+import net.sf.freecol.common.util.Xml;
 
 /**
 * Represents one founding father to be contained in a Player object.
 * Stateful information is in the Player object.
 */
-public class FoundingFather {
+public class FoundingFather implements Abilities {
+
     public static final String  COPYRIGHT = "Copyright (C) 2003-2005 The FreeCol Team";
     public static final String  LICENSE = "http://www.gnu.org/licenses/gpl.html";
     public static final String  REVISION = "$Revision$";
     
+    /**
+     * Describe id here.
+     */
+    private String id;
+
+    private int[] weight = new int[4];
+
+    private int type;
+
+    /**
+     * Stores the abilities of this Type.
+     */
+    private Hashtable<String, Boolean> abilities = new Hashtable<String, Boolean>();    
+
+
     // Remember to update the list in "getWeight" when you add an effect to a founding father:
     public static final int NONE = -1,
                             ADAM_SMITH = 0,
                             JACOB_FUGGER = 1,
                             PETER_MINUIT = 2,
                             PETER_STUYVESANT = 3, 
-                            JAN_DE_WITT = 4, //TODO
+                            JAN_DE_WITT = 4,
                             FERDINAND_MAGELLAN = 5,
                             FRANCISCO_DE_CORONADO = 6,
                             HERNANDO_DE_SOTO = 7,
@@ -37,7 +60,7 @@ public class FoundingFather {
                             WILLIAM_BREWSTER = 20,
                             WILLIAM_PENN = 21,
                             FATHER_JEAN_DE_BREBEUF = 22,
-                            JUAN_DE_SEPULVEDA = 23,//TODO
+                            JUAN_DE_SEPULVEDA = 23,
                             BARTOLOME_DE_LAS_CASAS = 24,
 
                             FATHER_COUNT = 25;
@@ -49,7 +72,24 @@ public class FoundingFather {
                             RELIGIOUS = 4,
                             TYPE_COUNT = 5;
 
-    
+    /**
+     * Get the <code>Id</code> value.
+     *
+     * @return a <code>String</code> value
+     */
+    public final String getId() {
+        return id;
+    }
+
+    /**
+     * Set the <code>Id</code> value.
+     *
+     * @param newId The new Id value.
+     */
+    public final void setId(final String newId) {
+        this.id = newId;
+    }
+
     public static String getName(int foundingFather) {
         return getPrefix(foundingFather) + ".name";
     }
@@ -103,6 +143,10 @@ public class FoundingFather {
     }
 
 
+    public int getType() {
+        return type;
+    }
+
     public static int getType(int foundingFather) {
         switch (foundingFather) {
             case ADAM_SMITH: return TRADE;
@@ -151,8 +195,7 @@ public class FoundingFather {
 
     public static int getWeight(int foundingFather, int age) {
         // This is the list of the founding fathers without effects:
-        if (foundingFather == BENJAMIN_FRANKLIN ||
-            foundingFather == JUAN_DE_SEPULVEDA) {
+        if (foundingFather == BENJAMIN_FRANKLIN) {
             return 0;
         }
         if (age == 1) {
@@ -161,6 +204,18 @@ public class FoundingFather {
             return getWeight2(foundingFather);
         } else {
             return getWeight3(foundingFather);
+        }
+    }
+
+    public int getWeight(int age) {
+        switch(age) {
+        case 1:
+            return weight[1];
+        case 2:
+            return weight[2];
+        case 3:
+        default:
+            return weight[3];
         }
     }
     
@@ -261,4 +316,61 @@ public class FoundingFather {
                 throw new IllegalArgumentException("FoundingFather has invalid type.");
         }
     }
+
+    /**
+     * Returns true if this FoundingFather has the ability with the given ID.
+     *
+     * @param id a <code>String</code> value
+     * @return a <code>boolean</code> value
+     */
+    public boolean hasAbility(String id) {
+        return abilities.containsKey(id) && abilities.get(id);
+    }
+
+    /**
+     * Sets the ability to newValue;
+     *
+     * @param id a <code>String</code> value
+     * @param newValue a <code>boolean</code> value
+     */
+    public void setAbility(String id, boolean newValue) {
+        abilities.put(id, newValue);
+    }
+
+
+    public void readFromXmlElement(Node xml, Map<String, GoodsType> goodsTypeByRef) {
+
+        id = Xml.attribute(xml, "id");
+        String typeString = Xml.attribute(xml, "type");
+        if ("trade".equals(typeString)) {
+            type = TRADE;
+        } else if ("exploration".equals(typeString)) {
+            type = EXPLORATION;
+        } else if ("military".equals(typeString)) {
+            type = MILITARY;
+        } else if ("political".equals(typeString)) {
+            type = POLITICAL;
+        } else if ("religious".equals(typeString)) {
+            type = RELIGIOUS;
+        } else {
+            throw new IllegalArgumentException("FoundingFather has unknown type " + typeString);
+        }                           
+
+        weight[1] = Xml.intAttribute(xml, "weight1");
+        weight[2] = Xml.intAttribute(xml, "weight2");
+        weight[3] = Xml.intAttribute(xml, "weight3");
+
+        Xml.Method method = new Xml.Method() {
+                public void invokeOn(Node node) {
+                    if ("ability".equals(node.getNodeName())) {
+                        String abilityId = Xml.attribute(node, "id");
+                        boolean value = Xml.booleanAttribute(node, "value");
+                        setAbility(abilityId, value);
+                    }
+                }
+            };
+        Xml.forEachChild(xml, method);
+
+    }
+
 }
