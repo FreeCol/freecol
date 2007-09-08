32864b16a8a0df1eb7670eefa7dcabdfc80180d3
Michael Vehrs <burschik@users.sourceforge.net>
2007-09-08 15:26:25 +0000

    Added missing classes.

---
+++ src/net/sf/freecol/client/gui/panel/AdvantageCellEditor.java
@@ -0,0 +1,33 @@
+
+package net.sf.freecol.client.gui.panel;
+
+import java.util.Vector;
+
+import javax.swing.DefaultCellEditor;
+import javax.swing.JComboBox;
+
+import net.sf.freecol.FreeCol;
+import net.sf.freecol.common.model.NationType;
+import net.sf.freecol.common.model.Player;
+
+/**
+* A table cell editor that can be used to select a nation.
+*/
+public final class AdvantageCellEditor extends DefaultCellEditor {
+
+    public static final String  COPYRIGHT = "Copyright (C) 2003-2005 The FreeCol Team";
+    public static final String  LICENSE = "http://www.gnu.org/licenses/gpl.html";
+    public static final String  REVISION = "$Revision$";
+
+    
+    /**
+    * A standard constructor.
+    */
+    public AdvantageCellEditor() {
+        super(new JComboBox(new Vector(FreeCol.getSpecification().getClassicNationTypes())));
+    }
+    
+    public Object getCellEditorValue() {
+        return (NationType) ((JComboBox) getComponent()).getSelectedItem();
+    }
+}
---
+++ src/net/sf/freecol/client/gui/panel/AdvantageCellRenderer.java
@@ -0,0 +1,109 @@
+
+package net.sf.freecol.client.gui.panel;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.util.List;
+import java.util.Vector;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+import net.sf.freecol.FreeCol;
+import net.sf.freecol.common.model.EuropeanNationType;
+import net.sf.freecol.common.model.IndianNationType;
+import net.sf.freecol.common.model.Nation;
+import net.sf.freecol.common.model.Player;
+
+/**
+* A table cell renderer that should be used to display the chosen nation in a table.
+* It is being used in the players table (StartGamePanel).
+*/
+public final class AdvantageCellRenderer implements TableCellRenderer {
+
+    public static final String  COPYRIGHT = "Copyright (C) 2003-2007 The FreeCol Team";
+    public static final String  LICENSE = "http://www.gnu.org/licenses/gpl.html";
+    public static final String  REVISION = "$Revision$";
+
+    private static Vector<EuropeanNationType> europeans = 
+        new Vector<EuropeanNationType>(FreeCol.getSpecification().getClassicNationTypes());
+    private static final JComboBox standardNationsComboBox = new JComboBox(europeans);
+
+    private List<Player> players;
+    private Player thisPlayer;
+
+    private static final JComboBox comboBox = new JComboBox();
+
+    /**
+    * The default constructor.
+    */
+    public AdvantageCellRenderer() {
+    }
+
+
+    /**
+    * Gives this table model the data that is being used in the table.
+    *
+    * @param players The players that should be rendered in the table.
+    * @param owningPlayer The player running the client that is displaying the table.
+    */
+    public void setData(List<Player> players, Player owningPlayer) {
+        this.players = players;
+        thisPlayer = owningPlayer;
+    }
+
+    private Player getPlayer(int i) {
+        if (i == 0) {
+            return thisPlayer;
+        } else if (players.get(i) == thisPlayer) {
+            return players.get(0);
+        } else {
+            return players.get(i);
+        }
+    }
+
+    /**
+    * Returns the component used to render the cell's value.
+    * @param table The table whose cell needs to be rendered.
+    * @param value The value of the cell being rendered.
+    * @param hasFocus Indicates whether or not the cell in question has focus.
+    * @param row The row index of the cell that is being rendered.
+    * @param column The column index of the cell that is being rendered.
+    * @return The component used to render the cell's value.
+    */
+    public Component getTableCellRendererComponent(JTable table, Object value,
+            boolean isSelected, boolean hasFocus, int row, int column) {
+
+        Player player = getPlayer(row);
+
+        JComboBox component;
+        component = standardNationsComboBox;
+        /*
+        if (player.isEuropean()) {
+
+        } else {
+            component = comboBox;
+        }
+        */
+
+        if (player.isReady()) {
+            component.setForeground(Color.GRAY);
+        } else {
+            component.setForeground(table.getForeground());
+        }
+        component.setBackground(table.getBackground());
+
+        int index = player.getIndex();
+        if (player.isIndian()) {
+            index -= FreeCol.getSpecification().getEuropeanNationTypes().size();
+        } else if (player.isREF()) {
+            index -= FreeCol.getSpecification().getEuropeanNationTypes().size();
+            index -= FreeCol.getSpecification().getIndianNationTypes().size();
+        }        
+        component.setSelectedIndex(index);
+
+        return component;
+    }
+}
