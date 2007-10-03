f0b8d54e38319d2995bdf0697ee41a92dcc257e0
Michael Vehrs <burschik@users.sourceforge.net>
2007-10-03 08:12:48 +0000

    Added more tests.

--- test/src/net/sf/freecol/common/model/ModifierTest.java
+++ test/src/net/sf/freecol/common/model/ModifierTest.java
@@ -32,6 +32,42 @@ public class ModifierTest extends FreeColTestCase {
 
     }
 
+    public void testCombineAdditiveModifiers() {
+
+        Modifier modifier1 = new Modifier("test1", 3, Modifier.ADDITIVE);
+        Modifier modifier2 = new Modifier("test2", 4, Modifier.ADDITIVE);
+        modifier1.combine(modifier2);
+        assertTrue(modifier1.getType() == Modifier.ADDITIVE);
+        assertTrue(modifier1.applyTo(1) == 1 + 3 + 4);
+        assertTrue(modifier1.getModifiers().size() == 2);
+        assertTrue(modifier1.getModifiers().get(1) == modifier2);
+
+    }
+
+    public void testCombineMultiplicativeModifiers() {
+
+        Modifier modifier1 = new Modifier("test1", 3, Modifier.MULTIPLICATIVE);
+        Modifier modifier2 = new Modifier("test2", 4, Modifier.MULTIPLICATIVE);
+        modifier1.combine(modifier2);
+        assertTrue(modifier1.getType() == Modifier.MULTIPLICATIVE);
+        assertTrue(modifier1.applyTo(2) == 2 * 3 * 4);
+        assertTrue(modifier1.getModifiers().size() == 2);
+        assertTrue(modifier1.getModifiers().get(1) == modifier2);
+
+    }
+
+    public void testCombinePercentageModifiers() {
+
+        Modifier modifier1 = new Modifier("test1", 3, Modifier.PERCENTAGE);
+        Modifier modifier2 = new Modifier("test2", 4, Modifier.PERCENTAGE);
+        modifier1.combine(modifier2);
+        assertTrue(modifier1.getType() == Modifier.PERCENTAGE);
+        assertTrue(modifier1.applyTo(100) == 107f);
+        assertTrue(modifier1.getModifiers().size() == 2);
+        assertTrue(modifier1.getModifiers().get(1) == modifier2);
+
+    }
+
     public void testCombinedModifier() {
         
         Modifier modifier1 = new Modifier("test1", 3, Modifier.ADDITIVE);
@@ -39,17 +75,20 @@ public class ModifierTest extends FreeColTestCase {
         Modifier modifier3 = new Modifier("test3", 50, Modifier.PERCENTAGE);
 
         modifier1.combine(modifier2);
+        assertTrue(modifier1.getType() == Modifier.COMBINED);
         assertTrue(modifier1.applyTo(1) == (1 + 3) * 1.5f);
         assertTrue(modifier1.getModifiers().size() == 2);
         assertTrue(modifier1.getModifiers().get(1) == modifier2);
 
         modifier1.combine(modifier3);
+        assertTrue(modifier1.getType() == Modifier.COMBINED);
         assertTrue(modifier1.applyTo(1) == ((1 + 3) * 1.5f) * 1.5f);
         assertTrue(modifier1.getModifiers().size() == 3);
         assertTrue(modifier1.getModifiers().get(1) == modifier2);
         assertTrue(modifier1.getModifiers().get(2) == modifier3);
 
         modifier2.combine(modifier3);
+        assertTrue(modifier2.getType() == Modifier.COMBINED);
         assertTrue(modifier2.applyTo(10) == 10 * 1.5f * 1.5f);
         assertTrue(modifier2.getModifiers().size() == 2);
         assertTrue(modifier2.getModifiers().get(1) == modifier3);
