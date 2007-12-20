6fe6076d36e797b742aac5d98d6433e39dd8cc10
Michael Vehrs <burschik@users.sourceforge.net>
2007-12-20 17:44:27 +0000

    Implemented FR#[ 1821270 ] Indian report could include available skills of settlements.

--- src/net/sf/freecol/client/gui/panel/ReportIndianPanel.java
+++ src/net/sf/freecol/client/gui/panel/ReportIndianPanel.java
@@ -23,21 +23,26 @@ package net.sf.freecol.client.gui.panel;
 
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
-import java.util.Iterator;
 
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 import net.sf.freecol.client.gui.Canvas;
 import net.sf.freecol.client.gui.i18n.Messages;
+import net.sf.freecol.common.model.IndianNationType;
+import net.sf.freecol.common.model.IndianSettlement;
 import net.sf.freecol.common.model.Player;
+import net.sf.freecol.common.model.UnitType;
 
+import cz.autel.dmi.HIGLayout;
 
 /**
  * This panel displays the Foreign Affairs Report.
  */
 public final class ReportIndianPanel extends ReportPanel implements ActionListener {
 
+    public static final int EXTRA_ROWS = 10;
     
     /**
      * The constructor that will add the items to this panel.
@@ -52,20 +57,23 @@ public final class ReportIndianPanel extends ReportPanel implements ActionListen
      */
     public void initialize() {
         Player player = getCanvas().getClient().getMyPlayer();
-        Iterator<Player> opponents = getCanvas().getClient().getGame().getPlayers().iterator();
         // Display Panel
         reportPanel.removeAll();
         reportPanel.setLayout(new GridLayout(8, 1));
-        while (opponents.hasNext()) {
-            buildIndianAdvisorLabel(player, opponents.next());
+        for (Player opponent : getCanvas().getClient().getGame().getPlayers()) {
+            buildIndianAdvisorPanel(player, opponent);
         }
         reportPanel.doLayout();
     }
 
     /**
-     * 
+     * Describe <code>buildIndianAdvisorPanel</code> method here.
+     *
+     * @param player a <code>Player</code> value
+     * @param opponent a <code>Player</code> value
+     * @return a <code>JPanel</code> value
      */
-    private void buildIndianAdvisorLabel(Player player, Player opponent) {
+    private void buildIndianAdvisorPanel(Player player, Player opponent) {
         if (opponent.isEuropean() || opponent.isREF()) {
           return;
         }
@@ -75,19 +83,68 @@ public final class ReportIndianPanel extends ReportPanel implements ActionListen
         if (!player.hasContacted(opponent)) {
           return;
         }
-        String report = "<html><p align=center><b>" +
-                        opponent.getNationAsString() + "</b>";
 
-        int settlementCount = opponent.getSettlements().size();
-        report += "<p>" + Messages.message("report.indian.knownCamps", 
-                "%number%", String.valueOf(settlementCount));
-        String tensionString = opponent.getTension(player).toString();
-        report += "<p>" + Messages.message("tension") + ": " + tensionString;
-        report += "</html>";
-        JLabel label;
-        label = new JLabel(report);
-        label.setVerticalAlignment(SwingConstants.TOP);
-        label.setVerticalTextPosition(SwingConstants.TOP);
-        reportPanel.add(label);
+        int numberOfSettlements = opponent.getSettlements().size();
+        int heights[] = new int[2 * (numberOfSettlements + EXTRA_ROWS)];
+        int widths[] = new int[] { 0, 10, 0 };
+        int labelColumn = 1;
+        int valueColumn = 3;
+        int row = 1;
+
+        for (int index = 1; index < heights.length; index += 2) {
+            heights[index] = 3;
+        }
+        heights[EXTRA_ROWS - 1] = 16;
+        heights[EXTRA_ROWS + 1] = 6;
+
+        JPanel result = new JPanel(new HIGLayout(widths, heights));
+
+        result.add(new JLabel(Messages.message("report.indian.nameOfTribe")),
+                   higConst.rc(row, labelColumn));
+        result.add(new JLabel(opponent.getNationAsString()),
+                   higConst.rc(row, valueColumn));
+        row += 2;
+        result.add(new JLabel(Messages.message("report.indian.chieftain")),
+                   higConst.rc(row, labelColumn));
+        result.add(new JLabel(opponent.getName()),
+                   higConst.rc(row, valueColumn));
+        row += 2;
+        result.add(new JLabel(Messages.message("report.indian.typeOfSettlements")),
+                   higConst.rc(row, labelColumn));
+        result.add(new JLabel(String.valueOf(((IndianNationType) opponent.getNationType()).getTypeOfSettlement())),
+                   higConst.rc(row, valueColumn, "r"));
+        row += 2;
+        result.add(new JLabel(Messages.message("report.indian.numberOfSettlements")),
+                   higConst.rc(row, labelColumn));
+        result.add(new JLabel(String.valueOf(numberOfSettlements)),
+                   higConst.rc(row, valueColumn, "r"));
+        row += 2;
+        result.add(new JLabel(Messages.message("report.indian.tension")),
+                   higConst.rc(row, labelColumn));
+        result.add(new JLabel(opponent.getTension(player).toString()),
+                   higConst.rc(row, valueColumn));
+        row += 2;
+        result.add(new JLabel(Messages.message("report.indian.location")),
+                   higConst.rc(row, labelColumn));
+        result.add(new JLabel(Messages.message("report.indian.skillTaught")),
+                   higConst.rc(row, valueColumn));
+        row += 2;
+
+        for (IndianSettlement settlement : opponent.getIndianSettlements()) {
+            String locationName = settlement.getLocationName() + 
+                " (" + settlement.getTile().getX() + ", " +
+                settlement.getTile().getY() + ")";
+            UnitType skillType = settlement.getLearnableSkill();
+            String skill = Messages.message("indianSettlement.skillUnknown");
+            if (skillType != null) {
+                skill = skillType.getName();
+            }
+            result.add(new JLabel(locationName),
+                       higConst.rc(row, labelColumn));
+            result.add(new JLabel(skill),
+                       higConst.rc(row, valueColumn));
+            row += 2;
+        }
+        add(result);
     }
 }
