89d3aea32d3f5ecf19ea68d1d3298a5b09af3a32
olivierschmitt <olivierschmitt@users.sourceforge.net>
2007-12-30 23:43:54 +0000

    Improved ModelMessageTest

--- test/src/net/sf/freecol/common/model/ModelMessageTest.java
+++ test/src/net/sf/freecol/common/model/ModelMessageTest.java
@@ -27,37 +27,42 @@ import net.sf.freecol.util.test.FreeColTestCase;
 public class ModelMessageTest extends FreeColTestCase {
 
     public void testHashCode() {
-    	
-    	GoodsType cotton = FreeCol.getSpecification().getGoodsType("model.goods.Cotton");
-    	
-        ModelMessage mm1 = new ModelMessage(getGame(), "buildColony.landLocked", null, ModelMessage.MISSING_GOODS,
+    
+        Game game = getGame();
+        Colony colony = getStandardColony(1);
+        GoodsType cotton = FreeCol.getSpecification().getGoodsType("model.goods.Cotton");
+        
+        ModelMessage mm1 = new ModelMessage(colony, "buildColony.landLocked", null, ModelMessage.MISSING_GOODS,
                 cotton);
-        ModelMessage mm2 = new ModelMessage(getGame(), "buildColony.landLocked", null, ModelMessage.MISSING_GOODS,
+        ModelMessage mm2 = new ModelMessage(colony, "buildColony.landLocked", null, ModelMessage.MISSING_GOODS,
                 cotton);
         assertEquals(mm1, mm2);
         assertEquals(mm1.hashCode(), mm2.hashCode());
         
-        ModelMessage mm3 = new ModelMessage(getGame(), "buildColony.landLocked", null, ModelMessage.MISSING_GOODS,
-            cotton);
-        ModelMessage mm4 = new ModelMessage(getGame(), "buildColony.landLocked", null, ModelMessage.MISSING_GOODS,
-            cotton);
+        ModelMessage mm3 = new ModelMessage(colony, "buildColony.landLocked", null, ModelMessage.MISSING_GOODS,
+                cotton);
+        ModelMessage mm4 = new ModelMessage(colony, "buildColony.landLocked", null, ModelMessage.MISSING_GOODS,
+                cotton);
         assertNotSame(mm3, mm4);
         assertNotSame(mm3.hashCode(), mm4.hashCode());
     }
     
     public void testModelMapSet2() {
-    	
-    	GoodsType cotton = FreeCol.getSpecification().getGoodsType("model.goods.Cotton");
-    	
-        ModelMessage mm1 = new ModelMessage(getGame(), "model.building.warehouseSoonFull",
+        
+        Game game = getGame();
+        Player player = game.getPlayer("model.nation.dutch");
+        Colony colony = getStandardColony(1);
+        GoodsType cotton = FreeCol.getSpecification().getGoodsType("model.goods.Cotton");
+        
+        ModelMessage mm1 = new ModelMessage(colony, "model.building.warehouseSoonFull",
                                     new String [][] {{"%goods%", cotton.getName()},
-                                                     {"%colony%", getName()},
+                                                     {"%colony%", colony.getName()},
                                                      {"%amount%", String.valueOf(10)}},
                                     ModelMessage.WAREHOUSE_CAPACITY,
                                     cotton);
-        ModelMessage mm2 = new ModelMessage(getGame(), "model.building.warehouseSoonFull",
+        ModelMessage mm2 = new ModelMessage(colony, "model.building.warehouseSoonFull",
                 new String [][] {{"%goods%", cotton.getName()},
-                                 {"%colony%", getName()},
+                                 {"%colony%", colony.getName()},
                                  {"%amount%", String.valueOf(10)}},
                 ModelMessage.WAREHOUSE_CAPACITY,
                 cotton);
