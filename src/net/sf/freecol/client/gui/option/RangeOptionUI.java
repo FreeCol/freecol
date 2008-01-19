e64f5071a98a61b59a13e831a70f6c50ef0b84e2
olivierschmitt <olivierschmitt@users.sourceforge.net>
2008-01-19 06:36:01 +0000

    Added new RangeOption for displaying numeric options with a JSlider

---
+++ src/net/sf/freecol/client/gui/option/RangeOptionUI.java
@@ -0,0 +1,123 @@
+/**
+ *  Copyright (C) 2002-2007  The FreeCol Team
+ *
+ *  This file is part of FreeCol.
+ *
+ *  FreeCol is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  FreeCol is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with FreeCol.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+package net.sf.freecol.client.gui.option;
+
+import java.awt.FlowLayout;
+import java.awt.Dimension;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.logging.Logger;
+import java.util.Hashtable;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+
+import net.sf.freecol.common.option.Option;
+import net.sf.freecol.common.option.RangeOption;
+
+
+
+/**
+* This class provides visualization for an {@link RangeOption}. In order to
+* enable values to be both seen and changed.
+*/
+public final class RangeOptionUI extends JPanel implements OptionUpdater, PropertyChangeListener {
+    @SuppressWarnings("unused")
+    private static final Logger logger = Logger.getLogger(RangeOptionUI.class.getName());
+
+
+    private final RangeOption option;
+    
+    private final JSlider slider;
+
+
+    /**
+    * Creates a new <code>RangeOptionUI</code> for the given <code>RangeOption</code>.
+    * @param option The <code>RangeOption</code> to make a user interface for.
+    */
+    public RangeOptionUI(RangeOption option, boolean editable) {
+        super(new FlowLayout(FlowLayout.LEFT));
+
+        this.option = option;
+
+        String name = option.getName();
+        String description = option.getShortDescription();
+        JLabel label = new JLabel(name, JLabel.LEFT);
+        label.setToolTipText((description != null) ? description : name);
+        add(label);
+
+        String[] strings = option.getOptions();
+
+        slider = new JSlider(JSlider.HORIZONTAL, 0, strings.length-1, option.getValue());
+        Hashtable<Integer,JComponent> labels = new Hashtable<Integer,JComponent>();
+        for (int i=0; i<strings.length; i++) {
+            labels.put(new Integer(i), new JLabel(strings[i]));
+        }
+        slider.setLabelTable(labels);
+        slider.setValue(option.getValue());
+        slider.setPaintLabels(true);
+        slider.setMajorTickSpacing(1);
+        slider.setExtent(0);
+        slider.setPaintTicks(true);
+        slider.setSnapToTicks(true);
+        slider.setPreferredSize(new Dimension(500,50));
+        add(slider);
+        
+        slider.setEnabled(editable);
+
+        option.addPropertyChangeListener(this);
+        setOpaque(false);
+    }
+
+    
+    /**
+     * Unregister <code>PropertyChangeListener</code>s.
+     */
+    public void unregister() {
+        option.removePropertyChangeListener(this);    
+    }
+    
+    /**
+     * Updates this UI with the new data from the option.
+     * @param event The event.
+     */
+    public void propertyChange(PropertyChangeEvent event) {
+        if (event.getPropertyName().equals("value")) {
+            slider.setValue(((Integer) event.getNewValue()).intValue());
+        }
+    }
+    
+    /**
+     * Updates the value of the {@link Option} this object keeps.
+     */
+    public void updateOption() {
+        option.setValue(slider.getValue());
+    }
+
+    /**
+     * Reset with the value from the option.
+     */
+    public void reset() {
+        slider.setValue(option.getValue());
+    }
+}
---
+++ src/net/sf/freecol/common/option/RangeOption.java
@@ -0,0 +1,215 @@
+/**
+ *  Copyright (C) 2002-2007  The FreeCol Team
+ *
+ *  This file is part of FreeCol.
+ *
+ *  FreeCol is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  FreeCol is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with FreeCol.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+package net.sf.freecol.common.option;
+
+import java.util.logging.Logger;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+
+import net.sf.freecol.client.gui.i18n.Messages;
+
+
+/**
+ * Represents an option where the valid choice is an integer and the
+ * choices are represented by strings. In general, these strings are
+ * localized by looking up the key of the choice, which consists of
+ * the id of the AbstractObject followed by a "." followed by the
+ * value of the option string. 
+ * RangeOption differs from SelectOption, as the value being selected
+ * represents a numeric measurement, defined by a bounded range of comparable values
+ */
+public class RangeOption extends AbstractOption {
+    @SuppressWarnings("unused")
+    private static Logger logger = Logger.getLogger(RangeOption.class.getName());
+
+
+
+    protected int value;
+    protected String[] options;
+
+    
+    /**
+     * Creates a new <code>RangeOption</code>.
+     *
+     * @param id The identifier for this option. This is used when the object should be
+     *           found in an {@link OptionGroup}.
+     * @param options All possible values.
+     * @param defaultOption The index of the default value.
+     */
+    public RangeOption(String id, String[] options, int defaultOption) {
+        this(id, null, options, defaultOption, false);
+    }
+    
+    /**
+     * Creates a new <code>RangeOption</code>.
+     *
+     * @param id The identifier for this option. This is used when the object should be
+     *           found in an {@link OptionGroup}.
+     * @param optionGroup The OptionGroup this Option belongs to.
+     * @param options All possible values.
+     * @param defaultOption The index of the default value.
+     */
+    public RangeOption(String id, OptionGroup optionGroup, String[] options, int defaultOption) {
+        this(id, optionGroup, options, defaultOption, false);
+    }
+    
+    /**
+     * Creates a new <code>RangeOption</code>.
+     *
+     * @param id The identifier for this option. This is used when the object should be
+     *           found in an {@link OptionGroup}.
+     * @param optionGroup The OptionGroup this Option belongs to.
+     * @param options All possible values.
+     * @param defaultOption The index of the default value.
+     * @param doNotLocalize Suppress the default localization of options.
+     */
+    public RangeOption(String id, OptionGroup optionGroup, String[] options, int defaultOption, boolean doNotLocalize) {
+        super(id, optionGroup);
+
+        if (doNotLocalize) {
+            this.options = options;
+        } else {
+            String[] localized = new String[options.length];
+            for (int i = 0; i < options.length; i++) {
+                localized[i] = Messages.message(getGroup() + id + "." + options[i]);
+            }        
+            this.options = localized;
+        }
+        
+        this.value = defaultOption;
+    }
+
+    /**
+     * Gets the current value of this <code>RangeOption</code>.
+     * @return The value.
+     */
+    public int getValue() {
+        return value;
+    }
+ 
+    /**
+     * Sets the value of this <code>RangeOption</code>.
+     * @param value The value to be set.
+     */
+    public void setValue(int value) {
+        final int oldValue = this.value;
+        this.value = value;
+        
+        if (value != oldValue) {
+            firePropertyChange("value", Integer.valueOf(oldValue), Integer.valueOf(value));
+        }
+    }
+
+    /**
+     * Gets the current options of this <code>RangeOption</code>.
+     * @return The options.
+     */
+    public String[] getOptions() {
+        return options;
+    }
+
+    
+    /**
+     * Sets the options of this <code>RangeOption</code>.
+     * @param options The options to be set.
+     */
+    protected void setOptions(String[] options) {
+        this.options = options;
+    }
+
+    /**
+     * Gets a <code>String</code> representation of the
+     * current value.
+     * 
+     * This method can be overwritten by subclasses to allow
+     * a custom save value, since this method is used by
+     * {@link #toXML(XMLStreamWriter)}.
+     * 
+     * @return The String value of the Integer.
+     * @see #setValue(String)
+     */
+    protected String getStringValue() {
+        return Integer.toString(value);
+    }
+    
+    /**
+     * Converts the given <code>String</code> to an Integer
+     * and calls {@link #setValue(int)}. 
+     * 
+     * <br><br>
+     * 
+     * This method can be overwritten by subclasses to allow
+     * a custom save value, since this method is used by
+     * {@link #readFromXML(XMLStreamReader)}.
+     * 
+     * @param value The String value of the Integer.
+     * @see #getStringValue()
+     */
+    protected void setValue(String value) {
+        setValue(Integer.parseInt(value));
+    }
+    
+    /**
+     * This method writes an XML-representation of this object to
+     * the given stream.
+     *  
+     * @param out The target stream.
+     * @throws XMLStreamException if there are any problems writing
+     *      to the stream.
+     */
+    protected void toXMLImpl(XMLStreamWriter out) throws XMLStreamException {
+        // Start element:
+        out.writeStartElement(getId());
+
+        out.writeAttribute("value", getStringValue());
+
+        out.writeEndElement();
+    }
+    
+    /**
+     * Initialize this object from an XML-representation of this object.
+     * @param in The input stream with the XML.
+     * @throws XMLStreamException if a problem was encountered
+     *      during parsing.
+     */
+    protected void readFromXMLImpl(XMLStreamReader in) throws XMLStreamException {
+        final int oldValue = this.value;
+        
+        setValue(in.getAttributeValue(null, "value"));
+        in.nextTag();
+        
+        if (value != oldValue) {
+            firePropertyChange("value", Integer.valueOf(oldValue), Integer.valueOf(value));
+        }
+    }
+
+
+    /**
+     * Gets the tag name of the root element representing this object.
+     * @return "rangeOption".
+     */
+    public static String getXMLElementTagName() {
+        return "rangeOption";
+    }
+
+}
