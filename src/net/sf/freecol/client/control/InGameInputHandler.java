865fd38fb3e81041b37c4ac4e789c495cb0266e5
olivierschmitt <olivierschmitt@users.sourceforge.net>
2008-05-05 00:40:45 +0000

    Fixed strange bug with founding fathers

--- src/net/sf/freecol/client/control/InGameInputHandler.java
+++ src/net/sf/freecol/client/control/InGameInputHandler.java
@@ -586,7 +586,7 @@ public final class InGameInputHandler extends InputHandler {
         final List<FoundingFather> possibleFoundingFathers = new ArrayList<FoundingFather>();
         for (FoundingFatherType type : FoundingFatherType.values()) {
             String id = element.getAttribute(type.toString());
-            if (id != null) {
+            if (id != null && id != "") {
                 possibleFoundingFathers.add(FreeCol.getSpecification().getFoundingFather(id));
             }
         }
