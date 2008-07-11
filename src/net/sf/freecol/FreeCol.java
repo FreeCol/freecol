dd7eb1943721bbbe8a1d87478fd45ad32f1b7ff9
Stian Grenborgen <stiangre@users.sourceforge.net>
2008-07-11 23:41:24 +0000

    Updating version number.

--- src/net/sf/freecol/FreeCol.java
+++ src/net/sf/freecol/FreeCol.java
@@ -91,7 +91,7 @@ public final class FreeCol {
      */
     private static Specification specification;
 
-    private static final String FREECOL_VERSION = "test";
+    private static final String FREECOL_VERSION = "0.8.0-alpha";
     private static String FREECOL_REVISION;
     
     private static final String MIN_JDK_VERSION = "1.5";
