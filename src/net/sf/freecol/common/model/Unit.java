cd45ad7b197c1ca9a8f99480be736449f0d98616
Michael Vehrs <burschik@users.sourceforge.net>
2008-07-24 15:16:50 +0000

    Make newLocation more robust.

--- src/net/sf/freecol/common/model/Unit.java
+++ src/net/sf/freecol/common/model/Unit.java
@@ -3602,31 +3602,26 @@ public class Unit extends FreeColGameObject implements Locatable, Location, Owna
         return "unit";
     }
 
-    /**
-     * ATTENTION!!! Longer prefixes must be checked first! For
-     * example, ColonyTile must be checked before Colony is checked!
-     */
     private Location newLocation(Game game, String locationString) {
-        Location loc = null;
-        if (locationString.startsWith(Tile.getXMLElementTagName())) {
-            loc = new Tile(game, locationString);
-        } else if (locationString.startsWith(ColonyTile.getXMLElementTagName())) {
-            loc = new ColonyTile(game, locationString);
-        } else if (locationString.startsWith(Colony.getXMLElementTagName())) {
-            loc = new Colony(game, locationString);
-        } else if (locationString.startsWith(IndianSettlement.getXMLElementTagName())) {
-            loc = new IndianSettlement(game, locationString);
-        } else if (locationString.startsWith(Europe.getXMLElementTagName())) {
-            loc = new Europe(game, locationString);
-        } else if (locationString.startsWith(Building.getXMLElementTagName())) {
-            loc = new Building(game, locationString);
-        } else if (locationString.startsWith(Unit.getXMLElementTagName())) {
-            loc = new Unit(game, locationString);
+        String XMLElementTag = locationString.substring(0, locationString.indexOf(':'));
+        if (XMLElementTag.equals(Tile.getXMLElementTagName())) {
+            return new Tile(game, locationString);
+        } else if (XMLElementTag.equals(ColonyTile.getXMLElementTagName())) {
+            return new ColonyTile(game, locationString);
+        } else if (XMLElementTag.equals(Colony.getXMLElementTagName())) {
+            return new Colony(game, locationString);
+        } else if (XMLElementTag.equals(IndianSettlement.getXMLElementTagName())) {
+            return new IndianSettlement(game, locationString);
+        } else if (XMLElementTag.equals(Europe.getXMLElementTagName())) {
+            return new Europe(game, locationString);
+        } else if (XMLElementTag.equals(Building.getXMLElementTagName())) {
+            return new Building(game, locationString);
+        } else if (XMLElementTag.equals(Unit.getXMLElementTagName())) {
+            return new Unit(game, locationString);
         } else {
             logger.warning("Unknown type of Location: " + locationString);
-            loc = new Tile(game, locationString);
+            return new Tile(game, locationString);
         }
-        return loc;
     }
 
     /**
