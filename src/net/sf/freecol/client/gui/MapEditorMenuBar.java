5c248b6bc3dfa8269f1d1dc269387c71ec09c8b0
Michael Vehrs <burschik@users.sourceforge.net>
2008-12-26 12:13:12 +0000

    Add shortcut to start game from editor menu.

--- src/net/sf/freecol/client/gui/MapEditorMenuBar.java
+++ src/net/sf/freecol/client/gui/MapEditorMenuBar.java
@@ -19,11 +19,15 @@
 
 package net.sf.freecol.client.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.File;
 import java.util.logging.Logger;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 import net.sf.freecol.FreeCol;
 import net.sf.freecol.client.FreeColClient;
@@ -56,6 +60,8 @@ import net.sf.freecol.client.gui.action.ZoomInAction;
 import net.sf.freecol.client.gui.action.ZoomOutAction;
 import net.sf.freecol.client.gui.i18n.Messages;
 import net.sf.freecol.client.gui.menu.DebugMenu;
+import net.sf.freecol.common.option.FileOption;
+import net.sf.freecol.server.generator.MapGeneratorOptions;
 
 /**
  * The menu bar used when running in editor mode.
@@ -69,12 +75,9 @@ import net.sf.freecol.client.gui.menu.DebugMenu;
  * @see InGameMenuBar
  */
 public class MapEditorMenuBar extends FreeColMenuBar {
+
     @SuppressWarnings("unused")
     private static final Logger logger = Logger.getLogger(MapEditorMenuBar.class.getName());
-
-
-
-
 
     /**
      * Creates a new <code>MapEditorMenuBar</code>. This menu bar will include
@@ -121,7 +124,21 @@ public class MapEditorMenuBar extends FreeColMenuBar {
 
         menu.add(getMenuItem(OpenAction.id));
         menu.add(getMenuItem(SaveAction.id));
-
+        JMenuItem playItem = new JMenuItem(Messages.message("startGame"));
+        playItem.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    File saveGameFile = new File(FreeCol.getAutosaveDirectory(), "tempMap");
+                    MapGeneratorOptions options = freeColClient.getPreGameController().getMapGeneratorOptions();
+                    if (options == null) {
+                        options = new MapGeneratorOptions();
+                    }
+                    options.setFile(MapGeneratorOptions.IMPORT_FILE, saveGameFile);                        
+                    freeColClient.getMapEditorController().saveGame(saveGameFile);
+                    freeColClient.getPreGameController().sendMapGeneratorOptions();
+                    freeColClient.getCanvas().newGame();
+                }
+            });
+        menu.add(playItem);
         menu.addSeparator();
 
         menu.add(getMenuItem(PreferencesAction.id));
