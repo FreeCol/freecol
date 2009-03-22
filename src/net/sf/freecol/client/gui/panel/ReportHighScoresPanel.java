df16bc2e440120265b6e7c4bac907e6dcdffe9a3
Michael Vehrs <burschik@users.sourceforge.net>
2009-03-22 15:13:44 +0000

    Use MigLayout.

--- src/net/sf/freecol/client/gui/panel/ReportHighScoresPanel.java
+++ src/net/sf/freecol/client/gui/panel/ReportHighScoresPanel.java
@@ -35,7 +35,7 @@ import net.sf.freecol.common.model.Turn;
 
 import org.w3c.dom.Element;
 
-import cz.autel.dmi.HIGLayout;
+import net.miginfocom.swing.MigLayout;
 
 /**
  * This panel displays the Foreign Affairs Report.
@@ -58,16 +58,7 @@ public final class ReportHighScoresPanel extends ReportPanel {
         Element report = client.getInGameController().getHighScores();
         int number = report.getChildNodes().getLength();
         
-        int[] widths1 = new int[] { 0, 30, 0 };
-        int[] heights1 = new int[4 * number - 1];
-        for (int index = 1; index < heights1.length; index += 2) {
-            heights1[index] = margin;
-        }
-
-        reportPanel.setLayout(new HIGLayout(widths1, heights1));
-        int reportRow = 1;
-        int scoreColumn = 1;
-        int panelColumn = 3;
+        reportPanel.setLayout(new MigLayout("wrap 3, gapx 30", "[][][align right]", ""));
 
         for (int i = 0; i < number; i++) {
             Element element = (Element) report.getChildNodes().item(i);
@@ -76,7 +67,7 @@ public final class ReportHighScoresPanel extends ReportPanel {
 
                 JLabel scoreValue = new JLabel(String.valueOf(highScore.getScore()));
                 scoreValue.setFont(smallHeaderFont);
-                reportPanel.add(scoreValue, higConst.rc(reportRow, scoreColumn, "r"));
+                reportPanel.add(scoreValue);
                 
                 String messageID = null;
                 String nation = null;
@@ -91,77 +82,35 @@ public final class ReportHighScoresPanel extends ReportPanel {
                                                               "%name%", highScore.getPlayerName(),
                                                               "%nation%", nation));
                 headline.setFont(smallHeaderFont);
-                reportPanel.add(headline, higConst.rc(reportRow, panelColumn));
-                reportRow += 2;
+                reportPanel.add(headline, "span, wrap 10");
 
-                JPanel scorePanel = new JPanel();
-                scorePanel.setOpaque(false);
-                
-                int rows = 9;
-                int[] widths = new int[] { 200, 30, 200 };
-                int[] heights = new int[2 * rows - 1];
+                reportPanel.add(new JLabel(Messages.message("report.highScores.score")), "skip");
+                reportPanel.add(new JLabel(String.valueOf(highScore.getScore())));
 
-                for (int index = 1; index < heights.length; index += 2) {
-                    heights[index] = margin;
-                }
-                scorePanel.setLayout(new HIGLayout(widths, heights));
-                int row = 1;
-                int labelColumn = 1;
-                int valueColumn = 3;
+                reportPanel.add(new JLabel(Messages.message("report.highScores.difficulty")), "skip");
+                reportPanel.add(new JLabel(Messages.message(highScore.getDifficulty())));
 
-                JLabel scoreLabel = new JLabel(Messages.message("report.highScores.score"));
-                scorePanel.add(scoreLabel, higConst.rc(row, labelColumn, "l"));
-                JLabel scoreValue2 = new JLabel(String.valueOf(highScore.getScore()));
-                scorePanel.add(scoreValue2, higConst.rc(row, valueColumn, "r"));
-                row += 2;
-
-                JLabel difficultyLabel = new JLabel(Messages.message("report.highScores.difficulty"));
-                scorePanel.add(difficultyLabel, higConst.rc(row, labelColumn, "l"));
-                JLabel difficultyValue = new JLabel(Messages.message(highScore.getDifficulty()));
-                scorePanel.add(difficultyValue, higConst.rc(row, valueColumn, "r"));
-                row += 2;
-
-                JLabel independenceLabel = new JLabel(Messages.message("report.highScores.independence"));
-                scorePanel.add(independenceLabel, higConst.rc(row, labelColumn, "l"));
+                reportPanel.add(new JLabel(Messages.message("report.highScores.independence")), "skip");
                 int independenceTurn = highScore.getIndependenceTurn();
                 String independence = independenceTurn > 0 ? Turn.toString(independenceTurn) :
                     Messages.message("no");
-                JLabel independenceValue = new JLabel(independence);
-                scorePanel.add(independenceValue, higConst.rc(row, valueColumn, "r"));
-                row += 2;
+                reportPanel.add(new JLabel(independence));
 
-                JLabel nationLabel = new JLabel(Messages.message("report.highScores.nation"));
-                scorePanel.add(nationLabel, higConst.rc(row, labelColumn, "l"));
-                JLabel nationValue = new JLabel(String.valueOf(highScore.getOldNationName()));
-                scorePanel.add(nationValue, higConst.rc(row, valueColumn, "r"));
-                row += 2;
+                reportPanel.add(new JLabel(Messages.message("report.highScores.nation")), "skip");
+                reportPanel.add(new JLabel(String.valueOf(highScore.getOldNationName())));
 
-                JLabel nationTypeLabel = new JLabel(Messages.message("report.highScores.nationType"));
-                scorePanel.add(nationTypeLabel, higConst.rc(row, labelColumn, "l"));
-                JLabel nationTypeValue = new JLabel(Messages.message(highScore.getNationTypeID() + ".name"));
-                scorePanel.add(nationTypeValue, higConst.rc(row, valueColumn, "r"));
-                row += 2;
+                reportPanel.add(new JLabel(Messages.message("report.highScores.nationType")), "skip");
+                reportPanel.add(new JLabel(Messages.message(highScore.getNationTypeID() + ".name")));
 
-                JLabel unitLabel = new JLabel(Messages.message("report.highScores.units"));
-                scorePanel.add(unitLabel, higConst.rc(row, labelColumn, "l"));
-                JLabel unitValue = new JLabel(String.valueOf(highScore.getUnits()));
-                scorePanel.add(unitValue, higConst.rc(row, valueColumn, "r"));
-                row += 2;
+                reportPanel.add(new JLabel(Messages.message("report.highScores.units")), "skip");
+                reportPanel.add(new JLabel(String.valueOf(highScore.getUnits())));
 
-                JLabel coloniesLabel = new JLabel(Messages.message("report.highScores.colonies"));
-                scorePanel.add(coloniesLabel, higConst.rc(row, labelColumn, "l"));
-                JLabel coloniesValue = new JLabel(String.valueOf(highScore.getColonies()));
-                scorePanel.add(coloniesValue, higConst.rc(row, valueColumn, "r"));
-                row += 2;
+                reportPanel.add(new JLabel(Messages.message("report.highScores.colonies")), "skip");
+                reportPanel.add(new JLabel(String.valueOf(highScore.getColonies())));
 
-                JLabel dateLabel = new JLabel(Messages.message("report.highScores.retired"));
-                scorePanel.add(dateLabel, higConst.rc(row, labelColumn, "l"));
+                reportPanel.add(new JLabel(Messages.message("report.highScores.retired")), "skip");
                 DateFormat format = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT);
-                JLabel dateValue = new JLabel(format.format(highScore.getDate()));
-                scorePanel.add(dateValue, higConst.rc(row, valueColumn, "r"));
-
-                reportPanel.add(scorePanel, higConst.rc(reportRow, panelColumn));
-                reportRow += 2;
+                reportPanel.add(new JLabel(format.format(highScore.getDate())), "wrap 20");
 
             } catch (XMLStreamException e) {
                 logger.warning(e.toString());
