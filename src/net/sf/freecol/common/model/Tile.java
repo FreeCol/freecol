363fefac4331f441341161d0d9b86879867757e3
olivierschmitt <olivierschmitt@users.sourceforge.net>
2009-04-09 07:14:46 +0000

    Regression bug fix caused by revision #4436

--- src/net/sf/freecol/common/model/Tile.java
+++ src/net/sf/freecol/common/model/Tile.java
@@ -37,7 +37,6 @@ import javax.xml.stream.XMLStreamWriter;
 
 import net.sf.freecol.FreeCol;
 import net.sf.freecol.client.gui.i18n.Messages;
-import net.sf.freecol.common.Specification;
 import net.sf.freecol.common.model.Unit.UnitState;
 import net.sf.freecol.common.model.Map.CircleIterator;
 import net.sf.freecol.common.model.Map.Direction;
@@ -453,12 +452,12 @@ public final class Tile extends FreeColGameObject implements Location, Named, Ow
 
                     public boolean check(Unit u, PathNode pathNode) {
                         Map map = getGame().getMap();
-                        TileType tileType = pathNode.getTile().getType();
-                        if (canMoveToEurope()) {
+                        Tile t = pathNode.getTile();
+                        if (t.canMoveToEurope()) {
                             goal = pathNode;
                             return true;
                         }
-                        if (map.isAdjacentToMapEdge(pathNode.getTile())) {
+                        if (map.isAdjacentToMapEdge(t)) {
                             goal = pathNode;
                             return true;
                         }
