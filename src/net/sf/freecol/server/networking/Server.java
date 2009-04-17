17e193e201203c0f8717f2ab5be6490b32ee14ca
Mike Pope <mpope@users.sourceforge.net>
2009-04-17 04:17:20 +0000

    Formatting and readability.

--- src/net/sf/freecol/server/networking/Server.java
+++ src/net/sf/freecol/server/networking/Server.java
@@ -40,21 +40,20 @@ import org.w3c.dom.Element;
 
 
 /**
-* The networking server in which new clients can connect and methods
-* like <code>sendToAll</code> are kept.
-*
-* <br><br>
-*
-* When a new client connects to the server a new {@link Connection}
-* is made, with {@link net.sf.freecol.server.control.UserConnectionHandler}
-* as the control object.
-*
-* @see net.sf.freecol.common.networking
-*/
+ * The networking server in which new clients can connect and methods
+ * like <code>sendToAll</code> are kept.
+ *
+ * <br><br>
+ *
+ * When a new client connects to the server a new {@link Connection}
+ * is made, with {@link net.sf.freecol.server.control.UserConnectionHandler}
+ * as the control object.
+ *
+ * @see net.sf.freecol.common.networking
+ */
 public final class Server extends Thread {
 
     private static Logger logger = Logger.getLogger(Server.class.getName());
-
 
     /** The public "well-known" socket to which clients may connect. */
     private ServerSocket serverSocket;
@@ -62,7 +61,10 @@ public final class Server extends Thread {
     /** A hash of Connection objects, keyed by the Socket they relate to. */
     private HashMap<Socket, Connection> connections = new HashMap<Socket, Connection>();
 
-    /** Whether to keep running the main loop that is awaiting new client connections. */
+    /**
+     * Whether to keep running the main loop that is awaiting new
+     * client connections.
+     */
     private boolean running = true;
 
     /** The owner of this <code>Server</code>. */
@@ -91,33 +93,37 @@ public final class Server extends Thread {
     }
 
     /**
-    * Starts the thread's processing. Contains the loop that is waiting for new
-    * connections to the public socket. When a new client connects to the server
-    * a new {@link Connection} is made, with
-    * {@link net.sf.freecol.server.control.UserConnectionHandler} as the control
-    * object.
-    */
+     * Starts the thread's processing.  Contains the loop that is
+     * waiting for new connections to the public socket.  When a new
+     * client connects to the server a new {@link Connection} is made,
+     * with {@link net.sf.freecol.server.control.UserConnectionHandler}
+     * as the control object.
+     */
     public void run() {
         // This method's entire body is synchronized to shutdownLock.
-        // The reason why this is done is to prevent the shutdown method
-        // from finishing before this thread is finished working.
-        // We have to do this because the ServerSocket::close method keeps
-        // the server alive for several milliseconds EVEN AFTER THE CLOSE METHOD
-        // IS FINISHED. And because of this a new server can't be created on the
-        // same port as this server right after closing this server.
+        // The reason why this is done is to prevent the shutdown
+        // method from finishing before this thread is finished
+        // working.  We have to do this because the
+        // ServerSocket::close method keeps the server alive for
+        // several milliseconds EVEN AFTER THE CLOSE METHOD IS
+        // FINISHED. And because of this a new server can't be created
+        // on the same port as this server right after closing this
+        // server.
         //
-        // Now that the shutdown method 'hangs' until the entire server thread is
-        // finished you can be certain that the ServerSocket is REALLY closed
-        // after execution of shutdown.
+        // Now that the shutdown method 'hangs' until the entire
+        // server thread is finished you can be certain that the
+        // ServerSocket is REALLY closed after execution of shutdown.
         synchronized (shutdownLock) {
             while (running) {
                 Socket clientSocket = null;
                 try {
                     clientSocket = serverSocket.accept();
 
-                    logger.info("Got client connection from " + clientSocket.getInetAddress().toString());
-                    //Connection connection = 
-                        new Connection(clientSocket, freeColServer.getUserConnectionHandler(), FreeCol.SERVER_THREAD);
+                    logger.info("Got client connection from "
+                                + clientSocket.getInetAddress().toString());
+                    //Connection connection =
+                        new Connection(clientSocket, freeColServer.getUserConnectionHandler(),
+                                       FreeCol.SERVER_THREAD);
                     //connections.put(clientSocket, connection);
                 } catch (IOException e) {
                     if (running) {
@@ -131,14 +137,15 @@ public final class Server extends Thread {
     }
 
     /**
-    * Sends a network message to all connections except <code>exceptConnection</code>
-    * (if the argument is non-null).
-    *
-    * @param element The root element of the message to send.
-    * @param exceptConnection If non-null, the <code>Connection</code> not to send to.
-    */
+     * Sends a network message to all connections except
+     * <code>exceptConnection</code> (if the argument is non-null).
+     *
+     * @param element The root element of the message to send.
+     * @param exceptConnection If non-null, the
+     *        <code>Connection</code> not to send to.
+     */
     public void sendToAll(Element element, Connection exceptConnection) {
-        Iterator<Connection> connectionIterator = connections.values().iterator();
+        Iterator<Connection> connectionIterator = getConnectionIterator();
 
         while (connectionIterator.hasNext()) {
             Connection connection = connectionIterator.next();
@@ -146,34 +153,38 @@ public final class Server extends Thread {
                 try {
                     connection.sendAndWait(element);
                 } catch (IOException e) {
-                    logger.warning("Exception while attempting to send to " + connection);
+                    logger.warning("Exception while attempting to send to "
+                                   + connection);
                 }
             }
         }
     }
 
     /**
-    * Sends a network message to all connections.
-    * @param element The root element of the message to send.
-    */
+     * Sends a network message to all connections.
+     *
+     * @param element The root element of the message to send.
+     */
     public void sendToAll(Element element) {
         sendToAll(element, null);
     }
 
     /**
-    * Gets the TCP port that is beeing used for the public socket.
-    * @return The TCP port.
-    */
+     * Gets the TCP port that is beeing used for the public socket.
+     *
+     * @return The TCP port.
+     */
     public int getPort() {
         return port;
     }
 
     /**
-    * Sets the specified <code>MessageHandler</code> to all connections.
-    * @param messageHandler The <code>MessageHandler</code>.
-    */
+     * Sets the specified <code>MessageHandler</code> to all connections.
+     *
+     * @param messageHandler The <code>MessageHandler</code>.
+     */
     public void setMessageHandlerToAllConnections(MessageHandler messageHandler) {
-        Iterator<Connection> connectionIterator = connections.values().iterator();
+        Iterator<Connection> connectionIterator = getConnectionIterator();
 
         while (connectionIterator.hasNext()) {
             Connection connection = connectionIterator.next();
@@ -182,18 +193,18 @@ public final class Server extends Thread {
     }
 
     /**
-    * Gets an iterator of every connection to this server.
-    *
-    * @return The <code>Iterator</code>.
-    * @see Connection
-    */
+     * Gets an iterator of every connection to this server.
+     *
+     * @return The <code>Iterator</code>.
+     * @see Connection
+     */
     public Iterator<Connection> getConnectionIterator() {
         return connections.values().iterator();
     }
 
     /**
-    * Shuts down the server thread.
-    */
+     * Shuts down the server thread.
+     */
     public void shutdown() {
         running = false;
 
@@ -204,11 +215,11 @@ public final class Server extends Thread {
         }
 
         synchronized (shutdownLock) {
-            // Nothing to do here... just waiting for the server thread to finish.
-            // For more info see the run() method
+            // Nothing to do here... just waiting for the server
+            // thread to finish.  For more info see the run() method
         }
 
-        Iterator<Connection> connectionsIterator = connections.values().iterator();
+        Iterator<Connection> connectionsIterator = getConnectionIterator();
         while (connectionsIterator.hasNext()) {
             Connection c = connectionsIterator.next();
 
@@ -221,43 +232,46 @@ public final class Server extends Thread {
                 logger.warning("Could not close the connection.");
             }
         }
-        
+
         freeColServer.removeFromMetaServer();
-        
+
         logger.info("Server shutdown.");
     }
 
     /**
-    * Gets a <code>Connection</code> identified by a <code>Socket</code>.
-    *
-    * @param socket The <code>Socket</code> that identifies the
-    *               <code>Connection</code>
-    * @return The <code>Connection</code>.
-    */
+     * Gets a <code>Connection</code> identified by a <code>Socket</code>.
+     *
+     * @param socket The <code>Socket</code> that identifies the
+     *               <code>Connection</code>
+     * @return The <code>Connection</code>.
+     */
     public Connection getConnection(Socket socket) {
         return connections.get(socket);
     }
 
     /**
-    * Adds a (usually Dummy)Connection into the hashmap.
-    * @param connection The connection to add.
-    */
+     * Adds a (usually Dummy)Connection into the hashmap.
+     *
+     * @param connection The connection to add.
+     */
     public void addDummyConnection(Connection connection) {
         connections.put(new Socket(), connection);
     }
 
     /**
-    * Adds a Connection into the hashmap.
-    * @param connection The connection to add.
-    */
+     * Adds a Connection into the hashmap.
+     *
+     * @param connection The connection to add.
+     */
     public void addConnection(Connection connection) {
         connections.put(connection.getSocket(), connection);
     }
 
     /**
-    * Removes the given connection.
-    * @param connection The connection that should be removed.
-    */
+     * Removes the given connection.
+     *
+     * @param connection The connection that should be removed.
+     */
     public void removeConnection(Connection connection) {
         connections.remove(connection.getSocket());
     }
