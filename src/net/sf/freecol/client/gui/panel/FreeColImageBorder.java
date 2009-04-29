a74d16b0a8b2ab4d421f60ea25131088c7780719
Michael Vehrs <burschik@users.sourceforge.net>
2009-04-29 04:50:57 +0000

    Fix for bug #1694725 by Alexander Poschmann.

--- src/net/sf/freecol/client/gui/panel/FreeColImageBorder.java
+++ src/net/sf/freecol/client/gui/panel/FreeColImageBorder.java
@@ -21,9 +21,12 @@ package net.sf.freecol.client.gui.panel;
 
 import java.awt.Component;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Insets;
-import java.awt.Shape;
+import java.awt.Rectangle;
+import java.awt.TexturePaint;
+import java.awt.image.BufferedImage;
 
 import javax.swing.border.AbstractBorder;
 
@@ -31,38 +34,51 @@ import javax.swing.border.AbstractBorder;
  * A border created from a set of images. 
  */
 public class FreeColImageBorder extends AbstractBorder {
-
-    private Image topImage;
-    private Image leftImage;
-    private Image bottomImage;    
-    private Image rightImage;
+	// The buffered image objects
+    private BufferedImage topImage;
+    private BufferedImage leftImage;
+    private BufferedImage bottomImage;    
+    private BufferedImage rightImage;
+    private BufferedImage topLeftCornerImage;
+    private BufferedImage topRightCornerImage;
+    private BufferedImage bottomLeftCornerImage;
+    private BufferedImage bottomRightCornerImage;
     
-    private Image topLeftCornerImage;
-    private Image topRightCornerImage;
-    private Image bottomLeftCornerImage;
-    private Image bottomRightCornerImage;
-    
-    private Insets insets;
-
-
     /**
-     * Creates a border with the given set of images.
+     * Creates a border with the given set of images.<br />
+     * Converts the <code>Image</code> objects to <code>BufferedImage</code>, because the images will
+     * be used as Textures for the border.
      */
     public FreeColImageBorder(Image topImage, Image leftImage, Image bottomImage, Image rightImage, 
-                Image topLeftCornerImage, Image topRightCornerImage, Image bottomLeftCornerImage, Image bottomRightCornerImage) {       
-        this.topImage = topImage;
-        this.leftImage = leftImage;
-        this.bottomImage = bottomImage;
-        this.rightImage = rightImage;
-        
-        this.topLeftCornerImage = topLeftCornerImage;
-        this.topRightCornerImage = topRightCornerImage;
-        this.bottomLeftCornerImage = bottomLeftCornerImage;
-        this.bottomRightCornerImage = bottomRightCornerImage;
-        
-        insets = getBorderInsets(null);
+                Image topLeftCornerImage, Image topRightCornerImage, Image bottomLeftCornerImage, Image bottomRightCornerImage) {
+    	// Convert images to buffered images
+        this.topImage = createBufferedImage(topImage);
+        this.leftImage = createBufferedImage(leftImage);
+        this.bottomImage = createBufferedImage(bottomImage);
+        this.rightImage = createBufferedImage(rightImage);
+        this.topLeftCornerImage = createBufferedImage(topLeftCornerImage);
+        this.topRightCornerImage = createBufferedImage(topRightCornerImage);
+        this.bottomLeftCornerImage = createBufferedImage(bottomLeftCornerImage);
+        this.bottomRightCornerImage = createBufferedImage(bottomRightCornerImage);
     }
     
+    /**
+     * Creates a buffered image out of a given <code>Image</code> object.
+     * 
+     * @param img The <code>Image</code> object.
+     * @return The created <code>BufferedImage</code> object.
+     */
+    private BufferedImage createBufferedImage(Image img) {
+    	if(img != null) {
+	    	BufferedImage buff = new BufferedImage(getWidth(img), getHeight(img), BufferedImage.TYPE_INT_ARGB);
+	    	Graphics gfx = buff.createGraphics();
+	    	gfx.drawImage(img, 0, 0, null);
+	    	gfx.dispose();
+	    	return buff;
+    	} else {
+    		return null;
+    	}
+    }
     
     /**
      * Gets the insets of this border around the given component.
@@ -104,20 +120,28 @@ public class FreeColImageBorder extends AbstractBorder {
         if (insets == null) {
             return new Insets(top, left, bottom, right);
         } else {
-            //insets.set(top, left, bottom, right);
             insets.top = top;
             insets.left = left;
             insets.bottom = bottom;
             insets.right = right;
-            
             return insets;
         }
     }
     
+    /**
+     * Get the height of an image. If image is null, return 0.
+     * @param im The image.
+     * @return The height of the image.
+     */
     private int getHeight(Image im) {
         return (im != null) ? im.getHeight(null) : 0;
     }
     
+    /**
+     * Get the width of an image. If image is null, return 0.
+     * @param im The image.
+     * @return The width of the image.
+     */
     private int getWidth(Image im) {
         return (im != null) ? im.getWidth(null) : 0;
     }        
@@ -133,48 +157,64 @@ public class FreeColImageBorder extends AbstractBorder {
      * @param height The height of the border.
      */
     public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
-        final Shape originalClip = g.getClip();
-        if (topLeftCornerImage != null) {
-            g.drawImage(topLeftCornerImage, x + Math.max(insets.left - topLeftCornerImage.getWidth(null), 0), y, null);
-        }
-        if (topImage != null) {
-            final int rcw = (topRightCornerImage != null) ? topRightCornerImage.getWidth(null) : 0;
-            g.setClip(x + insets.left, y, width - rcw, insets.top);
-            for (int i=0; i < width - rcw; i += topImage.getWidth(null)) {
-                g.drawImage(topImage, x + insets.left + i, y + insets.top - topImage.getHeight(null), null);
-            }
-            g.setClip(originalClip);
-        }
-        if (topRightCornerImage != null) {
-            g.drawImage(topRightCornerImage, x + width - Math.max(insets.right, topRightCornerImage.getWidth(null)), y, null);
-        }
-        g.setClip(x, y + insets.top, width, height - insets.bottom);
-        if (leftImage != null) {
-            for (int i=0; i<height - insets.bottom; i+=leftImage.getHeight(null)) {
-                g.drawImage(leftImage, x + insets.left - leftImage.getWidth(null), y + insets.top + i, null);
-            }
-        }
-        if (rightImage != null) {
-            for (int i=0; i<height - insets.bottom; i+=rightImage.getHeight(null)) {
-                g.drawImage(rightImage, x + width - insets.right, y + insets.top + i, null);
-            }
-        }
-        g.setClip(originalClip);
-                
-        if (bottomLeftCornerImage != null) {
-            g.drawImage(bottomLeftCornerImage, x + Math.max(insets.left - bottomLeftCornerImage.getWidth(null), 0), y + height - insets.bottom, null);
-        }
-        if (bottomImage != null) {
-            int xx = ((bottomLeftCornerImage != null) ? bottomLeftCornerImage.getWidth(null) : 0);
-            final int rcw = (bottomRightCornerImage != null) ? bottomRightCornerImage.getWidth(null) : 0;
-            g.setClip(x + xx, y + height - insets.bottom, width - rcw - xx, insets.bottom);
-            for (int i=0; i < width - rcw - xx; i += bottomImage.getWidth(null)) {
-                g.drawImage(bottomImage, x + xx + i, y + height - insets.bottom, null);
-            }
-            g.setClip(originalClip);
-        }
-        if (bottomRightCornerImage != null) {
-            g.drawImage(bottomRightCornerImage, x + width - Math.max(insets.right, bottomRightCornerImage.getWidth(null)), y + height - Math.max(insets.bottom, bottomRightCornerImage.getHeight(null)), null);
-        }
+    	Insets insets = getBorderInsets(c);
+    	Graphics2D g2 = (Graphics2D) g;
+    	
+    	// Get width and height of the images
+    	int topHeight = getHeight(topImage);
+    	int leftWidth = getWidth(leftImage);
+    	int bottomHeight = getHeight(bottomImage);
+    	int rightWidth = getWidth(rightImage);	
+		int topLeftCornerWidth = getWidth(topLeftCornerImage);
+    	int topLeftCornerHeight = getHeight(topLeftCornerImage);
+		int topRightCornerWidth = getWidth(topRightCornerImage);
+    	int topRightCornerHeight = getHeight(topRightCornerImage);
+		int bottomLeftCornerWidth = getWidth(bottomLeftCornerImage);
+    	int bottomLeftCornerHeight = getHeight(bottomLeftCornerImage);
+		int bottomRightCornerWidth = getWidth(bottomRightCornerImage);
+    	int bottomRightCornerHeight = getHeight(bottomRightCornerImage);
+    	
+    	// Add the image border
+    	if(topImage != null) {
+    		fillTexture(g2, topImage, x + topLeftCornerWidth, y + insets.top - topHeight, width - topLeftCornerWidth - topRightCornerWidth, topHeight);
+    	}
+    	if(leftImage != null) {
+    		fillTexture(g2, leftImage, x + insets.left - leftWidth, y + topLeftCornerHeight, leftWidth, height - topLeftCornerHeight - bottomLeftCornerHeight);
+    	}
+    	if(bottomImage != null) {
+    		fillTexture(g2, bottomImage, x + bottomLeftCornerWidth, y + height - insets.bottom, width - bottomLeftCornerWidth - bottomRightCornerWidth, bottomHeight);
+    	}
+    	if(rightImage != null) {
+    		fillTexture(g2, rightImage, x + width - insets.right, y + topRightCornerHeight, rightWidth, height - topRightCornerHeight - bottomRightCornerHeight);
+    	}
+    	if(topLeftCornerImage != null) {
+    		fillTexture(g2, topLeftCornerImage, x + Math.max(insets.left, topLeftCornerWidth) - topLeftCornerWidth, y + Math.max(insets.top, topLeftCornerHeight) - topLeftCornerHeight, topLeftCornerWidth, topLeftCornerHeight);
+    	}
+    	if(topRightCornerImage != null) {
+    		fillTexture(g2, topRightCornerImage, x + width - Math.max(insets.right, topRightCornerWidth), y + Math.max(insets.top, topRightCornerHeight) - topRightCornerHeight, topRightCornerWidth, topRightCornerHeight);
+    	}
+    	if(bottomLeftCornerImage != null) {
+    		fillTexture(g2, bottomLeftCornerImage, x + Math.max(insets.left, bottomLeftCornerWidth) - bottomLeftCornerWidth, y + height - Math.max(insets.bottom, bottomLeftCornerHeight), bottomLeftCornerWidth, bottomLeftCornerHeight);
+    	}
+    	if(bottomRightCornerImage != null) {
+    		fillTexture(g2, bottomRightCornerImage, x + width - Math.max(insets.right, bottomRightCornerWidth), y + height - Math.max(insets.bottom, bottomRightCornerHeight), bottomRightCornerWidth, bottomRightCornerHeight);
+    	}
+    }
+    
+    /**
+     * Fills a certain rectangle with the image texture.
+     * 
+     * @param g2 The <code>Graphics</code> used for painting the border.
+     * @param img The <code>BufferedImage</code> to fill the texture.
+     * @param x The x-component of the offset.
+     * @param y The y-component of the offset.
+     * @param width The width of the rectangle.
+     * @param height The height of the rectangle.
+     */
+    public void fillTexture(Graphics2D g2, BufferedImage img, int x, int y, int width, int height) {
+    	Rectangle anchor = new Rectangle(x, y, getWidth(img), getHeight(img));
+    	TexturePaint paint = new TexturePaint(img, anchor);
+    	g2.setPaint(paint);
+    	g2.fillRect(x, y, width, height);
     }
 }
