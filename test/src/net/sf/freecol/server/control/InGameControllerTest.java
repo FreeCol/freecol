44a884ed7d4e7e1d701ffec260667345d00c23da
Darklord <pruano@users.sourceforge.net>
2010-01-16 03:37:34 +0000

    Added test.

--- test/src/net/sf/freecol/server/control/InGameControllerTest.java
+++ test/src/net/sf/freecol/server/control/InGameControllerTest.java
@@ -21,13 +21,17 @@ package net.sf.freecol.server.control;
 
 import java.io.File;
 
+import net.sf.freecol.FreeCol;
 import net.sf.freecol.common.FreeColException;
 import net.sf.freecol.common.model.Game;
+import net.sf.freecol.common.model.Goods;
+import net.sf.freecol.common.model.GoodsType;
 import net.sf.freecol.common.model.IndianSettlement;
 import net.sf.freecol.common.model.Map;
 import net.sf.freecol.common.model.Player;
 import net.sf.freecol.common.model.Tension;
 import net.sf.freecol.common.model.Tile;
+import net.sf.freecol.common.model.TileType;
 import net.sf.freecol.common.model.Unit;
 import net.sf.freecol.common.model.UnitType;
 import net.sf.freecol.common.model.Player.Stance;
@@ -97,5 +101,44 @@ public class InGameControllerTest extends FreeColTestCase {
 
         assertFalse("Mission creation should have failed",result);
         assertNull("Indian settlement should not have a mission",camp.getMissionary());
+    }
+    
+    public void testDumpGoods() {
+    	final TileType ocean = spec().getTileType("model.tile.ocean");
+    	final GoodsType cottonType = spec().getGoodsType("model.goods.cotton");
+        final UnitType privateerType = spec().getUnitType("model.unit.privateer");
+    	
+		// start a server
+        server = ServerTestHelper.startServer(false, true);
+                
+        server.setMapGenerator(new MockMapGenerator(getTestMap(ocean)));
+        
+        PreGameController pgc = (PreGameController) server.getController();
+        
+        try {
+            pgc.startGame();
+        } catch (FreeColException e) {
+            fail("Failed to start game");
+        }
+        
+        Game game = server.getGame();
+        FreeColTestCase.setGame(game);
+        Map map = game.getMap();
+        
+        Player dutchPlayer = game.getPlayer("model.nation.dutch");
+        Tile tile = map.getTile(1, 1);
+        Unit privateer = new Unit(game, tile, dutchPlayer, privateerType, UnitState.ACTIVE);
+        assertEquals("Setup Error, privateer should not carry anything", 0, privateer.getGoodsCount());
+        Goods cotton = new Goods(game,privateer,cottonType,100);
+        privateer.add(cotton);
+        assertEquals("Setup Error, privateer should carry cotton", 1, privateer.getGoodsCount());
+        assertTrue("Setup Error, cotton should be aboard privateer",cotton.getLocation() == privateer);
+        
+        InGameController controller = (InGameController) server.getController();
+        // moving a good to a null location means dumping the good
+        controller.moveGoods(cotton, null);
+        
+        assertEquals("Privateer should no longer carry cotton", 0, privateer.getGoodsCount());
+        assertNull("Cotton should have no location",cotton.getLocation());
     }
 }
