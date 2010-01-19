abf50943cfeb3b9562adb1ff0b92360443b76069
Darklord <pruano@users.sourceforge.net>
2010-01-19 13:11:45 +0000

    Port of rev.6320 (0.9.x branch).
    Test refactoring.

--- test/src/net/sf/freecol/server/ai/REFTest.java
+++ test/src/net/sf/freecol/server/ai/REFTest.java
@@ -37,10 +37,21 @@ import net.sf.freecol.util.test.FreeColTestCase;
 import net.sf.freecol.util.test.MockMapGenerator;
 
 public class REFTest extends FreeColTestCase {
+
+	FreeColServer server = null;
+	
+	public void tearDown(){
+		if(server != null){
+			// must make sure that the server is stopped
+			ServerTestHelper.stopServer(server);
+			server = null;
+		}
+		setGame(null);
+	}
 	
 	public void testCreateREFPlayer() {
 		// start a server
-        FreeColServer server = ServerTestHelper.startServer(false, true);
+        server = ServerTestHelper.startServer(false, true);
         
         Map map = getTestMap();
         
@@ -55,136 +66,124 @@ public class REFTest extends FreeColTestCase {
             fail("Failed to start game");
         }
         
-        try{
-            Game game = server.getGame();
+        Game game = server.getGame();
 
-            c = server.getController();
-            assertTrue("Controller not instance of InGameController", c instanceof InGameController);
-            InGameController igc = (InGameController) c;
+        c = server.getController();
+        assertTrue("Controller not instance of InGameController", c instanceof InGameController);
+        InGameController igc = (InGameController) c;
 
-            assertNotNull("InGameController is null",igc);
+        assertNotNull("InGameController is null",igc);
 
-            // Create player
-            ServerPlayer player1 = (ServerPlayer) game.getPlayer("model.nation.dutch");
-            
-            ServerPlayer refPlayer = igc.createREFPlayer(player1);
-            
-            assertNotNull("REF player is null",refPlayer);
-            assertNotNull("Player ref is null",player1.getREFPlayer());
-            assertEquals("REF player should be player1 ref", refPlayer, player1.getREFPlayer());
-        }finally {
-        	// must make sure that the server is stopped
-            ServerTestHelper.stopServer(server);
-        }
+        // Create player
+        ServerPlayer player1 = (ServerPlayer) game.getPlayer("model.nation.dutch");
+
+        ServerPlayer refPlayer = igc.createREFPlayer(player1);
+
+        assertNotNull("REF player is null",refPlayer);
+        assertNotNull("Player ref is null",player1.getREFPlayer());
+        assertEquals("REF player should be player1 ref", refPlayer, player1.getREFPlayer());        
 	}
 	
-	   public void testCreateREFUnits() {
-	        UnitType soldierType = spec().getUnitType("model.unit.kingsRegular");
-	        UnitType artilleryType = spec().getUnitType("model.unit.artillery");
-	        UnitType shipType = spec().getUnitType("model.unit.manOWar");
-	        
-	        // start a server
-	        FreeColServer server = ServerTestHelper.startServer(false, true);
-	        
-	        Map map = getTestMap();
-	        
-	        server.setMapGenerator(new MockMapGenerator(map));
-	        
-	        Controller c = server.getController();
-	        PreGameController pgc = (PreGameController)c;
-	        
-	        try {
-	            pgc.startGame();
-	        } catch (FreeColException e) {
-	            fail("Failed to start game");
-	        }
-	        
-	        try{
-	            Game game = server.getGame();
+	public void testCreateREFUnits() {
+		UnitType soldierType = spec().getUnitType("model.unit.kingsRegular");
+		UnitType artilleryType = spec().getUnitType("model.unit.artillery");
 
-	            c = server.getController();
-	            InGameController igc = (InGameController) c;
+		// start a server
+		server = ServerTestHelper.startServer(false, true);
 
-	            // Create player
-	            ServerPlayer player1 = (ServerPlayer) game.getPlayer("model.nation.dutch");
-	            ServerPlayer refPlayer = igc.createREFPlayer(player1);
-	            
-	            List <AbstractUnit> refUnitsBeforeIndependence = player1.getMonarch().getREF();
-	            int soldiersBeforeIndependence = 0;
-	            int dragoonsBeforeIndependence = 0;
-	            int artilleryBeforeIndependence = 0;
-	            int shipsBeforeIndependence = 0; 
-	            for(AbstractUnit unit : refUnitsBeforeIndependence){
-	                UnitType unitType = unit.getUnitType();
-	                if(unitType.hasAbility("model.ability.navalUnit")){
-	                    shipsBeforeIndependence += unit.getNumber();
-	                    continue;
-	                }
-	                if(unitType == artilleryType){
-	                    artilleryBeforeIndependence += unit.getNumber();
-	                    continue;
-	                }
-	                if(unitType == soldierType){
-	                    switch(unit.getRole()){
-	                        case SOLDIER:
-	                            soldiersBeforeIndependence += unit.getNumber();
-	                            break;
-	                        case DRAGOON:
-	                            dragoonsBeforeIndependence += unit.getNumber();
-	                            break;
-	                        default:
-	                            fail("Unkown REF role: " + unit.getRole().toString());
-	                            break;
-	                    }
-	                    continue;
-	                }
-	                fail("Unkown REF unit: " +  unit.toString());
-	            }
-	            
-	            // Execute
-	            List<Unit> refUnitsAfterIndependence = igc.createREFUnits(player1, refPlayer);
-	            
-	            // Get results
-	            int soldiersAfterIndependence = 0;
-	            int dragoonsAfterIndependence = 0;
-	            int artilleryAfterIndependence = 0;
-	            int shipsAfterIndependence = 0; 
-	            for(Unit unit : refUnitsAfterIndependence){
-	                UnitType unitType = unit.getType();
-                    if(unitType.hasAbility("model.ability.navalUnit")){
-                        shipsAfterIndependence++;
-                        continue;
-                    }
-                    if(unitType == artilleryType){
-                        artilleryAfterIndependence++;
-                        continue;
-                    }
-                    if(unitType == soldierType){
-                        switch(unit.getRole()){
-                            case SOLDIER:
-                                soldiersAfterIndependence++;
-                                break;
-                            case DRAGOON:
-                                dragoonsAfterIndependence++;
-                                break;
-                            default:
-                                fail("Unkown REF role: " + unit.getRole().toString());
-                                break;
-                        }
-                        continue;
-                    }
-                    fail("Unkown REF unit: " +  unit.toString());
-	            }
-	            
-	            // Verify results 
-	            assertEquals("Wrong number of ships",shipsBeforeIndependence,shipsAfterIndependence);
-	            assertEquals("Wrong number of artillery",artilleryBeforeIndependence,artilleryAfterIndependence);
-	            assertEquals("Wrong number of soldiers",soldiersBeforeIndependence,soldiersAfterIndependence);
-	            assertEquals("Wrong number of dragoons",dragoonsBeforeIndependence,dragoonsAfterIndependence);
-	            
-	        }finally {
-	            // must make sure that the server is stopped
-	            ServerTestHelper.stopServer(server);
-	        }
-	    }
+		Map map = getTestMap();
+
+		server.setMapGenerator(new MockMapGenerator(map));
+
+		Controller c = server.getController();
+		PreGameController pgc = (PreGameController)c;
+
+		try {
+			pgc.startGame();
+		} catch (FreeColException e) {
+			fail("Failed to start game");
+		}
+
+		Game game = server.getGame();
+
+		c = server.getController();
+		InGameController igc = (InGameController) c;
+
+		// Create player
+		ServerPlayer player1 = (ServerPlayer) game.getPlayer("model.nation.dutch");
+		ServerPlayer refPlayer = igc.createREFPlayer(player1);
+
+		List <AbstractUnit> refUnitsBeforeIndependence = player1.getMonarch().getREF();
+		int soldiersBeforeIndependence = 0;
+		int dragoonsBeforeIndependence = 0;
+		int artilleryBeforeIndependence = 0;
+		int shipsBeforeIndependence = 0; 
+		for(AbstractUnit unit : refUnitsBeforeIndependence){
+			UnitType unitType = unit.getUnitType();
+			if(unitType.hasAbility("model.ability.navalUnit")){
+				shipsBeforeIndependence += unit.getNumber();
+				continue;
+			}
+			if(unitType == artilleryType){
+				artilleryBeforeIndependence += unit.getNumber();
+				continue;
+			}
+			if(unitType == soldierType){
+				switch(unit.getRole()){
+				case SOLDIER:
+					soldiersBeforeIndependence += unit.getNumber();
+					break;
+				case DRAGOON:
+					dragoonsBeforeIndependence += unit.getNumber();
+					break;
+				default:
+					fail("Unkown REF role: " + unit.getRole().toString());
+				break;
+				}
+				continue;
+			}
+			fail("Unkown REF unit: " +  unit.toString());
+		}
+
+		// Execute
+		List<Unit> refUnitsAfterIndependence = igc.createREFUnits(player1, refPlayer);
+
+		// Get results
+		int soldiersAfterIndependence = 0;
+		int dragoonsAfterIndependence = 0;
+		int artilleryAfterIndependence = 0;
+		int shipsAfterIndependence = 0; 
+		for(Unit unit : refUnitsAfterIndependence){
+			UnitType unitType = unit.getType();
+			if(unitType.hasAbility("model.ability.navalUnit")){
+				shipsAfterIndependence++;
+				continue;
+			}
+			if(unitType == artilleryType){
+				artilleryAfterIndependence++;
+				continue;
+			}
+			if(unitType == soldierType){
+				switch(unit.getRole()){
+				case SOLDIER:
+					soldiersAfterIndependence++;
+					break;
+				case DRAGOON:
+					dragoonsAfterIndependence++;
+					break;
+				default:
+					fail("Unkown REF role: " + unit.getRole().toString());
+				break;
+				}
+				continue;
+			}
+			fail("Unkown REF unit: " +  unit.toString());
+		}
+
+		// Verify results 
+		assertEquals("Wrong number of ships",shipsBeforeIndependence,shipsAfterIndependence);
+		assertEquals("Wrong number of artillery",artilleryBeforeIndependence,artilleryAfterIndependence);
+		assertEquals("Wrong number of soldiers",soldiersBeforeIndependence,soldiersAfterIndependence);
+		assertEquals("Wrong number of dragoons",dragoonsBeforeIndependence,dragoonsAfterIndependence);
+	}
 }
