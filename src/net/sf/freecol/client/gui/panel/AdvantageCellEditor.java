a4ec91074b31c700573af68818c7c6ede04d88cf
Michael Vehrs <burschik@users.sourceforge.net>
2010-03-08 15:18:22 +0000

    Patch by Andreas D. Landmark to fix lack of i18n.

--- src/net/sf/freecol/client/gui/panel/AdvantageCellEditor.java
+++ src/net/sf/freecol/client/gui/panel/AdvantageCellEditor.java
@@ -24,9 +24,13 @@ import java.util.Vector;
 
 import javax.swing.DefaultCellEditor;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
 
 import net.sf.freecol.FreeCol;
+import net.sf.freecol.client.gui.i18n.Messages;
+import net.sf.freecol.client.gui.plaf.FreeColComboBoxRenderer;
 import net.sf.freecol.common.model.EuropeanNationType;
+
 
 /**
 * A table cell editor that can be used to select a nation.
@@ -40,9 +44,19 @@ public final class AdvantageCellEditor extends DefaultCellEditor {
     */
     public AdvantageCellEditor() {
         super(new JComboBox(new Vector<EuropeanNationType>(FreeCol.getSpecification().getEuropeanNationTypes())));
+        ((JComboBox) getComponent()).setRenderer(new AdvantageRenderer());
     }
     
     public Object getCellEditorValue() {
         return ((JComboBox) getComponent()).getSelectedItem();
     }
+
+    private class AdvantageRenderer extends FreeColComboBoxRenderer {
+        @Override
+        public void setLabelValues(JLabel label, Object value) {
+            label.setText((value == null) ? "" : Messages.message(value.toString() + ".name"));
+        }
+    }
+
+
 }
