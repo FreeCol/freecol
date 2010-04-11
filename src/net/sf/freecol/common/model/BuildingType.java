0ae2f24d9030f7acda3f310b809f8ced600a7fc1
Michael Vehrs <burschik@users.sourceforge.net>
2010-04-11 13:30:38 +0000

    Use inheritance for building types.

--- src/net/sf/freecol/common/model/BuildingType.java
+++ src/net/sf/freecol/common/model/BuildingType.java
@@ -35,15 +35,19 @@ public final class BuildingType extends BuildableType implements Comparable<Buil
     
     private static int nextIndex = 0;
 
-    private int level;
-  
-    private int workPlaces, basicProduction, minSkill, maxSkill;
-    private GoodsType consumes, produces;
+    private static final BuildingType defaultType = new BuildingType();
 
+    private int level = 1;
+    private int workPlaces = 3;
+    private int basicProduction = 3;
+    private int minSkill = Integer.MIN_VALUE;
+    private int maxSkill = Integer.MAX_VALUE;
+    private int sequence = 0;
+
+    private GoodsType consumes, produces;
     private Modifier productionModifier = null;
     private BuildingType upgradesFrom;
     private BuildingType upgradesTo;
-    private int sequence;
     
     public BuildingType() {
         setIndex(nextIndex++);
@@ -123,30 +127,36 @@ public final class BuildingType extends BuildableType implements Comparable<Buil
 
 
     public void readAttributes(XMLStreamReader in, Specification specification) throws XMLStreamException {
-        if (hasAttribute(in, "upgradesFrom")) {
-            upgradesFrom = specification.getBuildingType(in.getAttributeValue(null, "upgradesFrom"));
+        String extendString = in.getAttributeValue(null, "extends");
+        BuildingType parent = (extendString == null) ? defaultType :
+            specification.getBuildingType(extendString);
+        String upgradeString = in.getAttributeValue(null, "upgradesFrom");
+        if (upgradeString == null) {
+            level = 1;
+        } else {
+            upgradesFrom = specification.getBuildingType(upgradeString);
             upgradesFrom.upgradesTo = this;
             level = upgradesFrom.level + 1;
-        } else {
-            level = 1;
         }
-        setPopulationRequired(getAttribute(in, "required-population", 1));
+        setPopulationRequired(getAttribute(in, "required-population", parent.getPopulationRequired()));
 
-        workPlaces = getAttribute(in, "workplaces", 0);
-        basicProduction = getAttribute(in, "basicProduction", 0);
+        workPlaces = getAttribute(in, "workplaces", parent.workPlaces);
+        basicProduction = getAttribute(in, "basicProduction", parent.basicProduction);
 
-        consumes = specification.getType(in, "consumes", GoodsType.class, null);
-        produces = specification.getType(in, "produces", GoodsType.class, null);
+        consumes = specification.getType(in, "consumes", GoodsType.class, parent.consumes);
+        produces = specification.getType(in, "produces", GoodsType.class, parent.produces);
 
         if (produces != null && basicProduction > 0) {
             productionModifier = new Modifier(produces.getId(), this, basicProduction,
                                               Modifier.Type.ADDITIVE);
         }
 
-        minSkill = getAttribute(in, "minSkill", Integer.MIN_VALUE);
-        maxSkill = getAttribute(in, "maxSkill", Integer.MAX_VALUE);
+        minSkill = getAttribute(in, "minSkill", parent.minSkill);
+        maxSkill = getAttribute(in, "maxSkill", parent.maxSkill);
         
-        sequence = getAttribute(in, "sequence", 0);
+        sequence = getAttribute(in, "sequence", parent.sequence);
+
+        getFeatureContainer().add(parent.getFeatureContainer());
 
     }
 
