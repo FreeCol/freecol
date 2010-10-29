c821f0246c07d9e098e0dddd476475374f241ecf
Michael Vehrs <burschik@users.sourceforge.net>
2010-10-29 16:06:53 +0000

    Add Plunder class.

---
+++ src/net/sf/freecol/common/model/Plunder.java
@@ -0,0 +1,170 @@
+package net.sf.freecol.common.model;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+
+public class Plunder {
+
+    /**
+     * Probability that a settlement provides plunder.
+     */
+    private int probability = 0;
+
+    /**
+     * Minimum plunder.
+     */
+    private int minimum = 0;
+
+    /**
+     * Maximum plunder.
+     */
+    private int maximum = 0;
+
+    /**
+     * Factor to multiply the plunder amount with.
+     */
+    private int factor = 1;
+
+
+    public Plunder() {
+        // empty constructor
+    }
+
+    /**
+     * Creates a new <code>Plunder</code> instance.
+     *
+     * @param probability an <code>int</code> value
+     * @param minimum an <code>int</code> value
+     * @param maximum an <code>int</code> value
+     * @param factor an <code>int</code> value
+     */
+    public Plunder(int probability, int minimum, int maximum, int factor) {
+        this.probability = probability;
+        this.minimum = minimum;
+        this.maximum = maximum;
+        this.factor = factor;
+    }
+
+
+    /**
+     * Creates a new <code>Plunder</code> instance.
+     *
+     * @param in a <code>XMLStreamReader</code> value
+     */
+    public Plunder(XMLStreamReader in) throws XMLStreamException {
+        readFromXML(in);
+    }
+
+    /**
+     * Get the <code>Probability</code> value.
+     *
+     * @return an <code>int</code> value
+     */
+    public final int getProbability() {
+        return probability;
+    }
+
+    /**
+     * Set the <code>Probability</code> value.
+     *
+     * @param newProbability The new Probability value.
+     */
+    public final void setProbability(final int newProbability) {
+        this.probability = newProbability;
+    }
+
+    /**
+     * Get the <code>Minimum</code> value.
+     *
+     * @return an <code>int</code> value
+     */
+    public final int getMinimum() {
+        return minimum;
+    }
+
+    /**
+     * Set the <code>Minimum</code> value.
+     *
+     * @param newMinimum The new Minimum value.
+     */
+    public final void setMinimum(final int newMinimum) {
+        this.minimum = newMinimum;
+    }
+
+    /**
+     * Get the <code>Maximum</code> value.
+     *
+     * @return an <code>int</code> value
+     */
+    public final int getMaximum() {
+        return maximum;
+    }
+
+    /**
+     * Set the <code>Maximum</code> value.
+     *
+     * @param newMaximum The new Maximum value.
+     */
+    public final void setMaximum(final int newMaximum) {
+        this.maximum = newMaximum;
+    }
+
+    /**
+     * Get the <code>Factor</code> value.
+     *
+     * @return an <code>int</code> value
+     */
+    public final int getFactor() {
+        return factor;
+    }
+
+    /**
+     * Set the <code>Factor</code> value.
+     *
+     * @param newFactor The new Factor value.
+     */
+    public final void setFactor(final int newFactor) {
+        this.factor = newFactor;
+    }
+
+    public int getRange() {
+        return maximum - minimum;
+    }
+
+    public int getRandomLimit() {
+        return getRange() * 100 / factor;
+    }
+
+    public int getAmount(int random) {
+        int value = minimum + random;
+        if (value <= maximum) {
+            return value * factor;
+        } else {
+            return 0;
+        }
+    }
+
+    public Plunder clone() {
+        return new Plunder(probability, maximum, minimum, factor);
+    }
+
+
+    public void readFromXML(XMLStreamReader in) throws XMLStreamException {
+        probability = Integer.parseInt(in.getAttributeValue(null, "probability"));
+        minimum = Integer.parseInt(in.getAttributeValue(null, "minimum"));
+        maximum = Integer.parseInt(in.getAttributeValue(null, "maximum"));
+        factor = Integer.parseInt(in.getAttributeValue(null, "factor"));
+        in.nextTag();
+    }
+
+    public void toXML(XMLStreamWriter out, String tag) throws XMLStreamException {
+        out.writeStartElement(tag);
+        out.writeAttribute("probability", Integer.toString(probability));
+        out.writeAttribute("minimum", Integer.toString(minimum));
+        out.writeAttribute("maximum", Integer.toString(maximum));
+        out.writeAttribute("factor", Integer.toString(factor));
+        out.writeEndElement();
+    }
+
+}
