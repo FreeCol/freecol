ac959b49beb6bb3ebcfde2e5a76bbeba6db2e427
Michael Vehrs <burschik@users.sourceforge.net>
2011-04-25 09:19:22 +0000

    Improve documentation.

--- src/net/sf/freecol/common/io/FreeColSavegameFile.java
+++ src/net/sf/freecol/common/io/FreeColSavegameFile.java
@@ -28,13 +28,38 @@ import java.util.logging.Logger;
  * Represents a FreeCol savegame.
  */
 public class FreeColSavegameFile extends FreeColDataFile {
+
     @SuppressWarnings("unused")
     private static final Logger logger = Logger.getLogger(FreeColSavegameFile.class.getName());
 
+    /**
+     * The name of the file that contains the actual savegame.
+     */
     public static final String SAVEGAME_FILE = "savegame.xml";
+
+    /**
+     *  The name of a properties file that contains information about
+     *  the saved game, such as the size of the map, the date and time
+     *  it was started, and so on. The map size is used in the {@link
+     *  MapGeneratorOptionsDialog}, for example.
+     */
     public static final String SAVEGAME_PROPERTIES = "savegame.properties";
+
+    /**
+     * The name of the file that contains the {@link ClientOptions}
+     * saved with the game.
+     */
     public static final String CLIENT_OPTIONS = "client-options.xml";
+
+    /**
+     * The name of the image file that contains the map thumbnail,
+     * i.e. a view of the game map as seen by the owner of the game
+     * when saving. The thumbnail image is used by the {@link
+     * MapGeneratorOptionsDialog}, in particular.
+     */
     public static final String THUMBNAIL_FILE = "thumbnail.png";
+
+
 
     public FreeColSavegameFile(File file) throws IOException {
         super(file);
