21abf39aa2c881c2da8c6852d69ed2f327f5ad22
Michael Vehrs <burschik@users.sourceforge.net>
2011-05-29 10:22:36 +0000

    Simplify.

--- src/net/sf/freecol/client/gui/panel/ColopediaTreeCellRenderer.java
+++ src/net/sf/freecol/client/gui/panel/ColopediaTreeCellRenderer.java
@@ -21,9 +21,7 @@ package net.sf.freecol.client.gui.panel;
 
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.Image;
 
-import javax.swing.ImageIcon;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
@@ -35,38 +33,32 @@ import net.sf.freecol.common.resources.ResourceManager;
  * This TreeCellRenderer is responsible for rendering tree items in the Colopedia.
  */
 public class ColopediaTreeCellRenderer extends DefaultTreeCellRenderer {
-    
-    public ImageIcon icon;
-    
+
     /**
      * The constructor makes sure that the backgrounds are transparent.
      */
     public ColopediaTreeCellRenderer() {
         setBackgroundNonSelectionColor(new Color(0,0,0,1));
     }
-    
+
     /**
      * Returns the rendered Component
-     * 
+     *
      * @return the rendered item's Component
      */
-    public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+    public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected,
+                                                  boolean expanded, boolean leaf, int row, boolean hasFocus) {
         DefaultMutableTreeNode node = (DefaultMutableTreeNode)value;
-        
+
         super.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus);
         ColopediaTreeItem nodeItem = (ColopediaTreeItem)node.getUserObject();
-        
+
         if (leaf || nodeItem.getFreeColGameObjectType() instanceof BuildingType) {
-            ImageIcon icon = nodeItem.getIcon();
-            setIcon(icon);
+            setIcon(nodeItem.getIcon());
         } else if (expanded) {
-            Image openImage = ResourceManager.getImage("Colopedia.openSection.image");
-            ImageIcon openIcon = new ImageIcon((openImage != null) ? openImage : null);
-            setIcon(openIcon);
+            setIcon(ResourceManager.getImageIcon("Colopedia.openSection.image"));
         } else {
-            Image closedImage = ResourceManager.getImage("Colopedia.closedSection.image");
-            ImageIcon closedIcon = new ImageIcon((closedImage != null) ? closedImage : null);
-            setIcon(closedIcon);
+            setIcon(ResourceManager.getImageIcon("Colopedia.closedSection.image"));
         }
         return this;
     }
