86aa09e2aa2d4b47786f85302ca4f54adf6fb97f
Michael Vehrs <burschik@users.sourceforge.net>
2011-08-08 06:09:28 +0000

    Remove unnecessary override.

--- src/net/sf/freecol/server/ai/mission/CashInTreasureTrainMission.java
+++ src/net/sf/freecol/server/ai/mission/CashInTreasureTrainMission.java
@@ -171,16 +171,6 @@ public class CashInTreasureTrainMission extends Mission {
     }
 
     /**
-     * Checks if this mission is still valid to perform.
-     *
-     * @return True if this mission is still valid to perform.
-     */
-    public boolean isValid() {
-        return super.isValid();
-    }
-
-
-    /**
      * Writes all of the <code>AIObject</code>s and other AI-related
      * information to an XML-stream.
      *
