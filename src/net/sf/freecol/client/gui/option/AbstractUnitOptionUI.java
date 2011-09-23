aa769610e330d1187d6be9ec08640ede000a7eea
Michael Vehrs <burschik@users.sourceforge.net>
2011-09-23 04:23:23 +0000

    Add AbstractUnitOptionUI.

---
+++ src/net/sf/freecol/client/gui/option/AbstractUnitOptionUI.java
@@ -0,0 +1,197 @@
+/**
+ *  Copyright (C) 2002-2011  The FreeCol Team
+ *
+ *  This file is part of FreeCol.
+ *
+ *  FreeCol is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  FreeCol is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with FreeCol.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.sf.freecol.client.gui.option;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.List;
+import java.util.Locale;
+import java.util.logging.Logger;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+
+import net.sf.freecol.client.gui.i18n.Messages;
+import net.sf.freecol.client.gui.plaf.FreeColComboBoxRenderer;
+import net.sf.freecol.common.model.AbstractUnit;
+import net.sf.freecol.common.model.Unit.Role;
+import net.sf.freecol.common.option.AbstractUnitOption;
+
+
+/**
+ * This class provides visualization for an {@link
+ * net.sf.freecol.common.option.AbstractUnitOption}. In order to enable
+ * values to be both seen and changed.
+ */
+public final class AbstractUnitOptionUI extends JPanel implements OptionUpdater, PropertyChangeListener {
+
+    @SuppressWarnings("unused")
+    private static final Logger logger = Logger.getLogger(AbstractUnitOptionUI.class.getName());
+
+    private final AbstractUnitOption option;
+    private AbstractUnit originalValue;
+    private JLabel label;
+    private JComboBox typeBox;
+    private JComboBox roleBox;
+    private JSpinner spinner;
+
+    /**
+    * Creates a new <code>AbstractUnitOptionUI</code> for the given <code>AbstractUnitOption</code>.
+    *
+    * @param option The <code>AbstractUnitOption</code> to make a user interface for
+    * @param editable boolean whether user can modify the setting
+    */
+    public AbstractUnitOptionUI(final AbstractUnitOption option, boolean editable) {
+
+        this.option = option;
+        this.originalValue = option.getValue();
+
+        String name = Messages.getName(option);
+        String description = Messages.getShortDescription(option);
+        String text = (description != null) ? description : name;
+        label = new JLabel(name, JLabel.LEFT);
+        label.setToolTipText(text);
+
+        List<String> choices = option.getChoices();
+
+        typeBox = new JComboBox();
+        typeBox.setModel(new DefaultComboBoxModel(choices.toArray(new String[choices.size()])));
+        typeBox.setSelectedItem(option.getValue());
+        typeBox.setRenderer(new ChoiceRenderer());
+
+        typeBox.setEnabled(editable);
+        typeBox.setOpaque(false);
+        add(typeBox);
+
+        if (option.getSelectRole()) {
+            roleBox = new JComboBox();
+            roleBox.setModel(new DefaultComboBoxModel(Role.values()));
+            roleBox.setSelectedItem(option.getValue());
+            roleBox.setRenderer(new ChoiceRenderer());
+
+            roleBox.setEnabled(editable);
+            roleBox.setOpaque(false);
+            add(roleBox);
+        }
+
+        if (editable && option.getMaximumValue() > option.getMinimumValue()) {
+            int stepSize = Math.min((option.getMaximumValue() - option.getMinimumValue()) / 10, 1000);
+            spinner = new JSpinner();
+            spinner.setModel(new SpinnerNumberModel(option.getValue().getNumber(), option.getMinimumValue(),
+                                                    option.getMaximumValue(), Math.max(1, stepSize)));
+        } else {
+            int value = option.getValue().getNumber();
+            spinner.setModel(new SpinnerNumberModel(value, value, value, 1));
+        }
+
+        option.addPropertyChangeListener(this);
+        setOpaque(false);
+
+    }
+
+    /**
+     * Get the <code>Label</code> value.
+     *
+     * @return a <code>JLabel</code> value
+     */
+    public JLabel getLabel() {
+        return label;
+    }
+
+    /**
+     * Set the <code>Label</code> value.
+     *
+     * @param newLabel The new Label value.
+     */
+    public void setLabel(final JLabel newLabel) {
+        this.label = newLabel;
+    }
+
+    /**
+     * Rollback to the original value.
+     *
+     * This method gets called so that changes made to options with
+     * {@link net.sf.freecol.common.option.Option#isPreviewEnabled()} is rolled back
+     * when an option dialoag has been cancelled.
+     */
+    public void rollback() {
+        option.setValue(originalValue);
+    }
+
+    /**
+     * Unregister <code>PropertyChangeListener</code>s.
+     */
+    public void unregister() {
+        option.removePropertyChangeListener(this);
+    }
+
+    /**
+     * Updates this UI with the new data from the option.
+     * @param event The event.
+     */
+    public void propertyChange(PropertyChangeEvent event) {
+        if (event.getPropertyName().equals("value")) {
+            final AbstractUnit value = (AbstractUnit) event.getNewValue();
+            originalValue = value;
+        }
+    }
+
+    /**
+     * Updates the value of the {@link net.sf.freecol.common.option.Option} this object keeps.
+     */
+    public void updateOption() {
+        option.setValue(new AbstractUnit((String) typeBox.getSelectedItem(),
+                                         (Role) roleBox.getSelectedItem(),
+                                         (Integer) spinner.getValue()));
+    }
+
+    /**
+     * Reset with the value from the option.
+     */
+    public void reset() {
+        typeBox.setSelectedItem(option.getValue().getId());
+        roleBox.setSelectedItem(option.getValue().getRole());
+        spinner.setValue(option.getValue().getNumber());
+    }
+
+    private class ChoiceRenderer extends FreeColComboBoxRenderer {
+
+        @Override
+        public void setLabelValues(JLabel label, Object value) {
+            label.setText(Messages.message((String) value + ".name"));
+        }
+    }
+
+
+    private class RoleRenderer extends FreeColComboBoxRenderer {
+
+        @Override
+        public void setLabelValues(JLabel label, Object value) {
+            label.setText(Messages.message("model.unit.role." + ((String) value).toLowerCase(Locale.US)));
+        }
+    }
+
+}
