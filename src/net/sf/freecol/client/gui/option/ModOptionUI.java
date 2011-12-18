45f8c8bb6db615b90ea9f04bb8eaf2f0fa8868fa
Michael Vehrs <burschik@users.sourceforge.net>
2011-12-18 10:03:20 +0000

    Simplify rendering.

--- src/net/sf/freecol/client/gui/option/ModOptionUI.java
+++ src/net/sf/freecol/client/gui/option/ModOptionUI.java
@@ -20,18 +20,10 @@
 package net.sf.freecol.client.gui.option;
 
 
-import java.awt.AlphaComposite;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Composite;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JPanel;
 import javax.swing.ListCellRenderer;
 
 import net.sf.freecol.client.gui.GUI;
@@ -96,76 +88,26 @@ public final class ModOptionUI extends OptionUI<ModOption>  {
 
         @Override
         public void setLabelValues(JLabel label, Object value) {
-            try {
-                FreeColModFile modFile = (FreeColModFile) value;
+            FreeColModFile modFile = null;
+            if (value instanceof FreeColModFile) {
+                modFile = (FreeColModFile) value;
+            } else if (value instanceof ModOption) {
+                modFile = (FreeColModFile) ((ModOption) value).getValue();
+            }
+            if (modFile == null) {
+                label.setText(value.toString());
+            } else {
                 String key = "mod." + modFile.getId() + ".name";
                 label.setText(Messages.message(key));
                 if (Messages.containsKey(key + ".shortDescription")) {
                     label.setToolTipText(Messages.message(key + ".shortDescription"));
                 }
-            } catch(Exception e) {
-                label.setText(value.toString());
-                e.printStackTrace();
             }
         }
     }
 
     public ListCellRenderer getListCellRenderer() {
-        return new ModFileRenderer();
+        return new ChoiceRenderer();
     }
-
-    private class ModFileRenderer implements ListCellRenderer {
-
-        private final JPanel normal = new JPanel();
-        private final JPanel selected = new JPanel() {
-                public void paintComponent(Graphics g) {
-                    Graphics2D g2d = (Graphics2D) g;
-                    Composite oldComposite = g2d.getComposite();
-                    Color oldColor = g2d.getColor();
-                    g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.1f));
-                    g2d.setColor(Color.BLACK);
-                    g2d.fillRect(0, 0, getWidth(), getHeight());
-                    g2d.setComposite(oldComposite);
-                    g2d.setColor(oldColor);
-
-                    super.paintComponent(g);
-                }
-            };
-
-        public ModFileRenderer() {
-            super();
-            normal.setOpaque(false);
-            selected.setOpaque(false);
-        }
-
-
-        /**
-         * Returns a <code>ListCellRenderer</code> for the given <code>JList</code>.
-         *
-         * @param list The <code>JList</code>.
-         * @param value The list cell.
-         * @param index The index in the list.
-         * @param isSelected <code>true</code> if the given list cell is selected.
-         * @param hasFocus <code>false</code> if the given list cell has the focus.
-         * @return The <code>ListCellRenderer</code>
-         */
-        public Component getListCellRendererComponent(JList list, Object value, int index,
-                                                      boolean isSelected, boolean hasFocus) {
-
-            JPanel c = isSelected ? selected : normal;
-            c.removeAll();
-            c.setForeground(list.getForeground());
-            c.setFont(list.getFont());
-
-            String key = "mod." + ((FreeColModFile) ((ModOption) value).getValue()).getId() + ".name";
-            JLabel label = new JLabel(Messages.message(key));
-            if (Messages.containsKey(key + ".shortDescription")) {
-                label.setToolTipText(Messages.message(key + ".shortDescription"));
-            }
-            c.add(label);
-            return c;
-        }
-    }
-
 
 }
