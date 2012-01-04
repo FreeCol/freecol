99aff13a4504a089d7dc30853fbdf1612a9b0968
Michael Vehrs <burschik@users.sourceforge.net>
2012-01-04 16:53:15 +0000

    Clean up dialog.

--- src/net/sf/freecol/client/gui/panel/EditSettlementDialog.java
+++ src/net/sf/freecol/client/gui/panel/EditSettlementDialog.java
@@ -19,12 +19,10 @@
 
 package net.sf.freecol.client.gui.panel;
 
-import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.util.List;
-import java.util.Vector;
 import java.util.logging.Logger;
 
 import javax.swing.DefaultComboBoxModel;
@@ -32,16 +30,15 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
-import javax.swing.ListCellRenderer;
 import javax.swing.SpinnerNumberModel;
 
 import net.miginfocom.swing.MigLayout;
 import net.sf.freecol.client.FreeColClient;
 import net.sf.freecol.client.gui.GUI;
 import net.sf.freecol.client.gui.i18n.Messages;
+import net.sf.freecol.client.gui.plaf.FreeColComboBoxRenderer;
 import net.sf.freecol.common.model.IndianNationType;
 import net.sf.freecol.common.model.IndianSettlement;
 import net.sf.freecol.common.model.Nation;
@@ -50,18 +47,19 @@ import net.sf.freecol.common.model.Tile;
 import net.sf.freecol.common.model.Unit;
 import net.sf.freecol.common.model.UnitType;
 import net.sf.freecol.server.model.ServerUnit;
+import net.sf.freecol.common.util.RandomChoice;
 
 /**
  * This dialog is used to edit an Indian settlement (map editor only).
  */
-public final class EditSettlementDialog extends FreeColDialog<IndianSettlement> 
+public final class EditSettlementDialog extends FreeColDialog<IndianSettlement>
     implements ItemListener {
 
     @SuppressWarnings("unused")
     private static final Logger logger = Logger.getLogger(EditSettlementDialog.class.getName());
 
     private static final String REMOVE = "REMOVE";
-    
+
     private final IndianSettlement settlement;
 
     private final JCheckBox capital;
@@ -70,45 +68,36 @@ public final class EditSettlementDialog extends FreeColDialog<IndianSettlement>
     private final JSpinner units;
     private final JTextField name;
 
-    private class SkillRenderer implements ListCellRenderer {
-
-        private JLabel label = new JLabel();
-
-        public Component getListCellRendererComponent(JList list, Object value, int index,
-                                                      boolean isSelected, boolean cellHasFocus) {
-            label.setText(Messages.message(((UnitType) value).getNameKey()));
-            return label;
-        }
-    }
-
     /**
      * The constructor that will add the items to this panel.
-     * @param freeColClient 
+     * @param freeColClient the client
+     * @param gui the GUI
+     * @param settlement the IndianSettlement to edit
      */
     public EditSettlementDialog(FreeColClient freeColClient, GUI gui, final IndianSettlement settlement) {
-        
+
         super(freeColClient, gui);
         this.settlement = settlement;
 
         okButton.addActionListener(this);
 
-        Vector<Nation> natives = new Vector<Nation>();
-        for (Player player : settlement.getGame().getPlayers()) {
-            if (player.isAI() && player.isIndian()) {
-                natives.add(player.getNation());
-            }
-        }
-
-        setLayout(new MigLayout("wrap 2, gapx 20", "", ""));
+        setLayout(new MigLayout("wrap 2, gapx 20"));
 
         add(new JLabel(Messages.message("name")));
         name = new JTextField(settlement.getName(), 30);
         add(name);
 
         add(new JLabel(Messages.message("nation")));
-        owner = new JComboBox(natives);
+        DefaultComboBoxModel nationModel = new DefaultComboBoxModel();
+        for (Player player : settlement.getGame().getPlayers()) {
+            if (player.isAI() && player.isIndian()) {
+                nationModel.addElement(player.getNation());
+            }
+        }
+        owner = new JComboBox(nationModel);
         owner.setSelectedItem(settlement.getOwner().getNation());
         owner.addItemListener(this);
+        owner.setRenderer(new FreeColComboBoxRenderer());
         add(owner);
 
         add(new JLabel(Messages.message("capital")));
@@ -117,17 +106,15 @@ public final class EditSettlementDialog extends FreeColDialog<IndianSettlement>
         add(capital);
 
         add(new JLabel(Messages.message("report.indian.skillTaught")));
-        skill = new JComboBox();
+        DefaultComboBoxModel skillModel = new DefaultComboBoxModel();
         IndianNationType ownerType = (IndianNationType)
             ((Nation) owner.getSelectedItem()).getType();
-        UnitType[] skills = new UnitType[ownerType.getSkills().size()];
-        for (int index = 0; index < skills.length; index++) {
-            skills[index] = ownerType.getSkills().get(index).getObject();
+        for (RandomChoice<UnitType> skill : ownerType.getSkills()) {
+            skillModel.addElement(skill.getObject());
         }
-        DefaultComboBoxModel model = new DefaultComboBoxModel(skills);
-        skill.setModel(model);
+        skill = new JComboBox(skillModel);
         skill.setSelectedItem(settlement.getLearnableSkill());
-        skill.setRenderer(new SkillRenderer());
+        skill.setRenderer(new FreeColComboBoxRenderer());
         add(skill);
 
         add(new JLabel(Messages.message("report.units")));
@@ -135,13 +122,13 @@ public final class EditSettlementDialog extends FreeColDialog<IndianSettlement>
         SpinnerNumberModel spinnerModel = new SpinnerNumberModel(unitCount, 1, 20, 1);
         units = new JSpinner(spinnerModel);
         add(units);
-        
+
         JButton remove = new JButton(Messages.message("editor.removeSettlement"));
         remove.setActionCommand(REMOVE);
         remove.addActionListener(this);
-        add(remove,"span,align center");
-        
-        add(okButton, "newline 20, span, split 2, tag ok");
+
+        add(okButton, "newline 20, span, split 3, tag ok");
+        add(remove, "");
         add(cancelButton, "tag cancel");
 
         setSize(getPreferredSize());
@@ -184,29 +171,29 @@ public final class EditSettlementDialog extends FreeColDialog<IndianSettlement>
                 UnitType BRAVE = getSpecification().getUnitType("model.unit.brave");
                 for (int index = 0; index < numberOfUnits; index++) {
                     settlement.add(new ServerUnit(settlement.getGame(),
-                            settlement, settlement.getOwner(), BRAVE));
+                                                  settlement, settlement.getOwner(), BRAVE));
                 }
             } else if (numberOfUnits < 0) {
                 List<Unit> unitList = settlement.getUnitList().subList(0, -numberOfUnits);
                 for (Unit unit : unitList) {
                     unit.dispose();
                 }
-            } 
-        }
-        else if(REMOVE.equals(command)) {
-        	boolean confirm = getGUI().showConfirmDialog("editor.removeSettlement.text",
-                                                          "ok", "cancel");
-        	if(!confirm){
-        		return;
-        	}
-        	// Dispose of units and settlement on tile
-        	Tile t = settlement.getTile();
-          for (Unit unit : t.getUnitList()) {
-              unit.dispose();
-          }
-        	t.setSettlement(null);
-          settlement.dispose();
+            }
+        } else if (REMOVE.equals(command)) {
+            if (!getGUI().showConfirmDialog("editor.removeSettlement.text",
+                                            "ok", "cancel")) {
+                return;
+            }
+            // Dispose of units and settlement on tile
+            Tile t = settlement.getTile();
+            for (Unit unit : t.getUnitList()) {
+                unit.dispose();
+            }
+            t.setSettlement(null);
+            settlement.dispose();
         }
         getGUI().removeFromCanvas(this);
     }
+
+
 }
