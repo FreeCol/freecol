bd0b1f6232df49da7bc4e1a30c31dbec8d8ec7c0
Michael Vehrs <burschik@users.sourceforge.net>
2012-01-29 11:51:45 +0000

    Update copyright.

--- src/net/sf/freecol/client/gui/GUI.java
+++ src/net/sf/freecol/client/gui/GUI.java
@@ -1,3 +1,23 @@
+/**
+ *  Copyright (C) 2002-2012   The FreeCol Team
+ *
+ *  This file is part of FreeCol.
+ *
+ *  FreeCol is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  FreeCol is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with FreeCol.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 package net.sf.freecol.client.gui;
 
 import java.awt.Component;
@@ -84,7 +104,7 @@ public class GUI {
 
     private static final Logger logger = Logger.getLogger(GUI.class.getName());
 
-    public static final String COPYRIGHT = "Copyright (C) 2003-2011 The FreeCol Team";
+    public static final String COPYRIGHT = "Copyright (C) 2002-2012   The FreeCol Team";
 
     public static final String LICENSE = "http://www.gnu.org/licenses/gpl.html";
 
