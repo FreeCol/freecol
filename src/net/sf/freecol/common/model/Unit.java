d30d43cf764b081ad867e4887ce13953b8be94b7
Mike Pope <mpope@users.sourceforge.net>
2012-05-04 10:43:01 +0000

    Fix bad choice of defender, BR#3523327.

--- src/net/sf/freecol/common/model/Unit.java
+++ src/net/sf/freecol/common/model/Unit.java
@@ -3284,9 +3284,11 @@ public class Unit extends FreeColGameObject
                                          Unit other, float otherPower) {
         if (defender == null) {
             return true;
-        } else if (!defender.isArmed() && other.isArmed()) {
+        } else if (defender.isPerson() && other.isPerson()
+            && !defender.isArmed() && other.isArmed()) {
             return true;
-        } else if (defender.isArmed() && !other.isArmed()) {
+        } else if (defender.isPerson() && other.isPerson()
+            && defender.isArmed() && !other.isArmed()) {
             return false;
         } else if (!defender.isDefensiveUnit() && other.isDefensiveUnit()) {
             return true;
