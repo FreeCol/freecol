2d4edf6eaf7b4a218a922420da1d65d465756ba0
Mike Pope <mpope@users.sourceforge.net>
2013-01-09 01:46:48 +0000

    Clean and comment game initialization.

--- src/net/sf/freecol/FreeCol.java
+++ src/net/sf/freecol/FreeCol.java
@@ -80,7 +80,7 @@ public final class FreeCol {
     public static final int     DEFAULT_TIMEOUT = 60; // 1 minute
     public static final int     TIMEOUT_MIN = 10; // 10s
 
-    private static final int MIN_MEMORY = 128; // Mbytes
+    private static final int    MIN_MEMORY = 128; // Mbytes
     private static final String MIN_JDK_VERSION = "1.5";
     private static final String DEFAULT_SPLASH_FILE = "splash.jpg";
 
