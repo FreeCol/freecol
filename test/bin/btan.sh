3d77fa764688a186a2acb62bd09f8dec2eb5a2fd
Mike Pope <mpope@computer.org>
2013-01-31 22:02:32 +1030

    Add AI mission success counting scripts.

---
+++ test/bin/btan.sh
@@ -0,0 +1,100 @@
+#! /bin/sh
+# btan.sh
+# Analyze bigtest run
+#
+# Usage:
+#   btan.sh <tag> < <fc-log-score-output>
+#
+if test "x$1" = "x" ; then name=Unknown ; else name="$1" ; fi
+name=`date +%Y%m%d`-"$name"
+STATS=stats # Where is the stats awk script?
+blockitstate=y
+
+
+blockit () {
+    if test "$blockitstate" = "y" ; then
+        echo -n "         "
+        blockitstate=n
+    else
+        echo
+        blockitstate=y
+    fi
+}
+
+statit () {
+    statitMEAN=0 ; statitSD=0
+    eval `$STATS | sed -e 's/^n=[^ ]* *mean=\([^ ]*\) *sd=\(.*\)$/statitMEAN="\1";statitSD="\2"/' -`
+    printf "%7.3f ~ %7.3f" "$statitMEAN" "$statitSD"
+}
+
+tmp=`mktemp btan.XXXXXXXX`
+trap "rm -f '$tmp'" 0
+cat - > "$tmp"
+runs=`sed -n -e 's/^run: .*colonies: n=\([0-9]*\) mean=\([\.0-9]*\) sd=\([\.0-9]*\)$/NC="\1";MEAN="\2";SD="\3"/p' "$tmp"`
+
+N=0
+for r in $runs ; do N=`expr $N + 1`; done
+echo "$name x $N"
+
+
+echo -n "Builds        "
+sed -n -e 's/^Count builds: *\(.*\)$/\1/p' "$tmp" | statit
+blockit
+
+echo -n "Cashins       "
+sed -n -e 's/^Count cashins: *\(.*\)$/\1/p' "$tmp" | statit
+blockit
+
+echo -n "Colony#       "
+(for r in $runs ; do eval $r ; echo $NC ;   done) | statit
+blockit
+
+echo -n "ColonySize    "
+(for r in $runs ; do eval $r ; echo $MEAN ; done) | statit
+blockit
+
+echo -n "Defences      "
+sed -n -e 's/^Count defences: *\(.*\)$/\1/p' "$tmp" | statit
+blockit
+
+echo -n "Missions      "
+sed -n -e 's/^Count missions: *\(.*\)$/\1/p' "$tmp" | statit
+blockit
+
+echo -n "Native Demands"
+sed -n -e 's/^Count demands: *\(.*\)$/\1/p' "$tmp" | statit
+blockit
+
+echo -n "Native Gifts  "
+sed -n -e 's/^Count gifts: *\(.*\)$/\1/p' "$tmp" | statit
+blockit
+
+echo -n "Pioneerings   "
+sed -n -e 's/^Count pioneerings: *\(.*\)$/\1/p' "$tmp" | statit
+blockit
+
+echo -n "Piracies      "
+sed -n -e 's/^Count piracies: *\(.*\)$/\1/p' "$tmp" | statit
+blockit
+
+echo -n "Scoutings     "
+sed -n -e 's/^Count scoutings: *\(.*\)$/\1/p' "$tmp" | statit
+blockit
+
+echo -n "Seek+Destroys "
+sed -n -e 's/^Count seek+dests: *\(.*\)$/\1/p' "$tmp" | statit
+blockit
+
+echo -n "Transports    "
+sed -n -e 's/^Count transports: *\(.*\)$/\1/p' "$tmp" | statit
+blockit
+
+echo -n "Turn Speed    "
+sed -n -e 's/^Average turn: *\(.*\)$/\1/p' "$tmp" | statit
+blockit
+
+echo -n "Wishes        "
+sed -n -e 's/^Count wishes: *\(.*\)$/\1/p' "$tmp" | statit
+blockit
+
+test "$blockitstate" = "y" || echo
---
+++ test/bin/fclogscore.sh
@@ -0,0 +1,44 @@
+#! /bin/sh
+# fclogscore
+# Analyze FC log
+#
+# Usage:
+#   fclogscore.sh <freecol-log-file>...
+#
+STATS=stats.awk # Where is the stats awk script?
+
+statit () {
+    statitMEAN=0 ; statitSD=0
+    eval `$STATS | sed -e 's/^n=[^ ]* *mean=\([^ ]*\) *sd=\(.*\)$/statitMEAN="\1";statitSD="\2"/' -`
+    printf "%7.3f" "$statitMEAN"
+}
+
+countinlog () {
+    echo -n 'Count builds: '      ; grep -c 'AI colony builder completed' "$1"
+    echo -n 'Count cashins: '     ; grep -c 'AI treasureTrain completed' "$1"
+    echo -n 'Count defences: '    ; grep -c 'AI defender completed' "$1"
+    echo -n 'Count missions: '    ; grep -c 'AI missionary completed' "$1"
+    echo -n 'Count demands:  '    ; grep -c 'AI native demander completed' "$1"
+    echo -n 'Count gifts:    '    ; grep -c 'AI native gifter completed' "$1"
+    echo -n 'Count pioneerings: ' ; grep -c 'AI pioneer completed' "$1"
+    echo -n 'Count piracies: '    ; grep -c 'AI privateer completed' "$1"
+    echo -n 'Count scoutings: '   ; grep -c 'AI scout completed' "$1"
+    echo -n 'Count seek+dests: '  ; grep -c 'AI seek+destroyer attacking' "$1"
+    echo -n 'Count transports: '  ; grep -c 'AI transport completed' "$1"
+    echo -n 'Count wishes: '      ; grep -c 'AI wisher completed' "$1"
+    echo -n 'Average turn: '  
+    avg=`sed -n -e 's/^.*duration = \(.*\)ms$/\1/p' "$1" | statit`
+    echo "scale=3; $avg / 1000.0" | bc
+#    echo "########## Spanish Succession"
+#    sed -n -e '{ s/^\t\(INFO: Spanish succession.*\)/\1/p ; T ; n ; p }' "$1"
+}
+
+for f in ${1+"$@"} ; do
+    if test -r "$f" ; then
+        countinlog "$f"
+    else
+        echo "can not read: $f" >&2
+        exit 1
+    fi
+done
+exit 0
