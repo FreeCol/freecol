dcd5dcb07b5bf9d3e850c8edaab8670a1c5d5949
Michael Pope <mpope@computer.org>
2013-02-18 16:24:30 +1030

    Tidy serialization #?, Event.

--- src/net/sf/freecol/common/model/Event.java
+++ src/net/sf/freecol/common/model/Event.java
@@ -20,6 +20,7 @@
 package net.sf.freecol.common.model;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,178 +31,178 @@ import javax.xml.stream.XMLStreamWriter;
 
 public class Event extends FreeColGameObjectType {
 
-    /**
-     * A restriction on the scope of the event.
-     */
+    /** A restriction on the scope of the event. */
     private String value;
 
-    /**
-     * The score value of this event.
-     */
+    /** The score value of this event. */
     private int scoreValue = 0;
 
+    /** Limits on this event. */
+    private Map<String, Limit> limits = null;
+
+
+
     /**
-     * Describe limits here.
+     * Create a new event.
+     *
+     * @param id The event id.
+     * @param specification The containing <code>Specification</code>.
      */
-    private Map<String, Limit> limits;
-
-
-
     public Event(String id, Specification specification) {
         super(id, specification);
     }
 
+
     /**
-     * Get the <code>Value</code> value.
+     * Gets the event restriction.
      *
-     * @return a <code>String</code> value
+     * @return The restriction.
      */
     public final String getValue() {
         return value;
     }
 
     /**
-     * Set the <code>Value</code> value.
+     * Sets the event restriction.
      *
-     * @param newValue The new Value value.
+     * @param newValue The new event restriction.
      */
     public final void setValue(final String newValue) {
         this.value = newValue;
     }
 
     /**
-     * Get the <code>Limits</code> value.
+     * Get the limits on this event.
      *
-     * @return a <code>List<Limit></code> value
+     * @return A list of limits.
      */
     public final Collection<Limit> getLimits() {
+        if (limits == null) return Collections.emptyList();
         return limits.values();
     }
 
     /**
-     * Return the <code>Limit</code> with the given id.
+     * Gets a particular limit by id.
      *
-     * @param id a <code>String</code> value
-     * @return a <code>Limit</code> value
+     * @param id The id to look for.
+     * @return The corresponding <code>Limit</code> or null if not found.
      */
     public final Limit getLimit(String id) {
-        return limits.get(id);
+        return (limits == null) ? null : limits.get(id);
     }
 
     /**
-     * Get the <code>ScoreValue</code> value.
+     * Get the score value of this event.
      *
-     * @return an <code>int</code> value
+     * @return The score value.
      */
     public final int getScoreValue() {
         return scoreValue;
     }
 
     /**
-     * Set the <code>ScoreValue</code> value.
+     * Set the score value of this event.
      *
-     * @param newScoreValue The new ScoreValue value.
+     * @param newScoreValue The new score value.
      */
     public final void setScoreValue(final int newScoreValue) {
         this.scoreValue = newScoreValue;
     }
 
 
+    // Serialization
+
+    private static final String SCORE_VALUE_TAG = "scoreValue";
+    private static final String VALUE_TAG = "value";
+
+
     /**
-     * This method writes an XML-representation of this object to
-     * the given stream.
-     *
-     * @param out The target stream.
-     * @throws XMLStreamException if there are any problems writing
-     *      to the stream.
+     * {@inheritDoc}
      */
+    @Override
     protected void toXMLImpl(XMLStreamWriter out) throws XMLStreamException {
         super.toXML(out, getXMLElementTagName());
     }
 
     /**
-     * Write the attributes of this object to a stream.
-     *
-     * @param out The target stream.
-     * @throws XMLStreamException if there are any problems writing to
-     *     the stream.
+     * {@inheritDoc}
      */
     @Override
-    protected void writeAttributes(XMLStreamWriter out)
-        throws XMLStreamException {
+    protected void writeAttributes(XMLStreamWriter out) throws XMLStreamException {
         super.writeAttributes(out);
 
         if (value != null) {
-            out.writeAttribute(VALUE_TAG, value);
+            writeAttribute(out, VALUE_TAG, value);
         }
+
         if (scoreValue != 0) {
-            out.writeAttribute("scoreValue", Integer.toString(scoreValue));
+            writeAttribute(out, SCORE_VALUE_TAG, scoreValue);
         }
     }
 
     /**
-     * Write the children of this object to a stream.
-     *
-     * @param out The target stream.
-     * @throws XMLStreamException if there are any problems writing to
-     *     the stream.
+     * {@inheritDoc}
      */
     @Override
-    protected void writeChildren(XMLStreamWriter out)
-        throws XMLStreamException {
+    protected void writeChildren(XMLStreamWriter out) throws XMLStreamException {
         super.writeChildren(out);
 
         if (limits != null) {
-            for (Limit limit : limits.values()) {
-                limit.toXMLImpl(out);
-            }
+            for (Limit limit : limits.values()) limit.toXMLImpl(out);
         }
     }
 
     /**
-     * Reads the attributes of this object from an XML stream.
-     *
-     * @param in The XML input stream.
-     * @throws XMLStreamException if a problem was encountered
-     *     during parsing.
+     * {@inheritDoc}
      */
     @Override
-    protected void readAttributes(XMLStreamReader in)
-        throws XMLStreamException {
+    protected void readAttributes(XMLStreamReader in) throws XMLStreamException {
         super.readAttributes(in);
 
-        value = in.getAttributeValue(null, VALUE_TAG);
-        scoreValue = getAttribute(in, "scoreValue", 0);
+        value = getAttribute(in, VALUE_TAG, (String)null);
+
+        scoreValue = getAttribute(in, SCORE_VALUE_TAG, 0);
     }
 
     /**
-     * Reads a child object.
-     *
-     * @param in The XML stream to read.
-     * @exception XMLStreamException if an error occurs
+     * {@inheritDoc}
+     */
+    @Override
+    protected void readChildren(XMLStreamReader in) throws XMLStreamException {
+        if (readShouldClearContainers(in)) {
+            limits = null;
+        }
+        
+        super.readChildren(in);
+    }        
+
+    /**
+     * {@inheritDoc}
      */
     @Override
     protected void readChild(XMLStreamReader in) throws XMLStreamException {
-        if (Limit.getXMLElementTagName().equals(in.getLocalName())) {
-            if (limits == null) {
-                limits = new HashMap<String, Limit>();
-            }
+        final String tag = in.getLocalName();
+
+        if (Limit.getXMLElementTagName().equals(tag)) {
             Limit limit = new Limit(getSpecification());
             limit.readFromXML(in);
+            if (limits == null) limits = new HashMap<String, Limit>();
+            limits.put(limit.getId(), limit);
+
             // @compat 0.10.5
             if (limit.getId().equals("model.limit.independence.colonies")) {
                 limit.setId("model.limit.independence.coastalColonies");
                 limit.getLeftHandSide().setMethodName("isConnectedPort");
             }
-            // @end compatibility code
-            limits.put(limit.getId(), limit);
+            // end @compat
+
         } else {
             super.readChild(in);
         }
     }
 
     /**
-     * Returns the tag name of the root element representing this object.
+     * Gets the tag name of the root element representing this object.
      *
      * @return "event".
      */
