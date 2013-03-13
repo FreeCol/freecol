b32f1cb415d272e7b09454626f48b171867239c7
Michael Pope <mpope@computer.org>
2013-03-13 10:40:12 +1030

    Tidy serialization #34, ProductionType.

--- src/net/sf/freecol/common/model/ProductionType.java
+++ src/net/sf/freecol/common/model/ProductionType.java
@@ -32,27 +32,27 @@ import javax.xml.stream.XMLStreamWriter;
  */
 public class ProductionType extends FreeColObject {
 
-    /**
-     * Whether this production type applies only to colony center tiles.
-     */
+    /** Whether this production type applies only to colony center tiles. */
     private boolean colonyCenterTile;
 
     /**
-     * The production level of this production type (used by difficulty levels).
+     * The production level of this production type (usually a
+     * difficulty level key).
      */
     private String productionLevel;
 
+    /** The goods that are produced by this production type. */
+    private List<AbstractGoods> outputs = null;
+
+    /** The goods that are consumed by this production type. */
+    private List<AbstractGoods> inputs = null;
+
+
     /**
-     * The goods that are produced by this production type.
+     * Simple constructor.
+     *
+     * @param specification The enclosing <code>Specification</code>.
      */
-    private List<AbstractGoods> outputs;
-
-    /**
-     * The goods that are consumed by this production type.
-     */
-    private List<AbstractGoods> inputs;
-
-
     public ProductionType(Specification specification) {
         setSpecification(specification);
     }
@@ -61,7 +61,7 @@ public class ProductionType extends FreeColObject {
      * Creates a new production type that consumes no raw materials
      * and produces the given output.
      *
-     * @param outputs the goods produced
+     * @param outputs A list of the <code>AbstractGoods</code> produced.
      */
     public ProductionType(List<AbstractGoods> outputs) {
         this.outputs = outputs;
@@ -71,8 +71,8 @@ public class ProductionType extends FreeColObject {
      * Creates a new production type that consumes the given raw materials
      * and produces the given output.
      *
-     * @param inputs the goods consumed
-     * @param outputs the goods produced
+     * @param inputs A list of the <code>AbstractGoods</code> consumed.
+     * @param outputs A list of the <code>AbstractGoods</code> produced.
      */
     public ProductionType(List<AbstractGoods> inputs, List<AbstractGoods> outputs) {
         this.inputs = inputs;
@@ -82,9 +82,9 @@ public class ProductionType extends FreeColObject {
     /**
      * Convenience constructor for a production type with a single output.
      *
-     * @param output an <code>AbstractGoods</code> value
-     * @param center a <code>boolean</code> value
-     * @param level a <code>String</code> value
+     * @param output The <code>AbstractGoods</code> produced.
+     * @param center True if this is a colony center tile.
+     * @param level The difficulty level key.
      */
     public ProductionType(AbstractGoods output, boolean center, String level) {
         outputs = new ArrayList<AbstractGoods>();
@@ -97,9 +97,9 @@ public class ProductionType extends FreeColObject {
      * Convenience constructor for a new <code>ProductionType</code>
      * instance with a single input and output.
      *
-     * @param input a <code>GoodsType</code> value
-     * @param output a <code>GoodsType</code> value
-     * @param amount an <code>int</code> value
+     * @param input The <code>GoodsType</code> consumed.
+     * @param output The <code>GoodsType</code> produced.
+     * @param amount The amount of goods both produced and consumed.
      */
     public ProductionType(GoodsType input, GoodsType output, int amount) {
         if (input != null) {
@@ -115,165 +115,240 @@ public class ProductionType extends FreeColObject {
     /**
      * Creates a new <code>ProductionType</code> instance.
      *
-     * @param in a <code>XMLStreamReader</code> value
+     * @param in The <code>XMLStreamReader</code> to read from.
      */
     public ProductionType(XMLStreamReader in) throws XMLStreamException {
         readFromXML(in);
     }
 
     /**
-     * Get the <code>Inputs</code> value.
+     * Get the input goods.
      *
-     * @return a <code>List<AbstractGoods></code> value
+     * @return A list of the input <code>AbstractGoods</code>.
      */
     public final List<AbstractGoods> getInputs() {
         return inputs;
     }
 
     /**
-     * Set the <code>Inputs</code> value.
+     * Set the input goods.
      *
-     * @param newInputs The new Inputs value.
+     * @param newInputs The new list of input <code>AbstractGoods</code>.
      */
     public final void setInputs(final List<AbstractGoods> newInputs) {
         this.inputs = newInputs;
     }
 
     /**
-     * Get the <code>Outputs</code> value.
+     * Get the output goods.
      *
-     * @return a <code>List<AbstractGoods></code> value
+     * @return A list of the output <code>AbstractGoods</code>.
      */
     public final List<AbstractGoods> getOutputs() {
         return outputs;
     }
 
     /**
-     * Set the <code>Outputs</code> value.
+     * Set the output goods.
      *
-     * @param newOutputs The new Outputs value.
+     * @param newOutputs The new list of output <code>AbstractGoods</code>.
      */
     public final void setOutputs(final List<AbstractGoods> newOutputs) {
         this.outputs = newOutputs;
     }
 
     /**
-     * Returns true if this production type only applies to colony
-     * center tiles.
+     * Is this production from a colony center tile?
      *
-     * @return a <code>boolean</code> value
+     * @return True if this production is from a colony center tile.
      */
     public final boolean isColonyCenterTile() {
         return colonyCenterTile;
     }
 
     /**
-     * Set the <code>ColonyCenterTile</code> value.
-     *
-     * @param newColonyCenterTile The new ColonyCenterTile value.
-     */
-    public final void setColonyCenterTile(final boolean newColonyCenterTile) {
-        this.colonyCenterTile = newColonyCenterTile;
-    }
-
-    /**
      * The production level of this type of production (used by
      * difficulty levels).
      *
-     * @return a <code>String</code> value
+     * @return The production level key.
      */
     public final String getProductionLevel() {
         return productionLevel;
     }
 
     /**
-     * Set the <code>ProductionLevel</code> value.
+     * Does this production apply to a given difficulty level.
      *
-     * @param newProductionLevel The new ProductionLevel value.
+     * @param The difficulty level key to check.
+     * @return True if this production applies.
      */
-    public final void setProductionLevel(final String newProductionLevel) {
-        this.productionLevel = newProductionLevel;
+    public boolean appliesTo(String level) {
+        return level == null
+            || productionLevel == null
+            || level.equals(productionLevel);
     }
 
-    public boolean appliesTo(String level) {
-        return (level == null
-                || productionLevel == null
-                || level.equals(productionLevel));
-    }
+
+    // Serialization
+
+    private static final String COLONY_CENTER_TILE_TAG = "colonyCenterTile";
+    private static final String GOODS_TYPE_TAG = "goods-type";
+    private static final String INPUT_TAG = "input";
+    private static final String OUTPUT_TAG = "output";
+    private static final String PRODUCTION_LEVEL_TAG = "productionLevel";
+    private static final String VALUE_TAG = "value";
 
 
     /**
-     * Makes an XML-representation of this object.
-     *
-     * @param out The output stream.
-     * @throws XMLStreamException if there are any problems writing to the
-     *             stream.
+     * {@inheritDoc}
      */
+    @Override
     protected void toXMLImpl(XMLStreamWriter out) throws XMLStreamException {
-        out.writeStartElement("production");
+        super.toXML(out, getXMLElementTagName());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void writeAttributes(XMLStreamWriter out) throws XMLStreamException {
+        super.writeAttributes(out);
+
         if (colonyCenterTile) {
-            out.writeAttribute("colonyCenterTile", "true");
+            writeAttribute(out, COLONY_CENTER_TILE_TAG, colonyCenterTile);
         }
+
         if (productionLevel != null) {
-            out.writeAttribute("productionLevel", productionLevel);
+            writeAttribute(out, PRODUCTION_LEVEL_TAG, productionLevel);
         }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void writeChildren(XMLStreamWriter out) throws XMLStreamException {
+        super.writeChildren(out);
+
         if (inputs != null) {
             for (AbstractGoods input : inputs) {
-                out.writeStartElement("input");
-                out.writeAttribute("goods-type", input.getType().getId());
-                out.writeAttribute("value", Integer.toString(input.getAmount()));
+                out.writeStartElement(INPUT_TAG);
+
+                writeAttribute(out, GOODS_TYPE_TAG, input.getType());
+
+                writeAttribute(out, VALUE_TAG, input.getAmount());
+
                 out.writeEndElement();
             }
         }
+
         if (outputs != null) {
             for (AbstractGoods output : outputs) {
-                out.writeStartElement("output");
-                out.writeAttribute("goods-type", output.getType().getId());
-                out.writeAttribute("value", Integer.toString(output.getAmount()));
+                out.writeStartElement(OUTPUT_TAG);
+
+                writeAttribute(out, GOODS_TYPE_TAG, output.getType());
+
+                writeAttribute(out, VALUE_TAG, output.getAmount());
+
                 out.writeEndElement();
             }
         }
-        out.writeEndElement();
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void readAttributes(XMLStreamReader in) throws XMLStreamException {
-        colonyCenterTile = "true".equalsIgnoreCase(in.getAttributeValue(null, "colonyCenterTile"));
-        productionLevel = in.getAttributeValue(null, "productionLevel");
+        super.readAttributes(in);
+
+        colonyCenterTile = getAttribute(in, COLONY_CENTER_TILE_TAG, false);
+
+        productionLevel = getAttribute(in, PRODUCTION_LEVEL_TAG, (String)null);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void readChildren(XMLStreamReader in) throws XMLStreamException {
+        // Clear containers
+        if (inputs != null) inputs.clear();
+        if (outputs != null) outputs.clear();
+
+        super.readChildren(in);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void readChild(XMLStreamReader in) throws XMLStreamException {
-        String childName = in.getLocalName();
-        GoodsType type = getSpecification().getGoodsType(in.getAttributeValue(null, "goods-type"));
-        int amount = Integer.parseInt(in.getAttributeValue(null, "value"));
-        if ("input".equals(childName)) {
-            if (inputs == null) {
-                inputs = new ArrayList<AbstractGoods>(1);
+        final Specification spec = getSpecification();
+        final String tag = in.getLocalName();
+
+        if (INPUT_TAG.equals(tag)) {
+            GoodsType type = spec.getType(in, GOODS_TYPE_TAG,
+                                          GoodsType.class, (GoodsType)null);
+            if (type == null) {
+                logger.warning("Invalid goods type: "
+                    + in.getAttributeValue(null, GOODS_TYPE_TAG));
             }
-            inputs.add(new AbstractGoods(type, amount));
-            in.nextTag();
-        } else if ("output".equals(childName)) {
-            if (outputs == null) {
-                outputs = new ArrayList<AbstractGoods>(1);
+
+            int amount = getAttribute(in, VALUE_TAG, -1);
+            if (amount < 0) {
+                logger.warning("Invalid amount: "
+                    + in.getAttributeValue(null, VALUE_TAG));
             }
-            outputs.add(new AbstractGoods(type, amount));
+
+            if (type != null && amount >= 0) {
+                if (inputs == null) inputs = new ArrayList<AbstractGoods>(1);
+                inputs.add(new AbstractGoods(type, amount));
+            }
             in.nextTag();
+
+        } else if (OUTPUT_TAG.equals(tag)) {
+            GoodsType type = spec.getType(in, GOODS_TYPE_TAG,
+                                          GoodsType.class, (GoodsType)null);
+            if (type == null) {
+                logger.warning("Invalid goods type: "
+                    + in.getAttributeValue(null, GOODS_TYPE_TAG));
+            }
+
+            int amount = getAttribute(in, VALUE_TAG, -1);
+            if (amount < 0) {
+                logger.warning("Invalid amount: "
+                    + in.getAttributeValue(null, VALUE_TAG));
+            }
+
+            if (type != null && amount >= 00) {
+                if (outputs == null) outputs = new ArrayList<AbstractGoods>(1);
+                outputs.add(new AbstractGoods(type, amount));
+            }
+            in.nextTag();
+
         } else {
             super.readChild(in);
         }
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public String toString() {
-        StringBuilder result = new StringBuilder();
-        result.append("[production: " + productionLevel);
+        StringBuilder result = new StringBuilder("[");
+        result.append(getXMLElementTagName()).append(": ");
+        if (productionLevel != null) {
+            result.append(productionLevel);
+        }
         if (colonyCenterTile) {
             result.append(", colony center tile");
         }
         if (!(inputs == null || inputs.isEmpty())) {
             result.append(" [inputs: ");
             for (AbstractGoods input : inputs) {
-                result.append(input);
-                result.append(", ");
+                result.append(input).append(", ");
             }
             int length = result.length();
             result.replace(length - 2, length, "]");
@@ -281,8 +356,7 @@ public class ProductionType extends FreeColObject {
         if (!(outputs == null || outputs.isEmpty())) {
             result.append(" [outputs: ");
             for (AbstractGoods output : outputs) {
-                result.append(output);
-                result.append(", ");
+                result.append(output).append(", ");
             }
             int length = result.length();
             result.replace(length - 2, length, "]");
@@ -291,4 +365,12 @@ public class ProductionType extends FreeColObject {
         return result.toString();
     }
 
+    /**
+     * Gets the tag name of the root element representing this object.
+     *
+     * @return "production".
+     */
+    public static String getXMLElementTagName() {
+        return "production";
+    }
 }
