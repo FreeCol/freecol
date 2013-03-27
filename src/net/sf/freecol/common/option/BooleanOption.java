394db47edab47f4b5b9d9f9677b5784d331c4306
Michael Pope <mpope@computer.org>
2013-03-27 10:46:42 +1030

    Tidy serialization #42, BooleanOption.

--- src/net/sf/freecol/common/option/BooleanOption.java
+++ src/net/sf/freecol/common/option/BooleanOption.java
@@ -26,6 +26,7 @@ import javax.xml.stream.XMLStreamWriter;
 
 import net.sf.freecol.common.model.Specification;
 
+
 /**
  * Represents an option that can be either <i>true</i>
  * or <i>false</i>.
@@ -35,13 +36,15 @@ public class BooleanOption extends AbstractOption<Boolean> {
     @SuppressWarnings("unused")
     private static Logger logger = Logger.getLogger(BooleanOption.class.getName());
 
+    /** The value of this option. */
     private boolean value;
+
 
     /**
      * Creates a new <code>BooleanOption</code>.
      *
-     * @param id The identifier for this option. This is used when the object
-     *            should be found in an {@link OptionGroup}.
+     * @param id The identifier for this option.  This is used when
+     *     the object should be found in an {@link OptionGroup}.
      */
     public BooleanOption(String id) {
         super(id);
@@ -50,13 +53,18 @@ public class BooleanOption extends AbstractOption<Boolean> {
     /**
      * Creates a new <code>BooleanOption</code>.
      *
-     * @param specification The specification this option belongs
-     *     to. May be null.
+     * @param specification The enclosing <code>Specification</code>.
      */
     public BooleanOption(Specification specification) {
         super(specification);
     }
 
+
+    // Interface Option
+
+    /**
+     * {@inheritDoc}
+     */
     public BooleanOption clone() {
         BooleanOption result = new BooleanOption(getId());
         result.setValues(this);
@@ -64,64 +72,54 @@ public class BooleanOption extends AbstractOption<Boolean> {
     }
 
     /**
-     * Gets the current value of this <code>BooleanOption</code>.
-     * @return The value.
+     * {@inheritDoc}
      */
     public Boolean getValue() {
         return value;
     }
 
-
     /**
-     * Sets the current value of this <code>BooleanOption</code>.
-     * @param value The value.
+     * {@inheritDoc}
      */
     public void setValue(Boolean value) {
         final boolean oldValue = this.value;
         this.value = value;
 
         if (value != oldValue && isDefined) {
-            firePropertyChange(VALUE_TAG, Boolean.valueOf(oldValue), Boolean.valueOf(value));
+            firePropertyChange(VALUE_TAG, Boolean.valueOf(oldValue),
+                Boolean.valueOf(value));
         }
         isDefined = true;
     }
 
-    /**
-     * Sets the value of this Option from the given string
-     * representation. Both parameters must not be null at the same
-     * time.
-     *
-     * @param valueString the string representation of the value of
-     * this Option
-     * @param defaultValueString the string representation of the
-     * default value of this Option
-     */
-    protected void setValue(String valueString, String defaultValueString) {
-        setValue(Boolean.parseBoolean((valueString != null) ? valueString : defaultValueString));
-    }
+
+    // Override AbstractOption
 
     /**
-     * This method writes an XML-representation of this object to
-     * the given stream.
-     *
-     * @param out The target stream.
-     * @throws XMLStreamException if there are any problems writing
-     *      to the stream.
+     * {@inheritDoc}
      */
+    @Override
+    protected void setValue(String valueString, String defaultValueString) {
+        setValue(Boolean.parseBoolean((valueString != null) ? valueString
+                : defaultValueString));
+    }
+
+
+    // Serialization
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     protected void toXMLImpl(XMLStreamWriter out) throws XMLStreamException {
         super.toXML(out, getXMLElementTagName());
     }
 
     /**
-     * Write the attributes of this object to a stream.
-     *
-     * @param out The target stream.
-     * @throws XMLStreamException if there are any problems writing to
-     *     the stream.
+     * {@inheritDoc}
      */
     @Override
-    protected void writeAttributes(XMLStreamWriter out)
-        throws XMLStreamException {
+    protected void writeAttributes(XMLStreamWriter out) throws XMLStreamException {
         super.writeAttributes(out);
 
         out.writeAttribute(VALUE_TAG, Boolean.toString(value));
