e0e85ab6bdda5aae21ee6633fdb4a0fcca7b1752
Mike Pope <mpope@computer.org>
2013-05-03 20:37:03 +0930

    Allow fortified units to display in Europe, BR#2551.

--- src/net/sf/freecol/client/gui/panel/EuropePanel.java
+++ src/net/sf/freecol/client/gui/panel/EuropePanel.java
@@ -559,7 +559,8 @@ public final class EuropePanel extends PortPanel {
         public boolean accepts(Unit unit) {
             if (!unit.isNaval()) return false;
             switch (unit.getState()) {
-            case ACTIVE: case SENTRY: case SKIPPED:
+            case ACTIVE: case FORTIFIED: case FORTIFYING:
+            case SENTRY: case SKIPPED:
                 return true;
             }
             return false;
