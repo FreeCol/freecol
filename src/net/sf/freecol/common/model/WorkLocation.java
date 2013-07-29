FreeCol/freecol

commit e5761db8890edc7e4f0aae7e6beae087aa3f7c30

Hoist code to WorkLocation.add().

src/net/sf/freecol/common/model/WorkLocation.java
@@ -312,6 +312,7 @@ public abstract class WorkLocation extends UnitLocation implements Ownable {
     /**
      * {@inheritDoc}
      */
+    @Override
     public final Tile getTile() {
         return colony.getTile();
     }
@@ -319,37 +320,61 @@ public abstract class WorkLocation extends UnitLocation implements Ownable {
     /**
      * {@inheritDoc}
      */
+    @Override
     public boolean add(final Locatable locatable) {
-        if (!(locatable instanceof Unit)) {
-            throw new IllegalStateException("Not a unit: " + locatable);
+        NoAddReason reason = getNoAddReason(locatable);
+        if (reason != NoAddReason.NONE) {
+            throw new IllegalStateException("Can not add " + locatable
+                + " to " + toString() + " because " + reason);
+        }
+        if (!super.add(locatable)) return false;
+        Unit unit = (Unit)locatable;
+
+        unit.setState(Unit.UnitState.IN_COLONY);
+        unit.setMovesLeft(0);
+
+        // Choose a sensible work type only if none already specified
+        // or it is not useful at this location.
+        ProductionType best = null;
+        if (unit.getWorkType() != null) {
+            if ((best = getBestProductionType(unit.getWorkType())) != null) {
+                setProductionType(best);
+            }
+        }
+        if (best == null) {
+            if ((best = getBestProductionType(unit)) != null) {
+                setProductionType(best);
+                // TODO: unit work type needs to be a production
+                // type rather than a goods type
+                unit.changeWorkType(best.getOutputs().get(0).getType());
+            }
         }
 
-        Unit unit = (Unit) locatable;
-        if (super.add(unit)) {
-            if (this.canTeach()) {
-                Unit student = unit.getStudent();
-                if (student == null
-                    && (student = getColony().findStudent(unit)) != null) {
-                    unit.setStudent(student);
-                    student.setTeacher(unit);
-                }
-                unit.changeWorkType(null);
-            } else {
-                Unit teacher = unit.getTeacher();
-                if (teacher == null
-                    && (teacher = getColony().findTeacher(unit)) != null) {
-                    unit.setTeacher(teacher);
-                    teacher.setStudent(unit);
-                }
+        if (this.canTeach()) {
+            Unit student = unit.getStudent();
+            if (student == null
+                && (student = getColony().findStudent(unit)) != null) {
+                unit.setStudent(student);
+                student.setTeacher(unit);
             }
-            return true;
+            unit.changeWorkType(null);
+        } else {
+            Unit teacher = unit.getTeacher();
+            if (teacher == null
+                && (teacher = getColony().findTeacher(unit)) != null) {
+                unit.setTeacher(teacher);
+                teacher.setStudent(unit);
+            }
         }
-        return false;
+
+        colony.invalidateCache();
+        return true;
     }
 
     /**
      * {@inheritDoc}
      */
+    @Override
     public boolean remove(final Locatable locatable) {
         if (!(locatable instanceof Unit)) {
             throw new IllegalStateException("Not a unit: " + locatable);
@@ -380,6 +405,7 @@ public abstract class WorkLocation extends UnitLocation implements Ownable {
     /**
      * {@inheritDoc}
      */
+    @Override
     public final Settlement getSettlement() {
         return colony;
     }
@@ -464,10 +490,26 @@ public abstract class WorkLocation extends UnitLocation implements Ownable {
      * @param unit The <code>Unit</code> to check.
      * @return The best production type.
      */
-    public abstract ProductionType getBestProductionType(Unit unit);
+    public ProductionType getBestProductionType(Unit unit) {
+        ProductionType best = null;
+        int amount = 0;
+        for (ProductionType productionType : getProductionTypes()) {
+            if (productionType.getOutputs() != null) {
+                for (AbstractGoods output : productionType.getOutputs()) {
+                    int newAmount = getPotentialProduction(output.getType(),
+                                                           unit.getType());
+                    if (newAmount > amount) {
+                        amount = newAmount;
+                        best = productionType;
+                    }
+                }
+            }
+        }
+        return best;
+    }
 
     /**
-     * Returns the best production type for the production of the
+     * Gets the best production type for the production of the
      * given goods type.  This method is likely to be removed in the
      * future.
      *

src/net/sf/freecol/common/model/Building.java
@@ -407,35 +407,9 @@ public class Building extends WorkLocation implements Named, Comparable<Building
     /**
      * {@inheritDoc}
      */
-    @Override
     public StringTemplate getLocationName() {
         return StringTemplate.template("inLocation")
             .add("%location%", getNameKey());
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean add(final Locatable locatable) {
-        NoAddReason reason = getNoAddReason(locatable);
-        if (reason != NoAddReason.NONE) {
-            throw new IllegalStateException("Can not add " + locatable
-                + " to " + toString() + " because " + reason);
-        }
-        Unit unit = (Unit) locatable;
-        if (contains(unit)) return true;
-
-        if (super.add(unit)) {
-            unit.setState(Unit.UnitState.IN_COLONY);
-            List<AbstractGoods> outputs = getOutputs();
-            if (outputs.size() == 1) {
-                unit.changeWorkType(outputs.get(0).getType());
-            }
-            getColony().invalidateCache();
-            return true;
-        }
-        return false;
     }
 
 
@@ -452,7 +426,7 @@ public class Building extends WorkLocation implements Named, Comparable<Building
     public NoAddReason getNoAddReason(Locatable locatable) {
         if (!(locatable instanceof Unit)) return NoAddReason.WRONG_TYPE;
         NoAddReason reason = getNoWorkReason();
-        Unit unit = (Unit) locatable;
+        Unit unit = (Unit)locatable;
         BuildingType type = getType();
 
         return (reason != NoAddReason.NONE) ? reason
@@ -562,15 +536,6 @@ public class Building extends WorkLocation implements Named, Comparable<Building
      */
     public List<ProductionType> getProductionTypes() {
         return getType().getProductionTypes();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public ProductionType getBestProductionType(Unit unit) {
-        // TODO: think of something better
-        return getType().getProductionTypes().isEmpty()
-            ? null : getType().getProductionTypes().get(0);
     }
 
 

src/net/sf/freecol/common/model/ColonyTile.java
@@ -233,40 +233,6 @@ public class ColonyTile extends WorkLocation {
                 .addName("%location%", name);
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    public boolean add(final Locatable locatable) {
-        NoAddReason reason = getNoAddReason(locatable);
-        if (reason != NoAddReason.NONE) {
-            throw new IllegalStateException("Can not add " + locatable
-                + " to " + toString() + " because " + reason);
-        }
-        Unit unit = (Unit) locatable;
-        if (contains(unit)) return true;
-
-        if (super.add(unit)) {
-            unit.setState(Unit.UnitState.IN_COLONY);
-
-            // Choose a sensible work type only if none already specified.
-            if (unit.getWorkType() == null) {
-                ProductionType best = getBestProductionType(unit);
-                if (best != null) {
-                    setProductionType(best);
-                    // TODO: unit work type needs to be a production
-                    // type rather than a goods type
-                    unit.changeWorkType(best.getOutputs().get(0).getType());
-                }
-            } else {
-                setProductionType(getBestProductionType(unit.getWorkType()));
-            }
-
-            getColony().invalidateCache();
-            return true;
-        }
-        return false;
-    }
-
 
     // Interface UnitLocation
     // Inherits:
@@ -430,26 +396,6 @@ public class ColonyTile extends WorkLocation {
         } else {
             return new ArrayList<ProductionType>();
         }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public ProductionType getBestProductionType(Unit unit) {
-        ProductionType best = null;
-        int amount = 0;
-        for (ProductionType productionType : getProductionTypes()) {
-            if (productionType.getOutputs() != null) {
-                for (AbstractGoods output : productionType.getOutputs()) {
-                    int newAmount = getPotentialProduction(output.getType(), unit.getType());
-                    if (newAmount > amount) {
-                        amount = newAmount;
-                        best = productionType;
-                    }
-                }
-            }
-        }
-        return best;
     }
 
     /**

commit 0cb3a44e037a36808c2794f5976f76884e8e6500

Attempt to fix reversion caused by e5761db.

src/net/sf/freecol/common/model/WorkLocation.java
@@ -327,8 +327,9 @@ public abstract class WorkLocation extends UnitLocation implements Ownable {
             throw new IllegalStateException("Can not add " + locatable
                 + " to " + toString() + " because " + reason);
         }
-        if (!super.add(locatable)) return false;
         Unit unit = (Unit)locatable;
+        if (contains(unit)) return true;
+        if (!super.add(unit)) return false;
 
         unit.setState(Unit.UnitState.IN_COLONY);
         unit.setMovesLeft(0);