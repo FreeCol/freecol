2124c6944be8de1e5bb832b33da4dee1fc85b8cb
Michael Pope <mpope@computer.org>
2013-09-02 09:36:47 +0930

    Fix missing i18n in ChatPanel.

--- src/net/sf/freecol/client/gui/panel/ChatPanel.java
+++ src/net/sf/freecol/client/gui/panel/ChatPanel.java
@@ -29,6 +29,7 @@ import javax.swing.JTextField;
 
 import net.sf.freecol.client.FreeColClient;
 import net.sf.freecol.client.gui.GUI;
+import net.sf.freecol.client.gui.i18n.Messages;
 
 
 /**
@@ -36,7 +37,7 @@ import net.sf.freecol.client.gui.GUI;
  * message to the other players. There is no close button because it
  * closes as soon as the user presses enter in the textfield.
  */
-public final class ChatPanel extends FreeColPanel implements ActionListener {
+public final class ChatPanel extends FreeColPanel {
 
     private static final Logger logger = Logger.getLogger(ChatPanel.class.getName());
 
@@ -51,16 +52,12 @@ public final class ChatPanel extends FreeColPanel implements ActionListener {
      * @param freeColClient The <code>FreeColClient</code> for the game.
      */
     public ChatPanel(FreeColClient freeColClient) {
-        super(freeColClient);
+        super(freeColClient, new BorderLayout(10, 10));
 
-        JLabel label = new JLabel("Message: ");
+        JLabel label = new JLabel(Messages.message("message") + ": ");
 
         field = new JTextField("", 40);
-
-        setLayout(new BorderLayout(10, 10));
-
         field.setActionCommand(String.valueOf(CHAT));
-
         field.addActionListener(this);
 
         add(label);
@@ -71,6 +68,27 @@ public final class ChatPanel extends FreeColPanel implements ActionListener {
         field.setFocusable(true);
 
         setSize(getPreferredSize());
+    }
+
+
+    /**
+     * Requests that the chat textfield in this chat panel gets the focus.
+     */
+    @Override
+    public void requestFocus() {
+        field.requestFocus();
+    }
+
+    /**
+     * Gets the chat message that the user has entered and clears the
+     * chat textfield to make room for a new message.
+     *
+     * @return The chat message.
+     */
+    public String getChatText() {
+        String message = field.getText();
+        field.setText("");
+        return message;
     }
 
     /**
@@ -96,27 +114,5 @@ public final class ChatPanel extends FreeColPanel implements ActionListener {
         } catch (NumberFormatException e) {
             logger.warning("Invalid Actioncommand: not a number: " + command);
         }
-    }
-
-
-    /**
-     * Returns the chat message that the user has entered and clears
-     * the chat textfield to make room for a new message.
-     *
-     * @return The chat message that the user has entered and clears the chat
-     *     textfield to make room for a new message.
-     */
-    public String getChatText() {
-        String message = field.getText();
-        field.setText("");
-        return message;
-    }
-
-    /**
-     * Requests that the chat textfield in this chat panel gets the focus.
-     */
-    @Override
-    public void requestFocus() {
-        field.requestFocus();
     }
 }
