295f62dde82e407248a4594424377fcddad93a66
Michael Pope <mpope@computer.org>
2014-07-02 16:30:41 +0930

    Add index to resource modifiers.

--- src/net/sf/freecol/common/model/ResourceType.java
+++ src/net/sf/freecol/common/model/ResourceType.java
@@ -87,17 +87,6 @@ public final class ResourceType extends FreeColGameObjectType {
     }
 
 
-    // Override FreeColGameObjectType
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getModifierIndex(String id) {
-        return Modifier.RESOURCE_PRODUCTION_INDEX;
-    }
-
-
     // Serialization
 
     private static final String MAXIMUM_VALUE_TAG = "maximum-value";
