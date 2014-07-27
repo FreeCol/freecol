78e06b4c9a3029195a8fc17e2c0035f32d9073f7
Mike Pope <mpope@computer.org>
2014-07-27 21:13:21 +0930

    Fix dodgy format and string handling in Rule.

--- src/net/sf/freecol/client/gui/i18n/Rule.java
+++ src/net/sf/freecol/client/gui/i18n/Rule.java
@@ -32,10 +32,11 @@ import java.util.StringTokenizer;
  */
 public class Rule {
 
-    List<List<Relation>> conditions = new ArrayList<List<Relation>>();
+    private List<List<Relation>> conditions = new ArrayList<List<Relation>>();
+
 
     public Rule(String input) {
-	parse(input);
+        parse(input);
     }
 
 
@@ -45,7 +46,7 @@ public class Rule {
      * @param condition a list of relations combined with the "and" operator
      */
     public void add(List<Relation> condition) {
-	conditions.add(condition);
+        conditions.add(condition);
     }
 
     /**
@@ -55,17 +56,15 @@ public class Rule {
      * @return a <code>boolean</code> value
      */
     public boolean matches(double number) {
-
-	outer: for (List<Relation> andCondition : conditions) {
-	    for (Relation relation : andCondition) {
-		if (!relation.matches(number)) {
-		    continue outer;
-		}
-	    }
-	    return true;
-	}
-	return false;
-
+        outer: for (List<Relation> andCondition : conditions) {
+            for (Relation relation : andCondition) {
+                if (!relation.matches(number)) {
+                    continue outer;
+                }
+            }
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -73,50 +72,49 @@ public class Rule {
      *
      * @param input a <code>String</code> value
      */
-    public void parse(String input) {
-	StringTokenizer st = new StringTokenizer(input.toLowerCase(Locale.US), " .");
-	List<String> tokens = new ArrayList<String>();
-	while (st.hasMoreTokens()) {
-	    String token = st.nextToken();
-	    if ("or".equals(token)) {
-		conditions.add(parseCondition(tokens));
-		tokens.clear();
-	    } else {
-		tokens.add(token);
-	    }
-	}
-	conditions.add(parseCondition(tokens));
+    public final void parse(String input) {
+        StringTokenizer st = new StringTokenizer(input.toLowerCase(Locale.US), " .");
+        List<String> tokens = new ArrayList<String>();
+        while (st.hasMoreTokens()) {
+            String token = st.nextToken();
+            if ("or".equals(token)) {
+                conditions.add(parseCondition(tokens));
+                tokens.clear();
+            } else {
+                tokens.add(token);
+            }
+        }
+        conditions.add(parseCondition(tokens));
     }
 
     private List<Relation> parseCondition(List<String> input) {
-	List<String> tokens = new ArrayList<String>();
-	List<Relation> result = new ArrayList<Relation>();
-	for (String token : input) {
-	    if ("and".equals(token)) {
-		result.add(new Relation(tokens));
-		tokens.clear();
-	    } else {
-		tokens.add(token);
-	    }
-	}
-	result.add(new Relation(tokens));
-	return result;
+        List<String> tokens = new ArrayList<String>();
+        List<Relation> result = new ArrayList<Relation>();
+        for (String token : input) {
+            if ("and".equals(token)) {
+                result.add(new Relation(tokens));
+                tokens.clear();
+            } else {
+                tokens.add(token);
+            }
+        }
+        result.add(new Relation(tokens));
+        return result;
     }
 
     @Override
     public String toString() {
-
-	String result = new String();
-	for (List<Relation> andCondition : conditions) {
-	    String condition = new String();
-	    for (Relation relation : andCondition) {
-		condition += " and " + relation.toString();
-	    }
-	    result += " or " + condition.substring(5);
-	}
-	return result.substring(4);
-
+        final String andString = " and ";
+        final String orString = " or ";
+        StringBuilder sb = new StringBuilder(32);
+        for (List<Relation> andCondition : conditions) {
+            for (Relation relation : andCondition) {
+                sb.append(relation).append(andString);
+            }
+            sb.setLength(sb.length() - andString.length());
+            sb.insert(0, orString);
+        }
+        sb.delete(0, orString.length());
+        return sb.toString();
     }
-
-
 }
