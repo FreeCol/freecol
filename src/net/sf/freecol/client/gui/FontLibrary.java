59e7abf2675c9c83870d78bc0db9a3ad41dccf30
Sebastian Zhorel <sebastian-zhorel@email.de>
2015-04-05 14:10:55 +0200

    Clean up FontLibrary, adding more comments to show intentional uses

--- src/net/sf/freecol/client/gui/FontLibrary.java
+++ src/net/sf/freecol/client/gui/FontLibrary.java
@@ -24,25 +24,22 @@ import java.util.logging.Logger;
 import net.sf.freecol.common.resources.ResourceManager;
 
 /**
- * Wraps anything Font related.
+ * Wraps anything <code>Font</code> related and contains a scale factor.
  * 
- * Changes can be made with <code>FontType</code and <code>FontSize</code><br>
- * <br>
- * FontType Sets the typeface. Choices are: NORMAL, SIMPLE or HEADER.<br>
- * FontSize Sets the font size. Choices are: TINY, SMALLER, SMALL, MEDIUM or BIG
+ * Should be used for getting a <code>Font</code> everywhere it is needed.
  */
 public class FontLibrary {
 
     private static final Logger logger = Logger.getLogger(FontLibrary.class.getName());
 
     /**
-     * Sets the typeface of the font.
+     * FontType is used for choosing the typeface of the <code>Font</code>.
      * 
      * Choices are:
      * <ul>
-     * <li>NORMAL -- Sets the typeface to be the normal system typeface.</li>
-     * <li>SIMPLE</li>
-     * <li>HEADER -- Sets the typeface to be the stylized Blackletter typeface.</li>
+     * <li>NORMAL -- a normal system typeface</li>
+     * <li>SIMPLE -- a simple typeface</li>
+     * <li>HEADER -- a stylized old-fashioned typeface for headers</li>
      * </ul>
      */
     public static enum FontType {
@@ -52,16 +49,15 @@ public class FontLibrary {
     }
 
     /**
-     * Sets the size of the font. This size is later multiplied by the
-     * ScaleFactor, currently set to "1".
+     * FontSize allows for choosing the relative size of the <code>Font</code>.
      * 
      * Choices are:
      * <ul>
-     * <li>TINY sets the font to be 12px</li>
-     * <li>SMALLER sets the font to be 16px</li>
-     * <li>SMALL sets the font to be 24px</li>
-     * <li>MEDIUM sets the font to be 36px</li>
-     * <li>BIG sets the font to be 48px</li>
+     * <li>TINY -- used for normal text</li>
+     * <li>SMALLER -- used for subsubheaders</li>
+     * <li>SMALL -- used for subheaders</li>
+     * <li>MEDIUM -- used for some headers</li>
+     * <li>BIG -- used for panel headers</li>
      * </ul>
      */
     public static enum FontSize {
@@ -72,16 +68,34 @@ public class FontLibrary {
         BIG
     }
 
+    /**
+     * How much the font size is scaled.
+     */
     private final float scaleFactor;
 
+    /**
+     * Create a <code>FontLibrary</code> without scaling.
+     */
     public FontLibrary() {
         this.scaleFactor = 1f;
     }
 
+    /**
+     * Create a <code>FontLibrary</code> with scaling.
+     * 
+     * @param scaleFactor How much scaling should be applied.
+     *                    Typically the same value as in ImageLibrary.
+     */
     public FontLibrary(float scaleFactor) {
         this.scaleFactor = scaleFactor;
     }
 
+    /**
+     * Create a default <code>Font</code> set on initialization of the GUI.
+     * 
+     * @param fontName Can be used to choose a different font from a
+     *                 user-provided name.
+    */
     static Font createMainFont(String fontName) {
         if (fontName != null) {
             Font font = Font.decode(fontName);
@@ -96,7 +110,16 @@ public class FontLibrary {
         return createFont(fontType, fontSize, Font.PLAIN, scaleFactor);
     }
 
-    public Font createScaledFont(FontType fontType, FontSize fontSize, int style) {
+    /**
+     * Create a scaled <code>Font</code>.
+     * 
+     * @param fontType How the font should look like.
+     * @param fontSize Its relative size.
+     * @param style The font style for choosing plain, bold or italic.
+     * @return The created Font.
+     */
+    public Font createScaledFont(FontType fontType, FontSize fontSize,
+                                 int style) {
         return createFont(fontType, fontSize, style, scaleFactor);
     }
 
@@ -104,15 +127,36 @@ public class FontLibrary {
         return createFont(fontType, fontSize, Font.PLAIN, 1f);
     }
 
-    public static Font createFont(FontType fontType, FontSize fontSize, int style) {
+    /**
+     * Create a <code>Font</code> in rare case one is needed without scaling.
+     * 
+     * @param fontType How the font should look like.
+     * @param fontSize Its size.
+     * @param style The font style for choosing plain, bold or italic.
+     * @return The created Font.
+     */
+    public static Font createFont(FontType fontType, FontSize fontSize,
+                                  int style) {
         return createFont(fontType, fontSize, style, 1f);
     }
 
-    public static Font createFont(FontType fontType, FontSize fontSize, float scaleFactor) {
+    public static Font createFont(FontType fontType, FontSize fontSize,
+                                  float scaleFactor) {
         return createFont(fontType, fontSize, Font.PLAIN, scaleFactor);
     }
 
-    public static Font createFont(FontType fontType, FontSize fontSize, int style, float scaleFactor) {
+    /**
+     * Create a scaled <code>Font</code> whe the scale factor is provided
+     * explicitly in the parameter.
+     * 
+     * @param fontType How the font should look like.
+     * @param fontSize Its relative size.
+     * @param style The font style for choosing plain, bold or italic.
+     * @param scaleFactor The applied scale factor.
+     * @return The created Font.
+     */
+    public static Font createFont(FontType fontType, FontSize fontSize,
+                                  int style, float scaleFactor) {
         String fontName;
         switch(fontType) {
             default:
@@ -145,7 +189,7 @@ public class FontLibrary {
             case BIG:
                 pixelSize = 48f;
         }
-        float scaledSize = pixelSize*scaleFactor;
+        float scaledSize = pixelSize * scaleFactor;
         Font font = ResourceManager.getFont(fontName);
         font = font.deriveFont(style, scaledSize);
         return font;
