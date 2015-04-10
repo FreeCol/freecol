5f4f2d53ee7d42212d80389e43fee3caef52b43b
Sebastian Zhorel <sebastian-zhorel@email.de>
2015-04-10 23:52:04 +0200

    Optimize constructor of ReportForeignAffairPanel a bit, as its visibly slow

--- src/net/sf/freecol/client/gui/panel/ReportForeignAffairPanel.java
+++ src/net/sf/freecol/client/gui/panel/ReportForeignAffairPanel.java
@@ -20,17 +20,21 @@
 package net.sf.freecol.client.gui.panel;
 
 import java.awt.Font;
+import java.awt.Image;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
 import net.miginfocom.swing.MigLayout;
 
 import net.sf.freecol.client.FreeColClient;
 import net.sf.freecol.client.gui.FontLibrary;
+import net.sf.freecol.client.gui.ImageLibrary;
 import net.sf.freecol.common.i18n.Messages;
 import net.sf.freecol.common.model.NationSummary;
 import net.sf.freecol.common.model.Player;
+import net.sf.freecol.common.model.StringTemplate;
 
 
 /**
@@ -46,73 +50,92 @@ public final class ReportForeignAffairPanel extends ReportPanel {
     public ReportForeignAffairPanel(FreeColClient freeColClient) {
         super(freeColClient, "reportForeignAction");
 
+        final ImageLibrary lib = freeColClient.getGUI().getImageLibrary();
+        final Font font = FontLibrary.createFont(FontLibrary.FontType.NORMAL,
+            FontLibrary.FontSize.SMALL, Font.BOLD, lib.getScalingFactor());
+        final String stance = Messages.message(
+            StringTemplate.key("report.stance"));
+        final String numberOfColonies = Messages.message(
+            StringTemplate.key("report.numberOfColonies"));
+        final String numberOfUnits = Messages.message(
+            StringTemplate.key("report.numberOfUnits"));
+        final String militaryStrength = Messages.message(
+            StringTemplate.key("report.militaryStrength"));
+        final String navalStrength = Messages.message(
+            StringTemplate.key("report.navalStrength"));
+        final String goldTitle = Messages.message(
+            StringTemplate.key("goldTitle"));
+        final String continentalCongress_title = Messages.message(
+            StringTemplate.key("report.continentalCongress.title"));
+        final String tax = Messages.message(
+            StringTemplate.key("tax"));
+        final String sonsOfLiberty = Messages.message(
+            StringTemplate.key("report.sonsOfLiberty"));
+
         // Display Panel
         reportPanel.removeAll();
         reportPanel.setLayout(new MigLayout("wrap 2", "[]push[]",
                                             "[align top]"));
 
+        int n;
         for (Player enemy : getGame().getLiveEuropeanPlayers(null)) {
-            int n;
             NationSummary ns = igc().getNationSummary(enemy);
             if (ns == null) continue;
-            JPanel enemyPanel = new MigPanel();
-            enemyPanel.setLayout(new MigLayout("gapy 0",
+
+            JPanel enemyPanel = new MigPanel(new MigLayout("gapy 0",
                                                "[][]20[align right]0[]", ""));
             enemyPanel.setOpaque(false);
-            JLabel coatLabel = new JLabel();
-            final ImageIcon coatOfArms = new ImageIcon(freeColClient.getGUI()
-                .getImageLibrary().getImage(enemy.getNation()));
-            if (coatOfArms != null) {
-                coatLabel.setIcon(coatOfArms);
-            }
+            final Image coatOfArms = lib.getImage(enemy.getNation());
+            JLabel coatLabel = (coatOfArms != null)
+                ? new JLabel(new ImageIcon(coatOfArms))
+                : new JLabel();
             enemyPanel.add(coatLabel, "spany, aligny top");
-            
+
             JLabel label = Utility.localizedLabel(enemy.getNationName());
-            Font font = FontLibrary.createFont(FontLibrary.FontType.NORMAL, FontLibrary.FontSize.SMALL, Font.BOLD);
             label.setFont(font);
             enemyPanel.add(label, "wrap 12");
-            
+
             //TODO: Get stance only if not player's Nation.
-            enemyPanel.add(Utility.localizedLabel("report.stance"), "newline");
+            enemyPanel.add(new JLabel(stance), "newline");
             enemyPanel.add(Utility.localizedLabel(ns.getStance().getLabel()));
 
             n = ns.getNumberOfSettlements();
-            enemyPanel.add(Utility.localizedLabel("report.numberOfColonies"), "newline");
+            enemyPanel.add(new JLabel(numberOfColonies), "newline");
             enemyPanel.add(new JLabel(Integer.toString(n)));
 
             n = ns.getNumberOfUnits();
-            enemyPanel.add(Utility.localizedLabel("report.numberOfUnits"), "newline");
+            enemyPanel.add(new JLabel(numberOfUnits), "newline");
             enemyPanel.add(new JLabel(Integer.toString(n)));
 
             n = ns.getMilitaryStrength();
-            enemyPanel.add(Utility.localizedLabel("report.militaryStrength"), "newline");
+            enemyPanel.add(new JLabel(militaryStrength), "newline");
             enemyPanel.add(new JLabel(Integer.toString(n)));
 
             n = ns.getNavalStrength();
-            enemyPanel.add(Utility.localizedLabel("report.navalStrength"), "newline");
+            enemyPanel.add(new JLabel(navalStrength), "newline");
             enemyPanel.add(new JLabel(Integer.toString(n)));
 
             n = ns.getGold();
-            enemyPanel.add(Utility.localizedLabel("goldTitle"), "newline");
+            enemyPanel.add(new JLabel(goldTitle), "newline");
             enemyPanel.add(new JLabel(Integer.toString(n)));
 
             n = ns.getFoundingFathers();
             if (n >= 0) {
-                enemyPanel.add(Utility.localizedLabel("report.continentalCongress.title"),
+                enemyPanel.add(new JLabel(continentalCongress_title),
                                "newline 8");
                 enemyPanel.add(new JLabel(Integer.toString(n)));
             }
 
             n = ns.getTax();
             if (n >= 0) {
-                enemyPanel.add(Utility.localizedLabel("tax"), "newline");
+                enemyPanel.add(new JLabel(tax), "newline");
                 enemyPanel.add(new JLabel(Integer.toString(n)));
                 enemyPanel.add(new JLabel("%"));
             }
 
             n = ns.getSoL();
             if (n >= 0) {
-                enemyPanel.add(Utility.localizedLabel("report.sonsOfLiberty"), "newline");
+                enemyPanel.add(new JLabel(sonsOfLiberty), "newline");
                 enemyPanel.add(new JLabel(Integer.toString(n)));
                 enemyPanel.add(new JLabel("%"));
             }
