9e8dc92765409a3dd35572a06d5d755fe9b190a0
Mike Pope <mpope@computer.org>
2015-05-10 18:46:29 +0930

    Add StringUtils.splitText.

--- src/net/sf/freecol/common/util/StringUtils.java
+++ src/net/sf/freecol/common/util/StringUtils.java
@@ -19,6 +19,8 @@
 
 package net.sf.freecol.common.util;
 
+import java.awt.FontMetrics;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -149,5 +151,53 @@ public class StringUtils {
             }
         }
         return -1;
+    }
+
+    /**
+     * Split some text at word boundaries into a list of strings that
+     * take up no more than a given width.
+     *
+     * @param text The text to split.
+     * @param delim Characters to consider as word delimiting.
+     * @param fontMetrics <code>FontMetrics</code> used to calculate
+     *     text width.
+     * @param width The text width maximum.
+     * @return A list of split text.
+     */
+    public static List<String> splitText(String text, String delim,
+                                         FontMetrics fontMetrics, int width) {
+        List<String> result = new ArrayList<>();
+        final int len = text.length();
+        int i = 0, start;
+        String top = "";
+        Character d = null;
+        for (;;) {
+            for (; i < len; i++) {
+                if (delim.indexOf(text.charAt(i)) < 0) break;
+            }
+            if (i >= len) break;
+            start = i;
+            for (; i < len; i++) {
+                if (delim.indexOf(text.charAt(i)) >= 0) break;
+            }
+            String s = text.substring(start, i);
+            String t = (top.isEmpty()) ? s : top + d + s;
+            if (fontMetrics.stringWidth(t) > width) {
+                if (top.isEmpty()) {
+                    result.add(s);
+                } else {
+                    result.add(top);
+                    top = s;
+                }
+            } else {
+                top = t;
+            }
+            if (i >= len) {
+                if (!top.isEmpty()) result.add(top);
+                break;
+            }
+            d = text.charAt(i);
+        }
+        return result;
     }
 }
