2564efd86b0bd33cff71a4fcc664b0ffdc006a01
Mike Pope <mpope@computer.org>
2015-05-18 18:22:47 +0930

    Remove Java8 Map.remove(K,V) call.

--- src/net/sf/freecol/server/ai/EuropeanAIPlayer.java
+++ src/net/sf/freecol/server/ai/EuropeanAIPlayer.java
@@ -910,7 +910,7 @@ public class EuropeanAIPlayer extends AIPlayer {
      */
     public void removeTileImprovementPlan(TileImprovementPlan plan) {
         if (plan == null) return;
-        if (plan.getTarget() != null) tipMap.remove(plan.getTarget(), plan);
+        if (plan.getTarget() != null) tipMap.remove(plan.getTarget());
         for (AIColony aic : getAIColonies()) {
             if (aic.removeTileImprovementPlan(plan)) break;
         }
