5697c48533f131c12e3328484c69c7ddafd02b19
Mike Pope <mpope@computer.org>
2015-07-01 21:07:47 +0930

    Fixes to website upload script.

--- bin/website.sh
+++ bin/website.sh
@@ -8,10 +8,7 @@ fi
 WEBDIR=www.freecol.org
 
 PREV=`git log --pretty=oneline "$WEBDIR" | sed -n -e '2s/^\([^ ]*\).*$/\1/p'`
-cd "$WEBDIR"
 {
     echo "cd /home/project-web/freecol/htdocs"
-    for f in `git diff --name-only "$PREV" HEAD "$WEBDIR" | sed -e "s|^$WEBDIR/||"` ; do
-        echo "put $f $f"
-    done
-} | sftp $USERNAME,freecol@web.sf.net
+    git diff --name-only "$PREV" HEAD -- "$WEBDIR" | sed -e 's|^[^/]*/\(.*\)$|put \1 \1|p'
+} | (cd "$WEBDIR" ; sftp $USERNAME,freecol@web.sf.net)
