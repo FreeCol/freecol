8d21822c976165c86e16521ae523ba41ccc01b5b
Sebastian Zhorel <sebastian-zhorel@email.de>
2015-08-07 18:04:56 +0200

    Add static to MapViewer.createLabel

--- src/net/sf/freecol/client/gui/MapViewer.java
+++ src/net/sf/freecol/client/gui/MapViewer.java
@@ -1238,8 +1238,8 @@ public final class MapViewer {
      * @param backgroundColor a <code>Color</code>
      * @return an <code>BufferedImage</code>
      */
-    private BufferedImage createLabel(Graphics2D g, String text, Font font,
-                              Color backgroundColor) {
+    private static BufferedImage createLabel(Graphics2D g, String text,
+                                             Font font, Color backgroundColor) {
         TextSpecification[] specs = new TextSpecification[1];
         specs[0] = new TextSpecification(text, font);
         return createLabel(g, specs, backgroundColor);
@@ -1254,8 +1254,9 @@ public final class MapViewer {
      * @param backgroundColor a <code>Color</code>
      * @return a <code>BufferedImage</code>
      */
-    private BufferedImage createLabel(Graphics2D g, TextSpecification[] textSpecs,
-                              Color backgroundColor) {
+    private static BufferedImage createLabel(Graphics2D g,
+                                             TextSpecification[] textSpecs,
+                                             Color backgroundColor) {
         int hPadding = 15;
         int vPadding = 10;
         int linePadding = 5;
