c9521c8690449526a2d0d1199e7fd096cf9c3e53
Michael Pope <mpope@computer.org>
2016-05-19 14:51:46 +0930

    Implement hashCode() for Operand.

--- src/net/sf/freecol/common/model/Operand.java
+++ src/net/sf/freecol/common/model/Operand.java
@@ -306,6 +306,18 @@ public class Operand extends Scope {
                 && super.equals(o));
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        int hash = super.hashCode();
+        hash = 31 * hash + this.operandType.ordinal();
+        hash = 31 * hash + this.scopeLevel.ordinal();
+        hash = 31 * hash + this.value;
+        return hash;
+    }
+
 
     // Serialization
 
