74a2d1f1e1f1baefea188807de479f79a2f5d35f
Mike Pope <mpope@computer.org>
2016-06-12 09:06:58 +0930

    Make sure concat() returns non-null stream.

--- src/net/sf/freecol/common/util/CollectionUtils.java
+++ src/net/sf/freecol/common/util/CollectionUtils.java
@@ -522,7 +522,7 @@ public class CollectionUtils {
     @SafeVarargs
     public static <T> Stream<T> concat(Stream<T> s0, Stream<T>... streams) {
         Stream<T>[] sts = streams;
-        Stream<T> ret = s0;
+        Stream<T> ret = (s0 == null) ? Stream.empty() : s0;
         for (int i = 0; i < sts.length; i++) {
             if (sts[i] != null) ret = Stream.concat(ret, sts[i]);
         }
