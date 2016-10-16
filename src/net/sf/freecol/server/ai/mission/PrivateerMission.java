d6dfcc2f0946d71ded7852a9ef700eb7ffc037bd
calebrw <calebrw@gmail.com>
2016-10-15 19:16:59 -0500

    Remove some duplicity

--- src/net/sf/freecol/server/ai/mission/PrivateerMission.java
+++ src/net/sf/freecol/server/ai/mission/PrivateerMission.java
@@ -147,12 +147,13 @@ public class PrivateerMission extends Mission {
      */
     public static int scorePath(AIUnit aiUnit, PathNode path) {
         Location loc = extractTarget(aiUnit, path);
+        final int totalTurns = path.getTotalTurns() + 1;
         if (loc instanceof Europe || loc instanceof Colony) {
-            return 1000 / (path.getTotalTurns() + 1);
+            return 1000 / totalTurns;
         } else if (loc instanceof Unit) {
-            return scoreUnit(aiUnit, (Unit)loc) / (path.getTotalTurns() + 1);
+            return scoreUnit(aiUnit, (Unit)loc) / totalTurns;
         } else if (loc instanceof Tile) {
-            return 50 / (path.getTotalTurns() + 1);
+            return 50 / totalTurns;
         } else {
             return Integer.MIN_VALUE;
         }
