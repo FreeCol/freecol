2ae90a81592e38c78c285eb8aecf08bea09d79f9
Sebastian Zhorel <sebastian-zhorel@email.de>
2017-02-07 16:51:54 +0100

    Fix changing tile type, removing river connections on neighbouring tiles when needed

--- src/net/sf/freecol/common/model/TileItemContainer.java
+++ src/net/sf/freecol/common/model/TileItemContainer.java
@@ -367,6 +367,11 @@ public class TileItemContainer extends FreeColGameObject {
         TileType tileType = tile.getType();
         boolean removed = false;
         synchronized (tileItems) {
+            TileImprovement river = getRiver();
+            if(river != null && !river.isTileTypeAllowed(tileType)
+                && !tileType.isWater()) {
+                river.updateRiverConnections(null);
+            }
             removed = removeInPlace(tileItems,
                                     ti -> !ti.isTileTypeAllowed(tileType));
         }
