94c72d73c5365b09d0d22d86dba10a81e158e740
Mike Pope <mpope@computer.org>
2017-02-12 12:42:28 +1030

    Improve check-savegame messages further.

--- src/net/sf/freecol/FreeCol.java
+++ src/net/sf/freecol/FreeCol.java
@@ -1440,11 +1440,25 @@ public final class FreeCol {
                 freeColServer = new FreeColServer(fis, (Specification)null,
                                                   serverPort, serverName);
                 if (checkIntegrity) {
-                    boolean integrityOK = freeColServer.getIntegrity() > 0;
-                    gripe((integrityOK)
-                        ? "cli.check-savegame.success"
-                        : "cli.check-savegame.failure");
-                    System.exit((integrityOK) ? 0 : 2);
+                    String k;
+                    int ret;
+                    switch (freeColServer.getIntegrity()) {
+                    case 1:
+                        k = "cli.check-savegame.success";
+                        ret = 0;
+                        break;
+                    case 0:
+                        k = "cli.check-savegame.fixed";
+                        ret = 2;
+                        break;
+                    case -1: default:
+                        k = "cli.check-savegame.failure";
+                        ret = 3;
+                        break;
+                    }
+                    gripe(StringTemplate.template(k)
+                        .add("%log%", FreeColDirectories.getLogFilePath()));
+                    System.exit(ret);
                 }
             } catch (Exception e) {
                 if (checkIntegrity) gripe("cli.check-savegame.failure");
