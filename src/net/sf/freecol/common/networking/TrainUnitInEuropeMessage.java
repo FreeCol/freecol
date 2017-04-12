d7076c267b073f8bc432d69510c0954bb2d38f76
Mike Pope <mpope@computer.org>
2017-04-12 20:25:34 +0930

    Stream input for TrainUnitInEuropeMessage.

--- src/net/sf/freecol/common/networking/TrainUnitInEuropeMessage.java
+++ src/net/sf/freecol/common/networking/TrainUnitInEuropeMessage.java
@@ -19,6 +19,7 @@
 
 package net.sf.freecol.common.networking;
 
+import net.sf.freecol.common.io.FreeColXMLReader;
 import net.sf.freecol.common.model.Game;
 import net.sf.freecol.common.model.Player;
 import net.sf.freecol.common.model.UnitType;
@@ -58,6 +59,16 @@ public class TrainUnitInEuropeMessage extends AttributeMessage {
         super(TAG, UNIT_TYPE_TAG, getStringAttribute(element, UNIT_TYPE_TAG));
     }
 
+    /**
+     * Create a new {@code TrainUnitInEuropeMessage} from a stream.
+     *
+     * @param game The {@code Game} this message belongs to (null here).
+     * @param xr The {@code FreeColXMLReader} to read from.
+     */
+    public TrainUnitInEuropeMessage(Game game, FreeColXMLReader xr) {
+        super(TAG, xr, UNIT_TYPE_TAG);
+    }
+
 
     /**
      * {@inheritDoc}
@@ -74,7 +85,6 @@ public class TrainUnitInEuropeMessage extends AttributeMessage {
     public MessagePriority getPriority() {
         return Message.MessagePriority.NORMAL;
     }
-
 
     /**
      * {@inheritDoc}
