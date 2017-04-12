80a195891dab4caa5d13272bdc66d021bfd5d88b
Mike Pope <mpope@computer.org>
2017-04-12 09:58:27 +0930

    Stream input for AssignTeacherMessage.

--- src/net/sf/freecol/common/networking/AssignTeacherMessage.java
+++ src/net/sf/freecol/common/networking/AssignTeacherMessage.java
@@ -19,6 +19,7 @@
 
 package net.sf.freecol.common.networking;
 
+import net.sf.freecol.common.io.FreeColXMLReader;
 import net.sf.freecol.common.model.Game;
 import net.sf.freecol.common.model.Player;
 import net.sf.freecol.common.model.Unit;
@@ -61,6 +62,16 @@ public class AssignTeacherMessage extends AttributeMessage {
               TEACHER_TAG, getStringAttribute(element, TEACHER_TAG));
     }
 
+    /**
+     * Create a new {@code AssignTeacherMessage} from a stream.
+     *
+     * @param game The {@code Game} this message belongs to.
+     * @param xr The {@code FreeColXMLReader} to read from.
+     */
+    public AssignTeacherMessage(Game game, FreeColXMLReader xr) {
+        super(TAG, xr, STUDENT_TAG, TEACHER_TAG);
+    }
+
 
     /**
      * {@inheritDoc}
@@ -77,7 +88,6 @@ public class AssignTeacherMessage extends AttributeMessage {
     public MessagePriority getPriority() {
         return Message.MessagePriority.NORMAL;
     }
-
 
     /**
      * {@inheritDoc}
