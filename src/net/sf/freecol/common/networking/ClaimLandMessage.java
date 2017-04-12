e6c08eef09f49c731433a38a2f977fe8a6c0a496
Mike Pope <mpope@computer.org>
2017-04-12 10:05:58 +0930

    Stream input for ClaimLandMessage.

--- src/net/sf/freecol/common/networking/ClaimLandMessage.java
+++ src/net/sf/freecol/common/networking/ClaimLandMessage.java
@@ -19,6 +19,7 @@
 
 package net.sf.freecol.common.networking;
 
+import net.sf.freecol.common.io.FreeColXMLReader;
 import net.sf.freecol.common.model.FreeColGameObject;
 import net.sf.freecol.common.model.FreeColObject;
 import net.sf.freecol.common.model.Game;
@@ -69,6 +70,16 @@ public class ClaimLandMessage extends AttributeMessage {
               PRICE_TAG, getStringAttribute(element, PRICE_TAG));
     }
 
+    /**
+     * Create a new {@code ClaimLandMessage} from a stream.
+     *
+     * @param game The {@code Game} this message belongs to.
+     * @param xr The {@code FreeColXMLReader} to read from.
+     */
+    public ClaimLandMessage(Game game, FreeColXMLReader xr) {
+        super(TAG, xr, TILE_TAG, CLAIMANT_TAG, PRICE_TAG);
+    }
+
 
     /**
      * {@inheritDoc}
@@ -85,7 +96,6 @@ public class ClaimLandMessage extends AttributeMessage {
     public MessagePriority getPriority() {
         return Message.MessagePriority.NORMAL;
     }
-
 
     /**
      * {@inheritDoc}
