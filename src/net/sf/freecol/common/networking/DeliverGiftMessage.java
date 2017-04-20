6a9192ba65ba850397deca2066b209b8b53512ca
Mike Pope <mpope@computer.org>
2017-04-20 14:09:12 +0930

    Stream I/O for DeliverGiftMessage.

--- src/net/sf/freecol/common/networking/DeliverGiftMessage.java
+++ src/net/sf/freecol/common/networking/DeliverGiftMessage.java
@@ -21,6 +21,7 @@ package net.sf.freecol.common.networking;
 
 import javax.xml.stream.XMLStreamException;
 
+import net.sf.freecol.common.io.FreeColXMLReader;
 import net.sf.freecol.common.io.FreeColXMLWriter;
 import net.sf.freecol.common.model.Game;
 import net.sf.freecol.common.model.Goods;
@@ -42,14 +43,8 @@ public class DeliverGiftMessage extends ObjectMessage {
     private static final String SETTLEMENT_TAG = "settlement";
     private static final String UNIT_TAG = "unit";
 
-    /** The object identifier of the unit that is delivering the gift. */
-    private final String unitId;
-
-    /** The object identifier of the settlement the gift is going to. */
-    private final String settlementId;
-
     /** The goods to be delivered. */
-    private final Goods goods;
+    private Goods goods = null;
 
 
     /**
@@ -60,10 +55,8 @@ public class DeliverGiftMessage extends ObjectMessage {
      * @param goods The {@code Goods} to deliverGift.
      */
     public DeliverGiftMessage(Unit unit, IndianSettlement is, Goods goods) {
-        super(TAG);
+        super(TAG, UNIT_TAG, unit.getId(), SETTLEMENT_TAG, is.getId());
 
-        this.unitId = unit.getId();
-        this.settlementId = is.getId();
         this.goods = goods;
     }
 
@@ -77,11 +70,37 @@ public class DeliverGiftMessage extends ObjectMessage {
     public DeliverGiftMessage(Game game, Element element) {
         super(TAG);
 
-        this.unitId = getStringAttribute(element, UNIT_TAG);
-        this.settlementId = getStringAttribute(element, SETTLEMENT_TAG);
         this.goods = getChild(game, element, 0, Goods.class);
     }
 
+    /**
+     * Create a new {@code DeliverGiftMessage} from stream.
+     *
+     * @param game The {@code Game} this message belongs to.
+     * @param xr The {@code FreeColXMLReader} to read from.
+     * @exception XMLStreamException if there a problem reading the stream.
+     */
+    public DeliverGiftMessage(Game game, FreeColXMLReader xr)
+        throws XMLStreamException {
+        super(TAG, xr, UNIT_TAG, SETTLEMENT_TAG);
+
+        this.goods = null;
+        while (xr.moreTags()) {
+            String tag = xr.getLocalName();
+            if (Goods.TAG.equals(tag)) {
+                if (this.goods == null) {
+                    this.goods = xr.readFreeColObject(game, Goods.class);
+                } else {
+                    expected(TAG, tag);
+                }
+            } else {
+                expected(Goods.TAG, tag);
+            }
+            xr.expectTag(tag);
+        }
+        xr.expectTag(TAG);
+    }
+    
 
     /**
      * {@inheritDoc}
@@ -99,7 +118,63 @@ public class DeliverGiftMessage extends ObjectMessage {
         return MessagePriority.NORMAL;
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ChangeSet serverHandler(FreeColServer freeColServer,
+                                   ServerPlayer serverPlayer) {
+        String unitId = getStringAttribute(UNIT_TAG);
+        Unit unit;
+        try {
+            unit = serverPlayer.getOurFreeColGameObject(unitId, Unit.class);
+        } catch (Exception e) {
+            return serverPlayer.clientError(e.getMessage());
+        }
 
+        IndianSettlement is;
+        try {
+            is = unit.getAdjacentSettlement(getStringAttribute(SETTLEMENT_TAG),
+                                            IndianSettlement.class);
+        } catch (Exception e) {
+            return serverPlayer.clientError(e.getMessage());
+        }
+
+        if (this.goods == null) {
+            return serverPlayer.clientError("No goods found");
+        } else if (this.goods.getLocation() != unit) {
+            // Make sure we are trying to deliver something that is there
+            return serverPlayer.clientError("Gift " + this.goods.getId()
+                + " is not with unit " + unitId);
+        }
+
+        // Proceed to deliver.
+        return igc(freeColServer)
+            .deliverGiftToSettlement(serverPlayer, unit, is, this.goods);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void writeChildren(FreeColXMLWriter xw) throws XMLStreamException {
+        if (this.goods != null) this.goods.toXML(xw);
+    }
+    
+    /**
+     * Convert this DeliverGiftMessage to XML.
+     *
+     * @return The XML representation of this message.
+     */
+    @Override
+    public Element toXMLElement() {
+        return new DOMMessage(TAG,
+            UNIT_TAG, getStringAttribute(UNIT_TAG),
+            SETTLEMENT_TAG, getStringAttribute(SETTLEMENT_TAG))
+            .add(this.goods).toXMLElement();
+    }
+    
+    
     // Public interface
 
     /**
@@ -107,11 +182,12 @@ public class DeliverGiftMessage extends ObjectMessage {
      * is a helper routine to be called in-client as it blindly trusts
      * its field.
      *
+     * @param game The {@code Game} to lookup the unit in.
      * @return The {@code Unit}, or null if none.
      */
-    public Unit getUnit() {
-        return this.goods.getGame().getFreeColGameObject(this.unitId,
-                                                         Unit.class);
+    public Unit getUnit(Game game) {
+        return game.getFreeColGameObject(getStringAttribute(UNIT_TAG),
+                                         Unit.class);
     }
 
     /**
@@ -119,11 +195,12 @@ public class DeliverGiftMessage extends ObjectMessage {
      * This is a helper routine to be called in-client as it blindly trusts
      * its field.
      *
+     * @param game The {@code Game} to lookup the unit in.
      * @return The {@code Settlement}, or null if none.
      */
-    public IndianSettlement getSettlement() {
-        return this.goods.getGame().getFreeColGameObject(this.settlementId,
-            IndianSettlement.class);
+    public IndianSettlement getSettlement(Game game) {
+        return game.getFreeColGameObject(getStringAttribute(SETTLEMENT_TAG),
+                                         IndianSettlement.class);
     }
 
     /**
@@ -135,60 +212,5 @@ public class DeliverGiftMessage extends ObjectMessage {
      */
     public Goods getGoods() {
         return this.goods;
-    }
-
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public ChangeSet serverHandler(FreeColServer freeColServer,
-                                   ServerPlayer serverPlayer) {
-        Unit unit;
-        try {
-            unit = serverPlayer.getOurFreeColGameObject(this.unitId, Unit.class);
-        } catch (Exception e) {
-            return serverPlayer.clientError(e.getMessage());
-        }
-
-        IndianSettlement is;
-        try {
-            is = unit.getAdjacentSettlement(this.settlementId,
-                                            IndianSettlement.class);
-        } catch (Exception e) {
-            return serverPlayer.clientError(e.getMessage());
-        }
-
-        // Make sure we are trying to deliver something that is there
-        if (this.goods.getLocation() != unit) {
-            return serverPlayer.clientError("Gift " + this.goods.getId()
-                + " is not with unit " + this.unitId);
-        }
-
-        // Proceed to deliver.
-        return freeColServer.getInGameController()
-            .deliverGiftToSettlement(serverPlayer, unit, is, goods);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void toXML(FreeColXMLWriter xw) throws XMLStreamException {
-        // Suppress toXML for now
-        throw new XMLStreamException(getType() + ".toXML NYI");
-    }
-
-    /**
-     * Convert this DeliverGiftMessage to XML.
-     *
-     * @return The XML representation of this message.
-     */
-    @Override
-    public Element toXMLElement() {
-        return new DOMMessage(TAG,
-            UNIT_TAG, this.unitId,
-            SETTLEMENT_TAG, this.settlementId)
-            .add(this.goods).toXMLElement();
     }
 }
