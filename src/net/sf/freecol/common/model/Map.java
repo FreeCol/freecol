62445634e29ccf4b722e715291fa5703abea6baf
Mike Pope <mpope@computer.org>
2017-10-13 10:20:57 +1030

    Map size read is now unconditional.

--- src/net/sf/freecol/common/model/Map.java
+++ src/net/sf/freecol/common/model/Map.java
@@ -2701,21 +2701,17 @@ public class Map extends FreeColGameObject implements Location {
     protected void readAttributes(FreeColXMLReader xr) throws XMLStreamException {
         super.readAttributes(xr);
 
-        setLayer(xr.getAttribute(LAYER_TAG, Layer.class, Layer.ALL));
-
-        if (this.tiles == null) {
-            int width = xr.getAttribute(WIDTH_TAG, -1);
-            if (width < 0) {
-                throw new XMLStreamException("Bogus width: " + width);
-            }
-               
-            int height = xr.getAttribute(HEIGHT_TAG, -1);
-            if (height < 0) {
-                throw new XMLStreamException("Bogus height: " + height);
-            }
-
-            setTiles(width, height);
+        int width = xr.getAttribute(WIDTH_TAG, -1);
+        if (width < 0) {
+            throw new XMLStreamException("Bogus width: " + width);
         }
+        int height = xr.getAttribute(HEIGHT_TAG, -1);
+        if (height < 0) {
+            throw new XMLStreamException("Bogus height: " + height);
+        }
+        setTiles(width, height);
+
+        setLayer(xr.getAttribute(LAYER_TAG, Layer.class, Layer.ALL));
 
         minimumLatitude = xr.getAttribute(MINIMUM_LATITUDE_TAG, -90);
 
