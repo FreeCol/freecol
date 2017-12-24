e755bf0ef314d8906508c8155ca82ccd068cba46
Sebastian Zhorel <sebastian-zhorel@email.de>
2017-12-24 12:20:27 +0100

    Stop drawing unexplored borders on unexplored tiles, fixes BR#3118-4

--- src/net/sf/freecol/client/gui/TileViewer.java
+++ src/net/sf/freecol/client/gui/TileViewer.java
@@ -499,12 +499,14 @@ public final class TileViewer extends FreeColClientHolder {
     }
 
     void displayUnknownTileBorder(Graphics2D g, Tile tile) {
-        for (Direction direction : Direction.values()) {
-            Tile borderingTile = tile.getNeighbourOrNull(direction);
-            if (borderingTile != null && !borderingTile.isExplored()) {
-                g.drawImage(lib.getBorderImage(
-                        null, direction, tile.getX(), tile.getY()),
-                    0, 0, null);
+        if (tile.isExplored()) {
+            for (Direction direction : Direction.values()) {
+                Tile borderingTile = tile.getNeighbourOrNull(direction);
+                if (borderingTile != null && !borderingTile.isExplored()) {
+                    g.drawImage(lib.getBorderImage(
+                            null, direction, tile.getX(), tile.getY()),
+                        0, 0, null);
+                }
             }
         }
     }
