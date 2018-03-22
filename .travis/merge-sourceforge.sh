1eb15467fd39effa6e1b5676bc1b93819e64b1e6
David Lewis <highwayoflife@gmail.com>
2018-03-22 01:43:06 -0700

    Add a github deploy key for merges from SF
    Update travis yaml

---
+++ .travis/merge-sourceforge.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -xe
+
+# Create a deploy key from the encrypted file
+openssl aes-256-cbc -K $encrypted_57654742c400_key -iv $encrypted_57654742c400_iv \
+  -in ./.travis/freecol-deploy-key.enc -out freecol-deploy-key -d
+chmod 600 freecol-deploy-key
+eval $(ssh-agent -s)
+ssh-add freecol-deploy-key
+
+git remote add upstream git://git.code.sf.net/p/freecol/git
+git remote add github git@github.com:FreeCol/freecol.git
+git remote update
+git checkout master
+git merge upstream/master --no-edit
