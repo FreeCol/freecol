36215cf08e7d1646c63049af50093ef18f2ea35d
Mike Pope <mpope@computer.org>
2020-07-06 14:05:27 +0930

    Make NegotiationDialog summary scrollable, BR#3191.

--- src/net/sf/freecol/client/gui/dialog/NegotiationDialog.java
+++ src/net/sf/freecol/client/gui/dialog/NegotiationDialog.java
@@ -21,6 +21,7 @@ package net.sf.freecol.client.gui.dialog;
 
 import java.awt.Component;
 import java.awt.Cursor;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -38,9 +39,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.JTextArea;
 import javax.swing.ListCellRenderer;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.SpinnerNumberModel;
 
 import net.miginfocom.swing.MigLayout;
@@ -946,7 +949,6 @@ public final class NegotiationDialog extends FreeColDialog<DiplomaticTrade> {
         this.summary = new MigPanel(new MigLayout("wrap 2", "[20px:n:n][]"));
         this.summary.setOpaque(false);
         this.summary.add(Utility.localizedTextArea(comment), "center, span 2");
-
         /**
          * Build Layout of Diplomatic Trade Dialog
          */
@@ -975,13 +977,19 @@ public final class NegotiationDialog extends FreeColDialog<DiplomaticTrade> {
         // TODO: Expand center panel so that contents fill cell horizontally. 
         panel.add(this.goldDemandPanel); // Left pane
         JPanel centerPanel = new MigPanel(new MigLayout("wrap 1"));
+        centerPanel.setMinimumSize(new Dimension(250, 50));
         if (tutorial != null) {
             // Display only if tutorial variable contents overriden
             //      Can only occur if: First Contact with a forgeign Nation
             JTextArea tutArea = Utility.localizedTextArea(tutorial, 30);
             centerPanel.add(tutArea, "center");
         }
-        centerPanel.add(this.summary, "top");
+        JScrollPane scroll = new JScrollPane(this.summary,
+            ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+            ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scroll.getViewport().setOpaque(false);
+        scroll.setBorder(null);
+        centerPanel.add(scroll, "top, width 100%");
         panel.add(centerPanel, "spany, top"); // Center pane
         panel.add(this.goldOfferPanel); // Right pane
 
