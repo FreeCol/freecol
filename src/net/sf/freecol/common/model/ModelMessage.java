16f92bdb2dc2ad05a5ba558214954f4af446d016
Mike Pope <mpope@computer.org>
2022-02-19 17:01:28 +1030

    Missing message buttons, BR#3231.

--- src/net/sf/freecol/common/model/ModelMessage.java
+++ src/net/sf/freecol/common/model/ModelMessage.java
@@ -380,7 +380,7 @@ public class ModelMessage extends StringTemplate {
                         if (index > start) {
                             next.add(str.substring(start, index));
                         }
-                        next.add(b);
+                        next.add(Utility.getMessageButton(key, val, player, source));
                         start = index + val.length();
                     }
                     next.add(str.substring(start, str.length()));
