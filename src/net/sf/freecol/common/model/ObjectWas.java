16a8f3e5cd6ff4a8b26eb3b802e3a3a77365e7b8
Mike Pope <mpope@computer.org>
2022-05-02 17:07:04 +0930

    Git fumble omission from previous.

---
+++ src/net/sf/freecol/common/model/ObjectWas.java
@@ -0,0 +1,33 @@
+/**
+ *  Copyright (C) 2002-2022   The FreeCol Team
+ *
+ *  This file is part of FreeCol.
+ *
+ *  FreeCol is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  FreeCol is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with FreeCol.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.sf.freecol.common.model;
+
+/**
+ * Parent for all the objects that may need to fireChanges.
+ */
+public abstract class ObjectWas {
+
+    /**
+     * Fire the property changes that have accumulated for this object.
+     *
+     * @return True if something changed.
+     */
+    public abstract boolean fireChanges();
+}
