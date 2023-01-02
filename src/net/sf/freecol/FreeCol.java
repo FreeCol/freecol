9367c6e09a16a83d259ca40259bdab2de1a05927
Stian Grenborgen <stiangre@ifi.uio.no>
2023-01-03 00:27:49 +0100

    Updates version number for release.

--- src/net/sf/freecol/FreeCol.java
+++ src/net/sf/freecol/FreeCol.java
@@ -112,7 +112,7 @@ public final class FreeCol {
     private static final Logger logger = Logger.getLogger(FreeCol.class.getName());
 
     /** The FreeCol release version number. */
-    private static final String FREECOL_VERSION = "0.13.0+dev";
+    private static final String FREECOL_VERSION = "1.0.0";
 
     /** The FreeCol protocol version number. */
     private static final String FREECOL_PROTOCOL_VERSION = "0.1.6";
