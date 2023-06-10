2591943d4da3a449852ab907afcfad2f0f87921e
Stian Grenborgen <stiangre@ifi.uio.no>
2023-06-10 21:40:41 +0200

    Fixes hidden OK-button on small screens.

--- src/net/sf/freecol/client/gui/panel/colopedia/ColopediaPanel.java
+++ src/net/sf/freecol/client/gui/panel/colopedia/ColopediaPanel.java
@@ -74,7 +74,7 @@ public final class ColopediaPanel extends FreeColPanel
      */
     public ColopediaPanel(FreeColClient freeColClient, String id) {
         super(freeColClient, "ColopediaPanelUI",
-              new MigLayout("fill", "[750:, grow, fill]",
+              new MigLayout("fill", "[grow, fill]",
                             "[][grow, fill][]"));
 
         add(Utility.localizedHeader("colopedia", Utility.FONTSPEC_TITLE),
@@ -103,7 +103,7 @@ public final class ColopediaPanel extends FreeColPanel
         detailPanel.setOpaque(true);
         JScrollPane detail = new JScrollPane(detailPanel,
                                              JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-                                             JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                                             JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         detail.getVerticalScrollBar().setUnitIncrement(16);
         detail.getViewport().setOpaque(false);
         
