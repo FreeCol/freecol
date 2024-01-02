a77adcc70a6207b600821bc8953dc1eff922dac0
Stian Grenborgen <stiangre@ifi.uio.no>
2024-01-02 18:08:57 +0100

    The selected carrier in the ColonyPanel is now displayed with a border.

--- src/net/sf/freecol/client/gui/panel/ColonyPanel.java
+++ src/net/sf/freecol/client/gui/panel/ColonyPanel.java
@@ -23,6 +23,7 @@ import static net.sf.freecol.common.util.CollectionUtils.dump;
 import static net.sf.freecol.common.util.CollectionUtils.sort;
 import static net.sf.freecol.common.util.CollectionUtils.transform;
 
+import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
@@ -34,6 +35,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.Stroke;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
@@ -1705,6 +1708,26 @@ public final class ColonyPanel extends PortPanel
             removePropertyChangeListeners();
             super.selectLabel();
             addPropertyChangeListeners();
+        
+        }
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            
+            final Graphics2D g2d = (Graphics2D) g;
+            
+            if (selectedUnitLabel != null) {
+                final Color oldColor = g.getColor();
+                final Stroke oldStroke = g2d.getStroke();
+                g2d.setColor(ImageLibrary.getColor("color.carrier.selected"));
+                final Rectangle bounds = selectedUnitLabel.getBounds();
+                final int scaledLine = getImageLibrary().scaleInt(2);
+                g2d.setStroke(new BasicStroke(scaledLine));
+                
+                g2d.drawRect(bounds.x - scaledLine, bounds.y - scaledLine, bounds.width + scaledLine * 2, bounds.height + scaledLine * 2);
+                g2d.setColor(oldColor);
+                g2d.setStroke(oldStroke);
+            }
         }
     }
 
