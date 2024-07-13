FreeCol/freecol

commit f107cdd308db7fb9660061bca6506a884ca47d4d

Fixes an issue where the accelerator keys, for the menu items, stopped working.

src/net/sf/freecol/client/gui/Canvas.java
@@ -233,13 +233,7 @@ public final class Canvas extends JDesktopPane {
         setLayout(null);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
-        // Create key bindings for all actions
-        for (Option<?> option : freeColClient.getActionManager().getOptions()) {
-            FreeColAction action = (FreeColAction)option;
-            getInputMap().put(action.getAccelerator(), action.getId());
-            getActionMap().put(action.getId(), action);
-        }
-        
+
         this.parentFrame.setVisible(true);
         
         updateSize();

src/net/sf/freecol/client/gui/action/DisplayBordersAction.java
@@ -19,10 +19,6 @@
 
 package net.sf.freecol.client.gui.action;
 
-import java.awt.event.ActionEvent;
-
-import javax.swing.JCheckBoxMenuItem;
-
 import net.sf.freecol.client.ClientOptions;
 import net.sf.freecol.client.FreeColClient;
 
@@ -44,18 +40,5 @@ public class DisplayBordersAction extends SelectableOptionAction {
         super(freeColClient, id, ClientOptions.DISPLAY_BORDERS);
 
         setSelected(shouldBeSelected());
-    }
-
-
-    // Interface ActionListener
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void actionPerformed(ActionEvent ae) {
-        setSelected(((JCheckBoxMenuItem)ae.getSource()).isSelected());
-        setOption(isSelected());
-        getGUI().refresh();
     }
 }

src/net/sf/freecol/client/gui/action/DisplayFogOfWarAction.java
@@ -47,18 +47,6 @@ public class DisplayFogOfWarAction extends SelectableOptionAction {
         setSelected(shouldBeSelected());
     }
 
-
-    // Interface ActionListener
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void actionPerformed(ActionEvent ae) {
-        setSelected(((JCheckBoxMenuItem)ae.getSource()).isSelected());
-        setOption(isSelected());
-        getGUI().refresh();
-    }
     
     @Override
     protected boolean shouldBeEnabled() {

src/net/sf/freecol/client/gui/action/DisplayGridAction.java
@@ -19,10 +19,6 @@
 
 package net.sf.freecol.client.gui.action;
 
-import java.awt.event.ActionEvent;
-
-import javax.swing.JCheckBoxMenuItem;
-
 import net.sf.freecol.client.ClientOptions;
 import net.sf.freecol.client.FreeColClient;
 
@@ -44,18 +40,5 @@ public class DisplayGridAction extends SelectableOptionAction {
         super(freeColClient, id, ClientOptions.DISPLAY_GRID);
 
         setSelected(shouldBeSelected());
-    }
-
-
-    // Interface ActionListener
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void actionPerformed(ActionEvent ae) {
-        setSelected(((JCheckBoxMenuItem)ae.getSource()).isSelected());
-        setOption(isSelected());
-        getGUI().refresh();
     }
 }

src/net/sf/freecol/client/gui/action/DisplayTileTextAction.java
@@ -96,10 +96,7 @@ public final class DisplayTileTextAction extends SelectableOptionAction {
      */
     @Override
     public void actionPerformed(ActionEvent ae) {
-        if (((JRadioButtonMenuItem)ae.getSource()).isSelected()) {
-            freeColClient.getClientOptions()
-                .setInteger(ClientOptions.DISPLAY_TILE_TEXT, display.ordinal());
-            getGUI().refresh();
-        }
+        freeColClient.getClientOptions().setInteger(ClientOptions.DISPLAY_TILE_TEXT, display.ordinal());
+        getGUI().refresh();
     }
 }

src/net/sf/freecol/client/gui/action/MapControlsAction.java
@@ -76,6 +76,5 @@ public class MapControlsAction extends SelectableOptionAction {
     @Override
     public void actionPerformed(ActionEvent ae) {
         getGUI().enableMapControls(isEnabled() && isSelected());
-        setOption(isSelected());
     }
 }

src/net/sf/freecol/client/gui/action/SelectableOptionAction.java
@@ -19,10 +19,11 @@
 
 package net.sf.freecol.client.gui.action;
 
+import java.awt.event.ActionEvent;
 import java.util.logging.Level;
 
-import net.sf.freecol.client.FreeColClient;
 import net.sf.freecol.client.ClientOptions;
+import net.sf.freecol.client.FreeColClient;
 
 
 /**
@@ -33,8 +34,6 @@ public abstract class SelectableOptionAction extends SelectableAction {
     public static final String id = "selectableOptionAction";
 
     private final String optionId;
-
-    protected boolean selected = false;
 
 
     /**
@@ -88,5 +87,14 @@ public abstract class SelectableOptionAction extends SelectableAction {
      */
     protected boolean shouldBeSelected() {
         return getOption();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        setOption(isSelected());
+        getGUI().refresh();
     }
 }

src/net/sf/freecol/client/gui/menu/FreeColMenuBar.java
@@ -133,6 +133,9 @@ public abstract class FreeColMenuBar extends JMenuBar {
             if (action.getMnemonic() != null) {
                 rtn.addMenuKeyListener(action.getMenuKeyListener());
             }
+            
+            rtn.getInputMap().put(action.getAccelerator(), action.getId());
+            rtn.getActionMap().put(action.getId(), action);
         } else {
             logger.finest("Could not create menu item. [" + actionId
                 + "] not found.");
@@ -174,7 +177,8 @@ public abstract class FreeColMenuBar extends JMenuBar {
             rtn.setAction(action);
             rtn.setOpaque(false);
 
-            //rtn.setSelected(((SelectableAction) am.getFreeColAction(actionId)).isSelected());
+            rtn.getInputMap().put(action.getAccelerator(), action.getId());
+            rtn.getActionMap().put(action.getId(), action);
         } else {
             logger.finest("Could not create menu item. [" + actionId + "] not found.");
         }
@@ -202,6 +206,9 @@ public abstract class FreeColMenuBar extends JMenuBar {
 
             rtn.setSelected(((SelectableOptionAction) am.getFreeColAction(actionId)).isSelected());
             group.add(rtn);
+            
+            rtn.getInputMap().put(action.getAccelerator(), action.getId());
+            rtn.getActionMap().put(action.getId(), action);
         } else {
             logger.finest("Could not create menu item. [" + actionId
                 + "] not found.");

commit 5616e40d7abb9f5cc49cc4615a0d7e0f19d1bbe9

Fixes a regression caused by f107cdd: Move keys stopped working.

src/net/sf/freecol/client/gui/Canvas.java
@@ -61,6 +61,7 @@ import javax.swing.plaf.basic.BasicInternalFrameUI;
 import net.sf.freecol.client.ClientOptions;
 import net.sf.freecol.client.FreeColClient;
 import net.sf.freecol.client.gui.SwingGUI.PopupPosition;
+import net.sf.freecol.client.gui.action.FreeColAction;
 import net.sf.freecol.client.gui.dialog.FreeColDialog;
 import net.sf.freecol.client.gui.mapviewer.CanvasMapViewer;
 import net.sf.freecol.client.gui.mapviewer.MapViewer;
@@ -81,6 +82,7 @@ import net.sf.freecol.client.gui.video.VideoListener;
 import net.sf.freecol.common.i18n.Messages;
 import net.sf.freecol.common.model.Specification;
 import net.sf.freecol.common.option.IntegerOption;
+import net.sf.freecol.common.option.Option;
 import net.sf.freecol.common.option.OptionGroup;
 import net.sf.freecol.common.resources.Video;
 
@@ -231,6 +233,15 @@ public final class Canvas extends JDesktopPane {
         setLayout(null);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
+        // Create key bindings for all actions
+        for (FreeColAction action  : freeColClient.getActionManager().getFreeColActions()) {
+            if (!action.isCanvasKeyBinding()) {
+                continue;
+            }
+            getInputMap().put(action.getAccelerator(), action.getId());
+            getActionMap().put(action.getId(), action);
+        }
+
 
         this.parentFrame.setVisible(true);
         

src/net/sf/freecol/client/gui/action/ActionManager.java
@@ -20,7 +20,7 @@
 package net.sf.freecol.client.gui.action;
 
 import java.util.logging.Logger;
-
+import java.util.stream.Collectors;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -211,6 +211,17 @@ public class ActionManager extends OptionGroup {
         return (hasOption(id, FreeColAction.class))
             ? getOption(id, FreeColAction.class)
             : null;
+    }
+    
+    /***
+     * Gets all registered {@code FreeColAction}s.
+     * @return The list of all actions.
+     */
+    public List<FreeColAction> getFreeColActions() {
+        return getOptions().stream()
+                .filter(o -> o instanceof FreeColAction)
+                .map(o -> (FreeColAction) o)
+                .collect(Collectors.toList());
     }
 
     /**

src/net/sf/freecol/client/gui/action/FreeColAction.java
@@ -118,6 +118,7 @@ public abstract class FreeColAction extends AbstractAction
 
     private int orderButtonImageCount = 0;
     private List<String> imageIconKeys = new ArrayList<>();
+    private boolean canvasKeyBinding = false;
 
     /**
      * Creates a new {@code FreeColAction}.
@@ -415,6 +416,24 @@ public abstract class FreeColAction extends AbstractAction
     public String getEnabledBy() {
         /* FreeColAction cannot be enabled/disabled by other options. */
         return null;
+    }
+    
+    /**
+     * Checks if this action should have its accelerator key added to the {@code Canvas}.
+     * @return {@code true} if the accelerator key should be added to the {@code Canvas}. This
+     *      is typically used when this action is not used in any menu.
+     */
+    public boolean isCanvasKeyBinding() {
+        return canvasKeyBinding;
+    }
+    
+    /**
+     * Defines if this action should have its accelerator key added to the {@code Canvas}.
+     * @param canvasKeyBinding {@code true} if the accelerator key should be added to the
+     *      {@code Canvas}. This is typically used when this action is not used in any menu.
+     */
+    public void setCanvasKeyBinding(boolean canvasKeyBinding) {
+        this.canvasKeyBinding = canvasKeyBinding;
     }
 
 

src/net/sf/freecol/client/gui/action/MoveAction.java
@@ -47,6 +47,8 @@ public class MoveAction extends MapboardAction {
         super(freeColClient, id + direction);
 
         this.direction = direction;
+        
+        setCanvasKeyBinding(true);
     }
 
     /**